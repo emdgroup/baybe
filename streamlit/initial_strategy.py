78a76cfb934bc627ed712a162b6658a7b7ee4fc2
Adrian Sosic <adrian.sosic@merckgroup.com>
2022-10-14 21:27:35 +0200

    Revise initial_strategy.py
    - Bugfix: actually set random seed
    - Rework Gaussian mixture logic
    - Add data distribution parameter options
    - Remove pandas dependency (use numpy only)
    - Remove unnecessary code
    - Fix docstrings
    - Fix linter issues

--- streamlit/initial_strategy.py
+++ streamlit/initial_strategy.py
@@ -1,89 +1,116 @@
 """
-The purpose of this script is to compare different surrogate models and verify the
-performance between different initial sampling algorithms.
+This script allows comparing initial selection strategies on different data sets.
 """
 
 import numpy as np
-import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 
 from baybe.utils.sampling_algorithms import farthest_point_sampling
 from sklearn.datasets import make_blobs
-from sklearn.mixture import GaussianMixture
 
 
-st.set_page_config(page_title="Initial Strategy", page_icon="🚀", layout="centered")
-# show docstring in dashboard
-st.info(__doc__)
-
-
-def random_strategy(points: pd.DataFrame, batch_quantity: int = 1):
-    """Random strategy"""
-    return pd.Index(np.random.choice(points.index, batch_quantity, replace=False))
+def random_strategy(points: np.ndarray, n_samples: int = 1) -> np.ndarray:
+    """Random point selection strategy."""
+    return np.random.choice(range(len(points)), n_samples, replace=False)
 
 
 def uniform_distribution(n_points: int) -> np.ndarray:
-    """Uniformly distributed point generation"""
+    """Uniformly distributed point generation."""
     return np.random.rand(n_points, 2)
 
 
-def gaussian_mixture_model(n_points: int) -> np.ndarray:
-    """GMM point generation"""
-    x_points, _ = make_blobs(  # pylint: disable=unbalanced-tuple-unpacking
-        n_samples=n_points
+def gaussian_mixture_model(
+    n_points: int,
+    n_mixtures: int = 3,
+    mixture_std: float = 1.0,
+) -> np.ndarray:
+    """Point generation via Gaussian mixture model."""
+    points, *_ = make_blobs(
+        n_samples=n_points,
+        centers=n_mixtures,
+        cluster_std=mixture_std,
     )
-    gmm = GaussianMixture(n_components=3)
-    gmm.fit(x_points)
-    features, _ = gmm.sample(n_points)
-    return features
+    return points
 
 
-def plot_plotly(results, title, features):
-    """Visualize point selection and its selection order"""
-    fig = go.Figure(data=go.Scatter(x=features[:, 0], y=features[:, 1], mode="markers"))
-    for step, _ in enumerate(results):
+def plot_point_selection(points, selection, title):
+    """Visualizes a point selection and the corresponding selection order."""
+    fig = go.Figure(data=go.Scatter(x=points[:, 0], y=points[:, 1], mode="markers"))
+    for step, _ in enumerate(selection):
         fig.add_annotation(
-            x=features[:, 0][results[step]],
-            y=features[:, 1][results[step]],
+            x=points[selection[step], 0],
+            y=points[selection[step], 1],
             text=str(step),
             arrowcolor="#8b0000",
-            font=dict(color="#8b0000"),
         )
-
     fig.update_layout(
         title={
             "text": title,
-            "y": 0.9,
-            "x": 0.5,
-            "xanchor": "center",
-            "yanchor": "top",
+            "y": 0.85,
         },
         template="seaborn",
+    )
+    fig.update_yaxes(
+        scaleanchor="x",
+        scaleratio=1,
     )
     return fig
 
 
-# define all available test functions
-test_datas = {
-    "Uniform Distribution": uniform_distribution,
+# collect all available data distributions
+data_distributions = {
+    "Uniform": uniform_distribution,
     "Gaussian Mixture Model": gaussian_mixture_model,
 }
 
-test_strategies = {
-    "Random": random_strategy,
+# collect all available strategies
+selection_strategies = {
     "FPS": farthest_point_sampling,
+    "Random": random_strategy,
 }
 
 
-# simulation parameters
-random_seed = int(st.sidebar.number_input("Random seed", value=42))
-dataset = st.sidebar.selectbox("Data", list(test_datas.keys()))
-strategy = st.sidebar.selectbox("Strategy", list(test_strategies.keys()))
-data_points = st.sidebar.slider("Number of points to be generated", 10, 1000, value=50)
-sampled_points = st.sidebar.slider("Number of points to be sampled", 2, 100, value=10)
+def main():
+    """Creates the streamlit dashboard."""
+
+    # show docstring in dashboard
+    st.info(__doc__)
+
+    # simulation parameters
+    random_seed = int(st.sidebar.number_input("Random seed", value=42))
+    strategy = st.sidebar.selectbox("Strategy", list(selection_strategies.keys()))
+    n_points = st.sidebar.slider("Number of points to be generated", 10, 100, value=50)
+    n_selected = st.sidebar.slider(
+        "Number of points to be selected",
+        1,
+        n_points,
+        value=int(np.ceil(n_points / 10)),
+    )
+    distribution = st.sidebar.selectbox(
+        "Data distribution", list(data_distributions.keys())
+    )
+
+    # set the distribution options
+    if distribution == "Gaussian Mixture Model":
+        distribution_params = {
+            "n_mixtures": st.sidebar.number_input("Number of mixtures", 1, value=2),
+            "mixture_std": st.sidebar.slider("Mixture size", 0.0, 1.0, value=1.0),
+        }
+    else:
+        distribution_params = {}
+
+    # fix the random seed
+    np.random.seed(random_seed)
+
+    # create the points and the selection
+    points = data_distributions[distribution](n_points, **distribution_params)
+    selection = selection_strategies[strategy](points, n_selected)
+
+    # show the result
+    fig = plot_point_selection(points, selection, strategy)
+    st.plotly_chart(fig)
 
 
-feats = test_datas[dataset](data_points)
-res = test_strategies[strategy](pd.DataFrame(feats), sampled_points)
-st.plotly_chart(plot_plotly(res, strategy, feats), use_container_width=True)
+if __name__ == "__main__":
+    main()
