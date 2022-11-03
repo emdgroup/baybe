5da285bbb7ab49da52f748e79c72f412557d8574
Martin Fitzner <martin.fitzner@merckgroup.com>
2022-11-03 16:39:28 +0100

    Make dependency debug script also test separate configuration variants

--- debug/Constraints_Discrete/run_dependency.py
+++ debug/Constraints_Discrete/run_dependency.py
@@ -1,6 +1,7 @@
 """
 Test for imposing dependency constraints
 """
+from copy import deepcopy
 
 import numpy as np
 
@@ -66,36 +67,67 @@ config_dict = {
     "strategy": {
         # "surrogate_model_cls": "GP",
     },
-    "constraints": [
-        {
-            "type": "DEPENDENCIES",
-            "parameters": ["Switch1", "Switch2"],
-            "conditions": [
-                {
-                    "type": "SUBSELECTION",
-                    "selection": ["on"],
-                },
-                {
-                    "type": "SUBSELECTION",
-                    "selection": ["right"],
-                },
-            ],
-            "affected_parameters": [
-                ["Solvent1", "Fraction1"],
-                ["FrameA", "FrameB"],
-            ],
-        },
-    ],
 }
 
-# Create BayBE object, add fake results and print what happens to internal data
-config = BayBEConfig(**config_dict)
-baybe_obj = BayBE(config)
-print(baybe_obj)
+constraints1 = [
+    {
+        "type": "DEPENDENCIES",
+        "parameters": ["Switch1", "Switch2"],
+        "conditions": [
+            {
+                "type": "SUBSELECTION",
+                "selection": ["on"],
+            },
+            {
+                "type": "SUBSELECTION",
+                "selection": ["right"],
+            },
+        ],
+        "affected_parameters": [
+            ["Solvent1", "Fraction1"],
+            ["FrameA", "FrameB"],
+        ],
+    },
+]
 
-N_ITERATIONS = 3
+constraints2 = [
+    {
+        "type": "DEPENDENCIES",
+        "parameters": ["Switch1"],
+        "conditions": [
+            {
+                "type": "SUBSELECTION",
+                "selection": ["on"],
+            },
+        ],
+        "affected_parameters": [
+            ["Solvent1", "Fraction1"],
+        ],
+    },
+    {
+        "type": "DEPENDENCIES",
+        "parameters": ["Switch2"],
+        "conditions": [
+            {
+                "type": "SUBSELECTION",
+                "selection": ["right"],
+            },
+        ],
+        "affected_parameters": [
+            ["FrameA", "FrameB"],
+        ],
+    },
+]
+
+N_ITERATIONS = 2
+
+# Create BayBE object, add fake results and print what happens to internal data
+conf1 = deepcopy(config_dict)
+conf1["constraints"] = constraints1
+config = BayBEConfig(**conf1)
+baybe_obj = BayBE(config)
 for kIter in range(N_ITERATIONS):
-    print(f"\n##### ITERATION {kIter+1} #####")
+    print(f"\n##### Version1 ITERATION {kIter+1} #####")
 
     print("### ASSERTS ###")
     print(
@@ -137,3 +169,52 @@ for kIter in range(N_ITERATIONS):
         add_parameter_noise(rec, baybe_obj, noise_level=0.1)
 
     baybe_obj.add_results(rec)
+
+
+conf2 = deepcopy(config_dict)
+conf2["constraints"] = constraints2
+config2 = BayBEConfig(**conf2)
+baybe_obj2 = BayBE(config2)
+for kIter in range(N_ITERATIONS):
+    print(f"\n##### Version2 ITERATION {kIter+1} #####")
+
+    print("### ASSERTS ###")
+    print(
+        f"Number entries with both switches on "
+        f"(expected {N_GRID_POINTS*len(solvs)*2*2}): ",
+        (
+            (baybe_obj2.searchspace_exp_rep["Switch1"] == "on")
+            & (baybe_obj2.searchspace_exp_rep["Switch2"] == "right")
+        ).sum(),
+    )
+    print(
+        f"Number entries with Switch1 off " f"(expected {2*2}):       ",
+        (
+            (baybe_obj2.searchspace_exp_rep["Switch1"] == "off")
+            & (baybe_obj2.searchspace_exp_rep["Switch2"] == "right")
+        ).sum(),
+    )
+    print(
+        f"Number entries with Switch2 off "
+        f"(expected {N_GRID_POINTS*len(solvs)}):"
+        f"      ",
+        (
+            (baybe_obj2.searchspace_exp_rep["Switch1"] == "on")
+            & (baybe_obj2.searchspace_exp_rep["Switch2"] == "left")
+        ).sum(),
+    )
+    print(
+        "Number entries with both switches off (expected 1): ",
+        (
+            (baybe_obj2.searchspace_exp_rep["Switch1"] == "off")
+            & (baybe_obj2.searchspace_exp_rep["Switch2"] == "left")
+        ).sum(),
+    )
+
+    rec = baybe_obj2.recommend(batch_quantity=5)
+
+    add_fake_results(rec, baybe_obj2)
+    if kIter % 2:
+        add_parameter_noise(rec, baybe_obj2, noise_level=0.1)
+
+    baybe_obj2.add_results(rec)
