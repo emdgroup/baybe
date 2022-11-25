21efb9e845856df36b4dee6c9fd5c68560414435
Martin Fitzner <martin.fitzner@merckgroup.com>
2022-11-25 23:53:24 +0100

    Update input/output tests

--- tests/test_input_output.py
+++ tests/test_input_output.py
@@ -1,6 +1,7 @@
 """
 Tests for basic input-output nad iterative loop.
 """
+import numpy as np
 import pytest
 
 from baybe.core import BayBE, BayBEConfig
@@ -26,17 +27,26 @@ def test_run_iterations(
         baybe_obj.add_results(rec)
 
 
-def test_bad_input_value(config_basic_1target, good_reference_values):
+@pytest.mark.parametrize(
+    "bad_val",
+    [1337, np.nan, "asd"],
+    ids=["not_within_tol", "nan", "string_instead_float"],
+)
+def test_bad_input_value(
+    baybe_object_batch3_iterations2, good_reference_values, bad_val
+):
     """
     Test attempting to read in an invalid parameter value.
     """
-    config = BayBEConfig(**config_basic_1target)
-    baybe_obj = BayBE(config)
 
-    rec = baybe_obj.recommend(batch_quantity=3)
-    add_fake_results(rec, baybe_obj, good_reference_values=good_reference_values)
+    rec = baybe_object_batch3_iterations2.recommend(batch_quantity=3)
+    add_fake_results(
+        rec,
+        baybe_object_batch3_iterations2,
+        good_reference_values=good_reference_values,
+    )
 
     # Add an invalid value
-    rec.Num_disc_1.iloc[0] = 1337
+    rec.Num_disc_1.iloc[0] = bad_val
     with pytest.raises(ValueError):
-        baybe_obj.add_results(rec)
+        baybe_object_batch3_iterations2.add_results(rec)
