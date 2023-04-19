4549ed9d576e779ddfbfb2f9635df26bd5dc0ade
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-04-19 11:02:24 +0200

    Refactor test_targets.py

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,120 +1,121 @@
+# pylint: disable=missing-function-docstring
+
 """Tests for the targets module."""
 
 import pytest
 
 from baybe.targets import NumericalTarget, Objective
-from baybe.utils import StrictValidationError
-from pydantic import ValidationError
-
-# Validation errors to be caught
-validation_errors = (ValidationError, StrictValidationError)
-
-# Two example targets used in the tests
-two_targets = [
-    NumericalTarget(
-        name="Target_1",
-        mode="MAX",
-        bounds=(0, 100),
-    ),
-    NumericalTarget(
-        name="Target_2",
-        mode="MIN",
-        bounds=(0, 100),
-    ),
-]
 
 
-def test_invalid_target_creation():
+class TestInvalidTargetCreation:
     """Invalid target creation raises expected error."""
 
-    # Scenario: missing bounds for match mode
-    with pytest.raises(validation_errors):
-        NumericalTarget(
-            name="missing_bounds",
-            mode="MATCH",
-        )
+    def test_missing_bounds_for_match_mode(self):
+        with pytest.raises(ValueError):
+            NumericalTarget(
+                name="missing_bounds",
+                mode="MATCH",
+            )
 
-    # Scenario: incompatible transformation for match mode
-    with pytest.raises(validation_errors):
-        NumericalTarget(
-            name="incompatible_transform",
-            mode="MATCH",
-            bounds=(0, 100),
-            bounds_transform_func="LINEAR",
-        )
+    def test_incompatible_transformation_for_match_mode(self):
+        with pytest.raises(ValueError):
+            NumericalTarget(
+                name="incompatible_transform",
+                mode="MATCH",
+                bounds=(0, 100),
+                bounds_transform_func="LINEAR",
+            )
 
-    # Scenario: invalid transformation
-    with pytest.raises(validation_errors):
-        NumericalTarget(
-            name="invalid_transform",
-            mode="MATCH",
-            bounds=(0, 100),
-            bounds_transform_func="SOME_STUFF",
-        )
+    def test_invalid_transformation(self):
+        with pytest.raises(ValueError):
+            NumericalTarget(
+                name="invalid_transform",
+                mode="MATCH",
+                bounds=(0, 100),
+                bounds_transform_func="SOME_STUFF",
+            )
 
 
-def test_invalid_objective_creation():
+class TestInvalidObjectiveCreation:
     """Invalid objective creation raises expected error."""
 
-    # Scenario: empty target list
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="SINGLE",
-            targets=[],
-        )
+    # Two example targets used in the tests
+    two_targets = [
+        NumericalTarget(
+            name="Target_1",
+            mode="MAX",
+            bounds=(0, 100),
+        ),
+        NumericalTarget(
+            name="Target_2",
+            mode="MIN",
+            bounds=(0, 100),
+        ),
+    ]
 
-    # Scenario: wrong target type
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="SINGLE",
-            targets={"A": 1, "B": 2},
-        )
+    @pytest.mark.xfail(reason="Attribute validation yet to be implemented")
+    def test_empty_target_list(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="SINGLE",
+                targets=[],
+            )
 
-    # Scenario: missing bounds for desirability
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="DESIRABILITY",
-            targets=[
-                NumericalTarget(
-                    name="Target_1",
-                    mode="MAX",
-                    bounds=(0, 100),
-                ),
-                NumericalTarget(
-                    name="Target_2",
-                    mode="MIN",
-                ),
-            ],
-        )
+    def test_wrong_target_type(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="SINGLE",
+                targets={"A": 1, "B": 2},
+            )
 
-    # Scenario: invalid combination function
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="DESIRABILITY",
-            combine_func="FALSE_STUFF",
-            targets=two_targets,
-        )
+    def test_missing_bounds_for_desirability(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="DESIRABILITY",
+                targets=[
+                    NumericalTarget(
+                        name="Target_1",
+                        mode="MAX",
+                        bounds=(0, 100),
+                    ),
+                    NumericalTarget(
+                        name="Target_2",
+                        mode="MIN",
+                    ),
+                ],
+            )
 
-    # Scenario: invalid weights
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="DESIRABILITY",
-            weights=[1, 2, 3],
-            targets=two_targets,
-        )
+    @pytest.mark.xfail(reason="Attribute validation yet to be implemented")
+    def test_invalid_combination_function(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="DESIRABILITY",
+                combine_func="FALSE_STUFF",
+                targets=self.two_targets,
+            )
 
-    # Scenario: invalid weights
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="DESIRABILITY",
-            weights=[1, "ABC"],
-            targets=two_targets,
-        )
+    def test_wrong_number_of_weights(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="DESIRABILITY",
+                weights=[1, 2, 3],
+                targets=self.two_targets,
+            )
 
-    # Scenario: invalid weights
-    with pytest.raises(validation_errors):
-        Objective(
-            mode="DESIRABILITY",
-            weights="ABC",
-            targets=two_targets,
-        )
+    @pytest.mark.xfail(reason="Attribute validation yet to be implemented")
+    def test_non_numeric_weights(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="DESIRABILITY",
+                weights=[1, "ABC"],
+                targets=self.two_targets,
+            )
+
+    @pytest.mark.xfail(reason="Attribute validation yet to be implemented")
+    def test_wrong_weights_type(self):
+        with pytest.raises(ValueError):
+            Objective(
+                mode="DESIRABILITY",
+                weights="ABC",
+                targets=self.two_targets,
+            )
