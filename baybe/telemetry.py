ee2225af072084a07a208508063a063ace0ebdd6
Martin Fitzner <martin.fitzner@merckgroup.com>
2023-04-27 19:27:10 +0200

    Add telemetry state variables

--- baybe/telemetry.py
+++ baybe/telemetry.py
@@ -1,16 +1,18 @@
 """
 Telemetry  functionality for BayBE.
 """
+import getpass
+import hashlib
+import os
+import socket
+
 from opentelemetry._metrics import get_meter, set_meter_provider
 from opentelemetry.exporter.otlp.proto.grpc._metric_exporter import OTLPMetricExporter
 from opentelemetry.sdk._metrics import MeterProvider
-
 from opentelemetry.sdk._metrics.export import PeriodicExportingMetricReader
 from opentelemetry.sdk.resources import Resource
 
 resource = Resource.create({"service.name": "baybe.sdk", "service.namespace": "baybe"})
-
-
 reader = PeriodicExportingMetricReader(
     OTLPMetricExporter(
         "***REMOVED***"
@@ -23,53 +25,21 @@ set_meter_provider(provider)
 
 # Setup Global Metric Provider
 meter = get_meter("aws-otel", "1.0")
-
 # Setup Metric Components
 recommendation_counter = meter.create_counter(
     "recommendation.counter", description="Counts the number of recommendations jobs"
 )
 
 
-# Old Code temporary
-# from opentelemetry import propagate, trace
-# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-#
-# from opentelemetry.propagators.aws import AwsXRayPropagator
-#
-# from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
-# from opentelemetry.sdk.resources import Resource
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor
-#
-# propagate.set_global_textmap(AwsXRayPropagator())
-#
-# # OTLP Exporter is configured through environment variables:
-# # - OTEL_EXPORTER_OTLP_ENDPOINT
-# # - OTEL_EXPORTER_OTLP_CERTIFICATE
-# otlp_span_exporter = OTLPSpanExporter(
-#     "***REMOVED***.elb.eu-central-1.amazonaws.com:4317",
-#     True,
-# )
-# span_processor = BatchSpanProcessor(otlp_span_exporter)
-#
-# service_resource = Resource.create(
-#     {"service.name": "baybe.sdk", "service.namespace": "baybe"}
-# )
-#
-# trace.set_tracer_provider(
-#     TracerProvider(
-#         active_span_processor=span_processor,
-#         id_generator=AwsXRayIdGenerator(),
-#         resource=service_resource,
-#     )
-# )
-#
-# tracer = trace.get_tracer(__name__)
+# Generate a unique hash value for the current user based on the host name and
+# uppercase username, e.g.
+CALLER_ID = hashlib.sha256(
+    (socket.gethostname() + getpass.getuser().upper()).encode()
+).hexdigest()
+# alternative:
 
-# "BayBE __init__", attributes={"client.mac": hex(uuid.getnode())}
-
-# from .telemetry.setup_tracer import tracer
-# @tracer.start_as_current_span("_simulate_experiment")
-# with tracer.start_as_current_span(
-#         "run_doe_iteration", attributes={"simulation.k_iteration": k_iteration}
-# ):
+# Flag that tells whether telemetry is active or disabled through environment variables
+# This is decided at the time the module is loaded
+ENABLED = "BAYBE_TELEMETRY_ENABLED" in os.environ and os.environ[
+    "TELEMETRY_ENABLED"
+].lower() in ["false", "no", "off", "0"]
