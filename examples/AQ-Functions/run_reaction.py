67e75fff59ca521e7ef1e64495fcf393cb44df65
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-05-16 18:34:31 +0200

    Change AQ-Functions examples to object-based and user-friendly variant

--- examples/AQ-Functions/run_reaction.py
+++ examples/AQ-Functions/run_reaction.py
@@ -3,12 +3,56 @@ Run history simulation for a direct arylation where all possible combinations ha
 been measured
 """
 
+from os import getcwd
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from baybe.simulation import simulate_from_configs
+from baybe.core import BayBE
+from baybe.parameters import GenericSubstance, NumericDiscrete
+from baybe.searchspace import SearchSpace
+from baybe.simulation import simulate_scenarios
+from baybe.strategies.bayesian import SequentialGreedyRecommender
+from baybe.strategies.sampling import RandomRecommender
+from baybe.strategies.strategy import Strategy
+from baybe.targets import NumericalTarget, Objective
 
-lookup = pd.read_excel("../Reaction_DirectArylation/lookup.xlsx")
+# --------------------------------------------------------------------------------------
+# PART 1: CONFIGURATION OF THE PARAMETERS AND TEST
+# --------------------------------------------------------------------------------------
+# Capitalized  parameters are kept meant to be fixed in this example.
+# Non-capitalized are varied during this example.
+# To signifiy this, we initialize everything, even if some parameters are "marked" None
+# Example-specific details are listed under the NOTE below
+
+# PART 1.1: To be adjusted by the user
+batch_quantity = 1  # pylint: disable=C0103
+
+# PART 1.2: Derived and fixed hyperparameters one can adjust
+INITIAL_RECOMMENDER = RandomRecommender()
+ALLOW_REPEATED_RECOMMENDATIONS = False
+ALLOW_RECOMMENDING_ALREADY_MEASURED = False
+NUM_MEAS_WITHIN_TOLERANCE = True
+N_EXP_ITERATIONS = 10 + (batch_quantity == 1) * 20
+N_MC_ITERATIONS = 5 + (batch_quantity == 1) * 3
+file_name = f"AQ_Functions_IT{N_EXP_ITERATIONS}_MC{N_MC_ITERATIONS}_BQ{batch_quantity}"
+
+# PART 1.3: Things that should not be changed resp. are contained for completeness
+LOOKUP = pd.read_excel(f"{getcwd()}/examples/Reaction_DirectArylation/lookup.xlsx")
+recommender = None  # pylint: disable=C0103
+
+# NOTE Example specific details:
+#   - If batch_quantity is set to 1, all available acquisition functions are tested.
+#       - In this case, more Monte Carlo runs and iterations are done to get more
+#           meaningful results.
+#   - If a larger value is chosen, only those that allow batching, i.e., 'q'-variants
+#       of the acquisition functions are tested
+
+# --------------------------------------------------------------------------------------
+# PART 2: DATA
+# --------------------------------------------------------------------------------------
+# In this part, we define data that is relevant for the specific example like solvents,
+# ligands and so on.
 
 dict_solvent = {
     "DMAc": r"CC(N(C)C)=O",
@@ -41,126 +85,84 @@ dict_ligand = {
     "Me2PPh": r"CP(C)C1=CC=CC=C1",
 }
 
-config_dict_base = {
-    "project_name": "Direct Arylation",
-    "allow_repeated_recommendations": False,
-    "allow_recommending_already_measured": False,
-    "numerical_measurements_must_be_within_tolerance": True,
-    "parameters": [
-        {
-            "name": "Solvent",
-            "type": "SUBSTANCE",
-            "data": dict_solvent,
-            "encoding": "MORDRED",
-        },
-        {
-            "name": "Base",
-            "type": "SUBSTANCE",
-            "data": dict_base,
-            "encoding": "MORDRED",
-        },
-        {
-            "name": "Ligand",
-            "type": "SUBSTANCE",
-            "data": dict_ligand,
-            "encoding": "MORDRED",
-        },
-        {
-            "name": "Temp_C",
-            "type": "NUM_DISCRETE",
-            "values": [90, 105, 120],
-            "tolerance": 2,
-        },
-        {
-            "name": "Concentration",
-            "type": "NUM_DISCRETE",
-            "values": [0.057, 0.1, 0.153],
-            "tolerance": 0.005,
-        },
-    ],
-    "objective": {
-        "mode": "SINGLE",
-        "targets": [
-            {
-                "name": "yield",
-                "type": "NUM",
-                "mode": "MAX",
-            },
-        ],
-    },
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "UNRESTRICTED_RANKING",
-    },
+# TODO is there a more elegant solution here?
+
+generic_substance_dict = {
+    "Solvent": (dict_solvent, "MORDRED"),
+    "Base": (dict_base, "MORDRED"),
+    "Ligand": (dict_ligand, "MORDRED"),
 }
 
-config_dict_v1 = {
-    "project_name": "PM",
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "UNRESTRICTED_RANKING",
-        "initial_recommender_cls": "RANDOM",
-        "acquisition_function_cls": "PM",
-    },
+
+num_discrete_dict = {
+    "Temp_C": ([90, 105, 120], 2),
+    "Concentration": ([0.057, 0.1, 0.153], 0.005),
 }
 
-config_dict_v2 = {
-    "project_name": "PI",
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "UNRESTRICTED_RANKING",
-        "initial_recommender_cls": "RANDOM",
-        "acquisition_function_cls": "PI",
-    },
-}
+# --------------------------------------------------------------------------------------
+# PART 3: CONSTRUCTION OF BAYBE OBJECTS
+# --------------------------------------------------------------------------------------
+# In this part, we collect all the data and values from the first two parts and
+# construct the respective BayBE objects
 
-config_dict_v3 = {
-    "project_name": "EI",
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "UNRESTRICTED_RANKING",
-        "initial_recommender_cls": "RANDOM",
-        "acquisition_function_cls": "EI",
-    },
-}
+numeric_discrete_parameters = [
+    NumericDiscrete(name, values=val, tolerance=tol)
+    for name, [val, tol] in num_discrete_dict.items()
+]
 
-config_dict_v4 = {
-    "project_name": "UCB",
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "UNRESTRICTED_RANKING",
-        "initial_recommender_cls": "RANDOM",
-        "acquisition_function_cls": "UCB",
-    },
-}
+generic_substance_parameters = [
+    GenericSubstance(name, data=data, encoding=encoding)
+    for name, [data, encoding] in generic_substance_dict.items()
+]
 
-config_dict_v5 = {
-    "project_name": "Random",
-    "strategy": {
-        "surrogate_model_cls": "GP",
-        "recommender_cls": "RANDOM",
-        "initial_recommender_cls": "RANDOM",
-    },
-}
-
-results = simulate_from_configs(
-    config_base=config_dict_base,
-    lookup=lookup,
-    impute_mode="worst",
-    n_exp_iterations=20,
-    n_mc_iterations=5,
-    batch_quantity=3,
-    config_variants={
-        "Posterior Mean": config_dict_v1,
-        "Probability of Improvement": config_dict_v2,
-        "Expected Improvement": config_dict_v3,
-        "Upper Confidence Bound": config_dict_v4,
-        "Random": config_dict_v5,
-    },
+searchspace = SearchSpace.create(
+    parameters=generic_substance_parameters + numeric_discrete_parameters
 )
 
-print(results)
+objective = Objective(
+    mode="SINGLE", targets=[NumericalTarget(name="yield", mode="MAX")]
+)
 
-sns.lineplot(data=results, x="Num_Experiments", y="yield_CumBest", hue="Variant")
+acq_functions = (
+    ["qPI", "qEI", "qUCB"]
+    if batch_quantity > 1
+    else ["qPI", "qEI", "qUCB", "PM", "PI", "EI", "UCB"]
+)
+stratgy_dict = {}
+for acq_function in acq_functions:
+    stratgy_dict[acq_function] = Strategy(
+        initial_recommender=INITIAL_RECOMMENDER,
+        recommender=SequentialGreedyRecommender(acquisition_function_cls=acq_function),
+        allow_repeated_recommendations=ALLOW_REPEATED_RECOMMENDATIONS,
+        allow_recommending_already_measured=ALLOW_RECOMMENDING_ALREADY_MEASURED,
+    )
+
+# --------------------------------------------------------------------------------------
+# PART 4: PUTTING THINGS TOGETHER
+# --------------------------------------------------------------------------------------
+# For putting things together, we define all test scenarios and simulate them.
+# Afterwards, the results are plotted and saved in a file.
+
+scenarios = {
+    name: BayBE(
+        searchspace=searchspace,
+        strategy=strategy,
+        objective=objective,
+        numerical_measurements_must_be_within_tolerance=NUM_MEAS_WITHIN_TOLERANCE,
+    )
+    for name, strategy in stratgy_dict.items()
+}
+
+results = simulate_scenarios(
+    scenarios=scenarios,
+    lookup=LOOKUP,
+    n_exp_iterations=N_EXP_ITERATIONS,
+    n_mc_iterations=N_MC_ITERATIONS,
+    batch_quantity=batch_quantity,
+)
+
+sns.lineplot(
+    data=results, x="Num_Experiments", y="yield_CumBest", hue="Variant"
+).set_title(file_name)
 plt.gcf().set_size_inches(22, 8)
-plt.savefig("./run_reaction.png")
+plt.savefig(f"{getcwd()}/examples/Results/{file_name}.png")
