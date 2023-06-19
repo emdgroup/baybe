3979a47a2567411503a45ddcb33b921a1ea7f6cc
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-06-19 11:56:22 +0200

    Add wrapper functions for BoTorch test functions

--- examples/Analytic_Functions/test_functions.py
+++ examples/Analytic_Functions/test_functions.py
@@ -1,109 +1,179 @@
-"""A file containing functions that can be used for analytical tests."""
-# pylint: disable=invalid-name
-from abc import ABC, abstractmethod
+"""A collection of test functions.
 
-import numpy as np
-from attrs import define
-from baybe.utils.serialization import SerialMixin
+They wrap synthetic BoTorch test function to simplify using these for testing purposes.
+"""
 
+from abc import ABC
+from typing import List, Optional, Tuple
 
-@define
-class AnalyticTestFunction(ABC, SerialMixin):
-    """Abstract parent class for analytical test functions."""
-
-    dimension: int
-
-    def __call__(self, x: np.array) -> float:
-        raise NotImplementedError("A __call__ method needs to be implemented.")
-
-    @property
-    @abstractmethod
-    def bounds(self) -> np.ndarray:
-        """The bounds of the individual variables"""
-
-    @property
-    @abstractmethod
-    def optimum(self) -> float:
-        """The optimum value of the function."""
-
-    @property
-    @abstractmethod
-    def argmin(self) -> np.ndarray:
-        """The input for which the optimum value is obtained."""
+from botorch.test_functions import (
+    Ackley,
+    Branin,
+    Cosine8,
+    Hartmann,
+    Rastrigin,
+    Rosenbrock,
+    Shekel,
+    SyntheticTestFunction,
+)
+from torch import Tensor
 
 
-@define
-class Hartmann3(AnalyticTestFunction):
-    """Three-dimensional Hartmann function.
-    It has 4 local minima."""
-
-    dimension: int = 3
-
-    def __call__(self, x1: float, x2: float, x3: float) -> float:
-        x = np.array([x1, x2, x3])
-
-        alpha = np.array([1.0, 1.2, 3.0, 3.2])
-        A = np.array(
-            [
-                [3, 10, 30],
-                [0.1, 10, 35],
-                [3, 10, 30],
-                [0.1, 10, 35],
-            ]
-        )
-        P = 1e-4 * np.array(
-            [
-                [3689, 1170, 2673],
-                [4699, 4387, 7470],
-                [1091, 8732, 5547],
-                [381, 5743, 8828],
-            ]
-        )
-
-        outer = 0
-        for i in range(4):
-            inner = sum(A[i, j] * (x[j] - P[i, j]) ** 2 for j in range(3))
-            outer = outer + alpha[i] * np.exp(-inner)
-
-        y = -outer
-        return y
-
-    @property
-    def bounds(self) -> np.ndarray:
-        return np.array(((0, 1),) * 3)
-
-    @property
-    def optimum(self) -> float:
-        return -3.86278
-
-    @property
-    def argmin(self) -> np.ndarray:
-        return np.array([0.114614, 0.555649, 0.852547])
-
-
-@define
-class Rastrigin(AnalyticTestFunction):
+class AbstractTestFunction(ABC):
     """
-    General form of the rastrigin test function.
-    This function has a lot of local minima, but only one global minimum which is at
-    x=(0,...,0).
+    Wrapper class for handling BoTorch synthetic test functions.
+
+    Enables to simply create a function which can be called for evaluation. It also
+    ensures that it is safe to construct a test function that is only available for
+    fixed dimensions even if a dimension keyword is used.
+
+    Parameters
+    ----------
+    test_function: SyntheticTestFunction
+        The BoTorch SyntheticTestFunction that should be wrapped
+    bounds: List[Tuple[float, float]]
+        The bounds of the input variables. Have to be set upon defining an inherited
+        class.
+    dim: int, optional
+        The dimension of the test function. Also used as a flag to check whether the
+        test function has a fixed or variable dimension.
     """
 
-    def __call__(self, *X: float, A: int = 10) -> float:
-        delta = [x**2 - A * np.cos(2 * np.pi * x) for x in X]
-        y = A * len(X) + np.sum(delta)
-        return y
+    def __init__(
+        self,
+        test_function: SyntheticTestFunction,
+        bounds: List[Tuple[float, float]],
+        dim: Optional[int] = None,
+    ) -> None:
+        # Only if a dim keyword is provided, we attempt to use it for the construction
+        # of the actual test function
+        if dim:
+            self.test_function = test_function(dim=dim, bounds=bounds)
+        else:
+            self.test_function = test_function(bounds=bounds)
 
-    @property
-    def bounds(self) -> list:
-        return [
-            (-5.12, 5.12),
-        ] * self.dimension
+    def __call__(self, x: Tensor) -> float:
+        # Make it easier to evaluate a function.
+        return self.test_function.forward(x)
 
-    @property
-    def optimum(self) -> float:
-        return 0
+    def __getattr__(self, item):
+        # Identify the attributes of this function with the original BoTorch function.
+        return getattr(self.test_function, item)
 
-    @property
-    def argmin(self) -> np.array:
-        return np.array((0,) * self.dimension)
+
+class AckleyTestFunction(AbstractTestFunction):
+    """The Ackley test function.
+
+    It can be of arbitrary dimension dim and is bounded by [-32.768,32.768]^dim.
+    It has one minimizer for its global minimum at x=(0,...,0) with f(x)=0.
+    """
+
+    def __init__(self, dim=2) -> None:
+        super().__init__(
+            test_function=Ackley,
+            bounds=[
+                (-32.768, 32.768),
+            ]
+            * dim,
+            dim=dim,
+        )
+
+
+class BraninTestFunction(AbstractTestFunction):
+    """The Branin test function.
+
+    It is two dimensional and bounded by [-5, 10] x [0,10]
+    It has three minimizer for its global minimum at x1=(-pi, 12.275), x2=(pi,2,275) and
+    x3=(9,42478, 2,475) with f(xi)=0.397887.
+    """
+
+    def __init__(self, dim=None):
+        super().__init__(test_function=Branin, bounds=[(-5, 10), (0, 15)])
+
+
+class Cosine8TestFunction(AbstractTestFunction):
+    """The Cosine8 test function.
+
+    It is eight dimensional and bounded by [-1,1]^8.
+    It has one minimizer for its global minimum at x=(0,...,0) with f(x)=0.
+    """
+
+    def __init__(self, dim=None):
+        super().__init__(
+            test_function=Cosine8,
+            bounds=[
+                (-1, 1),
+            ]
+            * 8,
+        )
+
+
+class HartmannTestFunction(AbstractTestFunction):
+    """The Hartmann test function.
+
+    It is six dimensional and bounded by [0,1]^6.
+    It has 6 lcaol minima and one global minimum at
+    x = (0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573) with f(x)=-3.32237
+    """
+
+    def __init__(self, dim=None) -> None:
+        super().__init__(
+            test_function=Hartmann,
+            bounds=[
+                (0, 1),
+            ]
+            * 6,
+        )
+
+
+class RastriginTestFunction(AbstractTestFunction):
+    """The Rastrigin test function.
+
+    It can be of arbitrary dimension dim and is bounded by [-5.12, 5.12]^dim.
+    It has a lot of local minima but only one global minimum at x=(0,...,0) with f(x)=0.
+    """
+
+    def __init__(self, dim: int = 4):
+        super().__init__(
+            test_function=Rastrigin,
+            bounds=[
+                (-5.12, 5.12),
+            ]
+            * dim,
+            dim=dim,
+        )
+
+
+class RosenbrockTestFunction(AbstractTestFunction):
+    """The Rosenbrock test function.
+
+    It can be of arbitrary dimension and is bounded by [-5,10]^dim.
+    It has one local minimum at x=(1,...,1) with f(x)=0.
+    """
+
+    def __init__(self, dim=4):
+        super().__init__(
+            test_function=Rosenbrock,
+            bounds=[
+                (-5, 10),
+            ]
+            * dim,
+            dim=dim,
+        )
+
+
+class ShekelTestFunction(AbstractTestFunction):
+    """The Shekel test function.
+
+    It is four dimensional and bounded by [0,10]^4.
+    It has one global minimum at x=(4,4,4,4) with f(x)=-10.5363.
+    """
+
+    def __init__(self, dim=None):
+        super().__init__(
+            test_function=Shekel,
+            bounds=[
+                (0, 10),
+            ]
+            * 4,
+        )
