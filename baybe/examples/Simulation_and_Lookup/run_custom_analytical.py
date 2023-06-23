06398f611ac4b1d4c8432bdc49b621d6f58b7692
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-06-23 16:26:16 +0200

    Add example for simulation run with custom test function

--- baybe/examples/Simulation_and_Lookup/run_custom_analytical.py
+++ baybe/examples/Simulation_and_Lookup/run_custom_analytical.py
@@ -1,5 +1,5 @@
 """
-Run history simulation for a single target with a function as lookup.
+Run history simulation for a single target with a custom defined function as lookup.
 
 This example shows a full simulation loop. That is, we do not only perform one or two
 iterations like in the other examples but rather a full loop. We also store and display
@@ -19,7 +19,6 @@ from baybe.core import BayBE
 
 # Note that this import here might be problematic depending on your exact
 # setup and that you might need to make some adjustments to make it work!
-from baybe.examples.Analytic_Functions.test_functions import BayBEBotorchFunctionWrapper
 from baybe.parameters import NumericDiscrete
 from baybe.searchspace import SearchSpace
 from baybe.simulation import simulate_scenarios
@@ -27,35 +26,51 @@ from baybe.strategies.bayesian import SequentialGreedyRecommender
 from baybe.strategies.sampling import RandomRecommender
 from baybe.strategies.strategy import Strategy
 from baybe.targets import NumericalTarget, Objective
-from botorch.test_functions import Rastrigin
+
 
 # For the full simulation, we need to define some additional parameters.
 # These are the number of Monte Carlo runs and the number of experiments to be
 # conducted per Monte Carlo run.
 N_MC_ITERATIONS = 2
-N_EXP_ITERATIONS = 5
+N_EXP_ITERATIONS = 4
 
-# The basic setup for this experiment is identical to the example presented in the file
-# run_discrete_space.py within examples/Analytic_Functions, so we refer to this file
-# for more details on the setup.
-DIMENSION = 2
-TEST_FUNCTION = BayBEBotorchFunctionWrapper(test_function=Rastrigin, dim=DIMENSION)
-POINTS_PER_DIM = 15
+# Here, we now need to define our custom function first. The function should accept an
+# arbitrary or fixed amount of floats as input and return either a single float or
+# a tuple of floats.
+# NOTE It is assumed that the analytical test function does only perform a single
+# calculation, i.e., it is assumed to work in a non-batched-way!
+# NOTE The case of using an analytical test function for multi-target optimization has
+# not yet been fully tested.
+
+
+# We implements a simple sum of squares function with a single output.
+def sum_of_squares(*x: float) -> float:
+    """
+    Calculates the sum of squares.
+    """
+    res = 0
+    for y in x:
+        res += y**2
+    return res
+
+
+# For our actual experiment, we need to specify the number of dimension that we want
+# to use as this is necessary to know for the creation of the parameters. The same is
+# true for the bounds of the parameters which should be provided as a list of
+# two-dimensional tuples.
+DIMENSION = 4
+BOUNDS = [(-2, 2), (-2, 2), (-2, 2), (-2, 2)]
 
 # As we expect it to be the most common use case, we construct a purely discrete space
-# here. We refer to the examples within examples/Analytic_Functions for details on
+# here. We refer to the examples within examples/SearchSpaces for details on
 # how to change this code for continuous or hybrid spaces.
 parameters = [
     NumericDiscrete(
         name=f"x_{k+1}",
-        values=list(
-            np.linspace(
-                TEST_FUNCTION.bounds[0, k], TEST_FUNCTION.bounds[1, k], POINTS_PER_DIM
-            )
-        ),
+        values=list(np.linspace(*BOUNDS[k], 15)),
         tolerance=0.01,
     )
-    for k in range(TEST_FUNCTION.dim)
+    for k in range(DIMENSION)
 ]
 
 # Construct searchspace and objective.
@@ -101,7 +116,7 @@ results = simulate_scenarios(
     batch_quantity=3,
     n_exp_iterations=N_EXP_ITERATIONS,
     n_mc_iterations=N_MC_ITERATIONS,
-    lookup=TEST_FUNCTION,
+    lookup=sum_of_squares,
 )
 
 # The following lines plot the results and save the plot in run_analytical.png
