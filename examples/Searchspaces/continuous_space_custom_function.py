16ca9bf1472e120c54c5926232ccf5d231c3ecbd
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-06-26 15:54:20 +0200

    Add examples for using custom analytic function

--- examples/Searchspaces/continuous_space_custom_function.py
+++ examples/Searchspaces/continuous_space_custom_function.py
@@ -1,7 +1,5 @@
 """
-Example for using synthetic test functions in continuous spaces.
-A synthetic test function can either be a wrapped BoTorch test function or a custom
-test function.
+Example for using custom synthetic test functions in continuous spaces.
 """
 
 from baybe.core import BayBE
@@ -9,43 +7,12 @@ from baybe.parameters import NumericContinuous
 from baybe.searchspace import SearchSpace
 from baybe.targets import NumericalTarget, Objective
 
-from baybe.utils.botorch_wrapper import BayBEBotorchFunctionWrapper
-
-# Import the desired test function from botorch here
-from botorch.test_functions import Rastrigin
-
-# CASE 1: USING A BOTORCH TEST FUNCTION
-
-# Here, you can choose the dimension of the test function and create the actual test
-# function. Note that some test functions are only defined for specific dimensions.
-# If the dimension you provide is not available for the given test function, a warning
-# will be printed and one of the available dimensions is used.
-# For details on constructing the baybe object, we refer to the basic example file.
-DIMENSION = 5
-TEST_FUNCTION_BOTORCH = BayBEBotorchFunctionWrapper(
-    test_function=Rastrigin, dim=DIMENSION
-)
-
-# Since this is the continuous test, we only construct NumericContinuous parameters.
-# We use that data of the test function to deduce bounds and number of parameters.
-parameters_botorch = [
-    NumericContinuous(
-        name=f"x_{k+1}",
-        bounds=(TEST_FUNCTION_BOTORCH.bounds[0, k], TEST_FUNCTION_BOTORCH.bounds[1, k]),
-    )
-    for k in range(TEST_FUNCTION_BOTORCH.dim)
-]
-
-# CASE 2: USING A CUSTOM TEST FUNCTION
-
 
 # Here, we now need to define our custom function first. The function should accept an
 # arbitrary or fixed amount of floats as input and return either a single float or
 # a tuple of floats.
 # NOTE It is assumed that the analytical test function does only perform a single
 # calculation, i.e., it is assumed to work in a non-batched-way!
-# NOTE The case of using an analytical test function for multi-target optimization has
-# not yet been fully tested.
 # We implements a simple sum of squares function with a single output.
 def sum_of_squares(*x: float) -> float:
     """
@@ -62,12 +29,12 @@ def sum_of_squares(*x: float) -> float:
 # true for the bounds of the parameters which should be provided as a list of
 # two-dimensional tuples.
 
-TEST_FUNCTION_CUSTOM = sum_of_squares
+TEST_FUNCTION = sum_of_squares
 
 DIMENSION = 4
 BOUNDS = [(-2, 2), (-2, 2), (-2, 2), (-2, 2)]
 
-parameters_custom = [
+parameters = [
     NumericContinuous(
         name=f"x_{k+1}",
         bounds=BOUNDS[k],
@@ -77,9 +44,7 @@ parameters_custom = [
 
 
 # Construct searchspace, objective and BayBE object.
-# Here you need to choose the correct parameters, depending on what you want to test!
-searchspace = SearchSpace.create(parameters=parameters_custom)
-# searchspace = SearchSpace.create(parameters=parameters_botorch)
+searchspace = SearchSpace.create(parameters=parameters)
 
 objective = Objective(
     mode="SINGLE", targets=[NumericalTarget(name="Target", mode="MIN")]
@@ -89,12 +54,6 @@ baybe_obj = BayBE(
     searchspace=searchspace,
     objective=objective,
 )
-
-# Here, we now choose the actual test function.
-# Make sure that this matches your previous choice!
-# TEST_FUNCTION = TEST_FUNCTION_CUSTOM
-TEST_FUNCTION = TEST_FUNCTION_BOTORCH
-
 
 # Get a recommendation for a fixed batched quantity
 BATCH_QUANTITY = 3
