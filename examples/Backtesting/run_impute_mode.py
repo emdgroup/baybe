342f11112f2c75ce367926c78fe3d932dad15b7d
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-07-28 18:28:15 +0200

    Change comments for .md conversion in run_impute_mode.py

--- examples/Backtesting/run_impute_mode.py
+++ examples/Backtesting/run_impute_mode.py
@@ -1,8 +1,15 @@
+### Example for full simulation loop using a table-based lookup mechanism with incomplete data
+
 """
-Run history simulation for a direct arylation where not all possible combinations have
-been measured previously. This uses the lookup mechanism that allows us to access
-information about previously conducted experiments from .xlsx-files.
+This example shows a simulation for a direct arylation where not all combinations have been measured
+This allows us to access information about previously conducted experiments from .xlsx-files.
 """
+
+# This examples assumes some basic familiarity with using BayBE and the lookup mechanism.
+# We refer to [`baybe_object`](./../Basics/baybe_object.md) for a more  basic example resp.
+# to [`run_full_lookup`](./run_full_lookup.md) for details on the lookup mechanism.
+
+#### Necessary imports for this example
 
 import matplotlib.pyplot as plt
 
@@ -17,32 +24,38 @@ from baybe.strategies.sampling import RandomRecommender
 from baybe.strategies.strategy import Strategy
 from baybe.targets import NumericalTarget, Objective
 
-# We read the information about the conducted experiments from a .xlsx-file and save it
-# as a pandas DataFrame.
-# NOTE Depending on your system and settings, you might need to slightly adjust the
-# following path as this is relevant to the folder in which you execute the 'python'
-# call. This path assumes that this call is made from the main BayBE folder.
+### Parameters for a full simulation loop
 
-lookup = pd.read_excel("examples/Backtesting/lookup_withmissing.xlsx")
+# For the full simulation, we need to define some additional parameters.
+# These are the number of Monte Carlo runs and the number of experiments to be conducted per run.
+N_MC_ITERATIONS = 2
+N_EXP_ITERATIONS = 5
 
-# As usual, we set up some experiment. Note that we now need to ensure that the names
-# fit the names in the provided .xlsx file!
+### Lookup functionality and data creation
+
+# See [`run_full_lookup`](./run_full_lookup.md) for details.
+try:
+    lookup = pd.read_excel("./lookup_withmissing.xlsx")
+except FileNotFoundError:
+    try:
+        lookup = pd.read_excel("examples/Backtesting/lookup_withmissing.xlsx")
+    except FileNotFoundError as e:
+        print(e)
+
+# As usual, we set up some experiment.
+# Note that we now need to ensure that the names fit the names in the provided .xlsx file!
 dict_solvent = {
     "DMAc": r"CC(N(C)C)=O",
     "Butyornitrile": r"CCCC#N",
     "Butyl Ester": r"CCCCOC(C)=O",
     "p-Xylene": r"CC1=CC=C(C)C=C1",
 }
-solvent = GenericSubstance(name="Solvent", data=dict_solvent, encoding="MORDRED")
-
 dict_base = {
     "Potassium acetate": r"O=C([O-])C.[K+]",
     "Potassium pivalate": r"O=C([O-])C(C)(C)C.[K+]",
     "Cesium acetate": r"O=C([O-])C.[Cs+]",
     "Cesium pivalate": r"O=C([O-])C(C)(C)C.[Cs+]",
 }
-base = GenericSubstance(name="Base", data=dict_base, encoding="MORDRED")
-
 dict_ligand = {
     "BrettPhos": r"CC(C)C1=CC(C(C)C)=C(C(C(C)C)=C1)C2=C(P(C3CCCCC3)C4CCCCC4)C(OC)="
     "CC=C2OC",
@@ -59,6 +72,13 @@ dict_ligand = {
     "SCHEMBL15068049": r"C[C@]1(O2)O[C@](C[C@]2(C)P3C4=CC=CC=C4)(C)O[C@]3(C)C1",
     "Me2PPh": r"CP(C)C1=CC=CC=C1",
 }
+
+### Creating the searchspace and the objective
+
+# Here, we create the parameter objects, the searchspace and the objective.
+
+solvent = GenericSubstance(name="Solvent", data=dict_solvent, encoding="MORDRED")
+base = GenericSubstance(name="Base", data=dict_base, encoding="MORDRED")
 ligand = GenericSubstance(name="Ligand", data=dict_ligand, encoding="MORDRED")
 temperature = NumericDiscrete(name="Temp_C", values=[90, 105, 120], tolerance=2)
 concentration = NumericDiscrete(
@@ -67,14 +87,16 @@ concentration = NumericDiscrete(
 
 parameters = [solvent, base, ligand, temperature, concentration]
 
-# Construct searchspace and objective. Note that the objective is maximization!
 searchspace = SearchSpace.from_product(parameters=parameters)
 objective = Objective(
     mode="SINGLE", targets=[NumericalTarget(name="yield", mode="MAX")]
 )
 
-# Create two BayBE objects: One using the default recommender and one making random
-# recommendations.
+### Constructing BayBE objects for the simulation loop
+
+# In this example, we create two BayBE objects.
+# One uses the default recommender and the other one makes random recommendations.
+
 baybe = BayBE(searchspace=searchspace, objective=objective)
 baybe_rand = BayBE(
     searchspace=searchspace,
@@ -82,29 +104,26 @@ baybe_rand = BayBE(
     objective=objective,
 )
 
-# We can now use the simulate_scenarios function from simulation.py to simulate a
-# full experiment. Note that this function enables to run multiple scenarios one after
-# another by a single function call, which is why we need to define a dictionary
-# mapping names for the scenarios to actual BayBE objects
+# We can now use the `simulate_scenarios` function to simulate a full experiment.
+# Note that this function enables to run multiple scenarios by a single function call.
+# For this, it is necessary to define a dictionary mapping scenario names to BayBE objects.
 scenarios = {"Test_Scenario": baybe, "Random": baybe_rand}
 
-# This is the call to the actual function.
-# Since the lookup table does not provide a full overview, we need to tell the function
-# how to deal with missing entries. This is done via the 'impute_mode' keyword.
+# The lookup table does not contain data for all possible combination of parameters.
+# Consequently, we need to inform the function how to deal with missing entries.
+# This is done via the `impute_mode` keyword.
 # The following options are available:
-#   * 'error': an error will be thrown
-#   * 'worst': imputation using the worst available value for each target
-#   * 'best': imputation using the best available value for each target
-#   * 'mean': imputation using mean value for each target
-#   * 'random': a random row will be used as lookup
-#   * 'ignore': the search space is stripped before recommendations are made
+#   * `"error"`: an error will be thrown
+#   * `"worst"`: imputation using the worst available value for each target
+#   * `"best"`: imputation using the best available value for each target
+#   * `"mean"`: imputation using mean value for each target
+#   * `"random"`: a random row will be used as lookup
+#   * `"ignore"`: the search space is stripped before recommendations are made
 #       so that unmeasured experiments will not be recommended
-BATCH_QUANTITY = 2
-N_EXP_ITERATIONS = 5
-N_MC_ITERATIONS = 3
+
 results = simulate_scenarios(
     scenarios=scenarios,
-    batch_quantity=BATCH_QUANTITY,
+    batch_quantity=3,
     n_exp_iterations=N_EXP_ITERATIONS,
     n_mc_iterations=N_MC_ITERATIONS,
     lookup=lookup,
@@ -116,9 +135,7 @@ max_yield = lookup["yield"].max()
 sns.lineplot(
     data=results, x="Num_Experiments", y="yield_CumBest", hue="Variant", marker="x"
 )
-plt.plot(
-    [BATCH_QUANTITY, BATCH_QUANTITY * N_EXP_ITERATIONS], [max_yield, max_yield], "--r"
-)
+plt.plot([3, 3 * N_EXP_ITERATIONS], [max_yield, max_yield], "--r")
 plt.legend(loc="lower right")
 plt.gcf().set_size_inches(20, 8)
 plt.savefig("./run_impute_mode.png")
