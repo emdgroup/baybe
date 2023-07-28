3fd8a22caf4ff2b069397df6083cad31ee670ae3
Emeline Sola < >
2023-07-28 13:29:44 +0200

    Add description lines in head of .md file

--- examples/py_to_markdown.py
+++ examples/py_to_markdown.py
@@ -11,21 +11,46 @@ import os
 DIRECTORY = "Basics"
 FILE = "baybe_object.py"
 FILENAME = "baybe_object"
-
+ORDER = 1
 
 # Create the Markdown file:
 
 # 1. Convert the file to jupyter notebook
-os.system(rf"p2j examples\ {DIRECTORY} \ {FILE}")
+os.system(rf"p2j examples\{DIRECTORY}\{FILE}")
 NOTEBOOK = FILENAME + ".ipynb"
 
 # 2. Execute the notebook
 os.system(
-    rf"jupyter nbconvert --execute --to notebook --inplace examples\ {DIRECTORY} \ {NOTEBOOK}"
+    rf"jupyter nbconvert --execute --to notebook --inplace examples\{DIRECTORY}\{NOTEBOOK}"
 )
+MARKDOWN = FILENAME + ".md"
 
 # 3. Convert the notebook to markdown
-os.system(rf"jupyter nbconvert --to markdown examples\ {DIRECTORY} \ {NOTEBOOK}")
+os.system(rf"jupyter nbconvert --to markdown examples\{DIRECTORY}\{NOTEBOOK}")
 
 # 4. Delete the no-longer-necessary notebook
-os.remove(rf"examples\ {DIRECTORY} \ {NOTEBOOK}")
+os.remove(rf"examples\{DIRECTORY}\{NOTEBOOK}")
+
+# %. Add lines at the top of the .md file
+
+LINES_TO_ADD = (
+    "---"
+    + "\neleventyNavigation:"
+    + "\n  key: "
+    + FILENAME
+    + "\n  order: "
+    + str(ORDER)
+    + "\n  parent: Examples/"
+    + DIRECTORY
+    + "\nlayout: layout.njk"
+    + "\npermalink: baybe/sdk/examples/"
+    + DIRECTORY
+    + "\ntitle: "
+    + FILENAME
+    + "\n---\n\n "
+)
+with open(rf"examples\{DIRECTORY}\{MARKDOWN}", "r+", encoding="UTF-8") as f:
+    content = f.read()
+    f.seek(0)
+    f.write(LINES_TO_ADD)
+    f.write(content)
