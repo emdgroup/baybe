57c45edb0221ceba55019a8d3cf9a8c9c2d343d1
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-07-31 17:00:04 +0200

    Change comments for .md conversion in hybrid_space.py

--- examples/Searchspaces/hybrid_space.py
+++ examples/Searchspaces/hybrid_space.py
@@ -1,40 +1,45 @@
-"""
-Example for using the synthetic test functions in hybrid spaces.
-All test functions that are available in BoTorch are also available here and wrapped
-via the BayBEBotorchFunctionWrapper.
+### Example for using synthetic test functions in hybrid spaces
 
-For an example on how to use a custom function instead of a wrapped one, we refer to the
-continuous_space example.
 """
+This examples shows how to optimize a custom test function in a hybrid searchspace.
+It focuses on the searchspace-related aspects and not on the custom test function.
+"""
+
+# This example assumes some basic familiarity with using BayBE and synthetic test functions.
+# We thus refer to [`baybe_object`](./../Basics/baybe_object.md) for a basic example.
+# For details on using synthetic test functions, we refer to other examples in this directory.
+
+#### Necessary imports for this example
 
 import numpy as np
 
 from baybe.core import BayBE
 from baybe.parameters import NumericContinuous, NumericDiscrete
 from baybe.searchspace import SearchSpace
-from baybe.strategies.bayesian import NaiveHybridRecommender
+from baybe.strategies.bayesian import (
+    NaiveHybridRecommender,
+    SequentialGreedyRecommender,
+)
 from baybe.strategies.strategy import Strategy
 from baybe.targets import NumericalTarget, Objective
 
 from baybe.utils.botorch_wrapper import botorch_function_wrapper
 
-# Import the desired test function from botorch here
 from botorch.test_functions import Rastrigin
 
-# Here, you can choose the dimension  and the actual the test function.
-# All BoTorch test functions can be used.
-# Note that some test functions are only defined for specific dimensions.
-# If the dimension you provide is not available for the given test function, a warning
-# will be printed and the dimension is adjusted.
-# For details on constructing the BayBE object, we refer to the basic example file.
+### Defining the test function and the hybrid dimensions
+
+# See [`discrete_space`](./discrete_space.md) for details on the test function.
+
 DIMENSION = 6
 
-# DISC_INDICES and CONT_INDICES together should contain the integers 0,1,...,DIMENSION-1
-# NOTE Although this is checked also in this file, you should configure these indices
+# This examples uses two lists containing the discrete resp. discrete dimensions.
+# Together, these should contain the integers `0,1,...,DIMENSION-1`.
+# Although this is checked also in this file, you should configure these indices manually
 # manually here and verify that the experiment you set up is configured correctly.
 # In particular, if the function that you want to use is only available for a fixed
 # dimension, then these will be overwritten by distributing the first half of the
-# dimension to DISC_INDICES and the remaining ones to CONT_INDICES
+# dimension to `DISC_INDICES` and the remaining ones to `CONT_INDICES`.
 DISC_INDICES = [0, 1, 2]
 CONT_INDICES = [3, 4, 5]
 
@@ -58,25 +63,25 @@ else:
     DISC_INDICES = list(range(0, (DIMENSION + 1) // 2))
     CONT_INDICES = list(range((DIMENSION + 1) // 2, DIMENSION))
 
-# Get the bounds of the variables as they are set by BoTorch
-BOUNDS = TestFunction.bounds
-# Create the wrapped function itself.
-WRAPPED_FUNCTION = botorch_function_wrapper(test_function=TestFunction)
-
-POINTS_PER_DIM = 3
-
-# This if-statement check whether the union of the given index sets yields indices
-# matching the dimension of the test function. If this test fails, then either the
-# intersection between the index sets is not empty or the test function has another
-# dimension. Note that this might in particular happen for test functions that ignore
-# the dim keyword!
+# This check verifies if the union of the given index sets yields indices matching `DIMENSION`.
+# If this fails, then either the intersection between the index sets is not empty or the test
+# function has another dimension.
+# Note that this might in particular happen for test functions that ignore the `dim` keyword!
 if set(CONT_INDICES + DISC_INDICES) != set(range(DIMENSION)):
     raise ValueError(
         "Either the intersection between CONT_IND and DISC_IND is not empty or your "
         "indices do not match."
     )
 
-# Construct the continuous parameters as NumericContinuous parameters
+BOUNDS = TestFunction.bounds
+WRAPPED_FUNCTION = botorch_function_wrapper(test_function=TestFunction)
+
+### Constructing the hybrid searchspace
+
+# The following parameter decides how many points each discrete dimension should have.
+POINTS_PER_DIM = 3
+
+# Construct the continuous parameters as `NumericContinuous` parameters.
 cont_parameters = [
     NumericContinuous(
         name=f"x_{k+1}",
@@ -85,7 +90,7 @@ cont_parameters = [
     for k in CONT_INDICES
 ]
 
-# Construct the discrete parameters as NumericDiscrete parameters
+# Construct the discrete parameters as `NumericDiscrete` parameters.
 disc_parameters = [
     NumericDiscrete(
         name=f"x_{k+1}",
@@ -95,40 +100,48 @@ disc_parameters = [
     for k in DISC_INDICES
 ]
 
-# Construct searchspace, objective and BayBE object.
 searchspace = SearchSpace.from_product(parameters=disc_parameters + cont_parameters)
-
 objective = Objective(
     mode="SINGLE", targets=[NumericalTarget(name="Target", mode="MIN")]
 )
 
-# Here, we explicitly create a strategy object to use the NaiveHybridRecommender.
-# Using the keywords disc_recommender and cont_recommender, it can be chosen which
-# individual recommenders should then be used for the corresponding subspaces.
-# We use the default choices, which is the SequentialGreedy.
+### Constructing hybrid recommenders
 
-hybrid_recommender = NaiveHybridRecommender()
+# Here, we explicitly create a strategy object to use the `NaiveHybridRecommender`.
+# The keywords `disc_recommender` and `cont_recommender` can be used to select different
+# recommenders for the corresponding subspaces.
+# We use the default choices, which is the `SequentialGreedyRecommender`.
+
+hybrid_recommender = NaiveHybridRecommender(
+    cont_recommender=SequentialGreedyRecommender(),
+    disc_recommender=SequentialGreedyRecommender(),
+)
+
+hybrid_strategy = Strategy(recommender=hybrid_recommender)
+
+## Constructing the BayBE object and performing a recommendation
 
 baybe_obj = BayBE(
     searchspace=searchspace,
     objective=objective,
-    strategy=Strategy(recommender=hybrid_recommender),
+    strategy=hybrid_strategy,
 )
 
-
-# Get a recommendation for a fixed batched quantity
+# Get a recommendation for a fixed batched quantity.
 BATCH_QUANTITY = 3
 recommendation = baybe_obj.recommend(batch_quantity=BATCH_QUANTITY)
-# Evaluate the test function. Note that we need iterate through the rows of the
-# recommendation and that we need to interpret the row as a list.
+
+# Evaluate the test function.
+# Note that we need iterate through the rows of the recommendation.
+# Furthermore, we need to interpret the row as a list.
 target_values = []
 for index, row in recommendation.iterrows():
     target_values.append(WRAPPED_FUNCTION(*row.to_list()))
 
-# We add an additional column with the calculated target values...
+# We add an additional column with the calculated target values.
 recommendation["Target"] = target_values
 
-# ... and inform the BayBE object about our measurement.
+# Here, we inform the BayBE object about our measurement.
 baybe_obj.add_measurements(recommendation)
 print("\n\nRecommended experiments with measured values: ")
 print(recommendation)
