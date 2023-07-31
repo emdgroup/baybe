c97629fb7287308dcb44d23f5912af6c0caae804
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-07-31 16:26:22 +0200

    Use furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 # -- Options for intersphinx extension ---------------------------------------
