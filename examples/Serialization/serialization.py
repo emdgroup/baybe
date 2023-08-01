4bcc465c0d5d0b64308f7113410fdff2c81df5b0
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2023-08-01 10:08:34 +0200

    Change comments for .md conversion in serialization.py

--- examples/Serialization/serialization.py
+++ examples/Serialization/serialization.py
@@ -1,13 +1,14 @@
-"""Simple serialization example.
+### Example for the serialization of a BayBE object
 
-This example shows the creation of a BayBE object, how to serialize it and also
-de-serialize it and shows that the "original" and "new" objects behave the same.
-It also shows an additional way of creating BayBE objects via config files and
-how to validate these.
-
-Note that this example does not explain the basics of object creation, we refer to the
-basics example for explanations on this.
 """
+This example shows how to serialize and also de-serialize a BayBE object.
+It demonstrates and shows that the "original" and "new" objects behave the same.
+"""
+
+# This example assumes some basic familiarity with using BayBE.
+# We thus refer to [`baybe_object`](./../Basics/baybe_object.md) for a basic example.
+
+#### Necessary imports
 
 import numpy as np
 
@@ -19,7 +20,8 @@ from baybe.strategies.sampling import FPSRecommender
 from baybe.strategies.strategy import Strategy
 from baybe.targets import NumericalTarget, Objective
 
-# We start by defining the parameters of the experiment.
+### Experiment setup
+
 parameters = [
     Categorical(
         name="Granularity",
@@ -47,41 +49,46 @@ parameters = [
     ),
 ]
 
-# Define a single target
-targets = [NumericalTarget(name="Yield", mode="MAX")]
+### Creating the BayBE object
 
-# Create the DOE object
 baybe_orig = BayBE(
     searchspace=SearchSpace.from_product(parameters=parameters, constraints=None),
-    objective=Objective(mode="SINGLE", targets=targets),
+    objective=Objective(
+        mode="SINGLE", targets=[NumericalTarget(name="Yield", mode="MAX")]
+    ),
     strategy=Strategy(
         recommender=SequentialGreedyRecommender(),
         initial_recommender=FPSRecommender(),
     ),
 )
+
+### Serialization and de-serialization
+
 # We begin by printing the original BayBE object
 print(f"{'#'*30} Original object {'#'*30}")
 print(baybe_orig, end="\n" * 3)
 
-# We next serialize the BayBE object to JSON
-# This yields a JSON representation that we also print here for convenience.
+# We next serialize the BayBE object to JSON.
+# This yields a JSON representation  in string format.
+# Since it is rather complex, we do not print this string here.
 string = baybe_orig.to_json()
 print(f"{'#'*30} JSON string {'#'*30}")
-print(string, end="\n" * 3)
 
-# Deserialize the JSON string back to an object
+
+# Deserialize the JSON string back to an object.
 print(f"{'#'*30} Deserialized object {'#'*30}")
 baybe_recreate = BayBE.from_json(string)
 print(baybe_recreate, end="\n" * 3)
 
-# Verify that both objects are equal
+# Verify that both objects are equal.
 assert baybe_orig == baybe_recreate
 print("Passed basic assertion check!")
+
+### Comparing recommendations in both objects
 
 # To further show how serialization affects working with BayBE objects, we will now
 # create and compare some recommendations in both bayBE objects.
 
-# We now do recommendations for both objects and print them
 recommendation_orig = baybe_orig.recommend(batch_quantity=2)
 recommendation_recreate = baybe_recreate.recommend(batch_quantity=2)
 
