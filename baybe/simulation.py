d231b86b055e30ea819ee4e35ac87b3b44f54c40
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-09-04 16:21:05 +0200

    Extend simulation.py module docstring

--- baybe/simulation.py
+++ baybe/simulation.py
@@ -1,5 +1,19 @@
 """
-Provides functions to simulate a Bayesian DOE with BayBE given a lookup.
+Functionality to "simulate" Bayesian DOE given a lookup mechanism.
+
+The term "simulation" can have two slightly different interpretations, depending on the
+applied context:
+
+*   It can refer to "backtesting" a particular DOE strategy on a fixed (finite)
+    dataset. In this context, "simulation" means investigating what experimental
+    trajectory we would have observed if we had applied the strategy in a certain
+    defined context and restricted the possible parameter configurations to those
+    contained in the dataset.
+
+*   It can refer to the simulation of an actual DOE loop (i.e. recommending experiments
+    and retrieving the corresponding measurements) where the loop closure is realized
+    in the form of a callable (black-box) function that can be queried during the
+    optimization to provide target values.
 """
 
 from __future__ import annotations
