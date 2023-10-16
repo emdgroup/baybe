dea90b0c74154dfc8454d3d50c8e5efaeebf88c9
Alex Lee <alex.lee@emdgroup.com>
2023-10-16 19:54:20 +0000

    Add tests for custom surrogate architectures

--- tests/test_custom_surrogate.py
+++ tests/test_custom_surrogate.py
@@ -1,7 +1,7 @@
 """Tests for custom surrogate models."""
 
 import pytest
-from baybe.surrogate import CustomONNXSurrogate
+from baybe.surrogate import CustomONNXSurrogate, register_custom_architecture
 from baybe.utils.dataframe import add_fake_results
 
 
@@ -41,3 +41,53 @@ def test_invalid_onnx_str(baybe):
 
     with pytest.raises(ValueError):
         baybe.recommend()
+
+
+def test_validate_architectures():
+    """Test architecture class validation."""
+    # Scenario: Empty Class
+    with pytest.raises(AssertionError) as excinfo:
+        register_custom_architecture()(type("EmptyArch"))
+        assert "must exist" in excinfo.value.message
+
+    # Scenario: Class with just `_fit`
+    with pytest.raises(AssertionError):
+        register_custom_architecture()(type("PartialArch", (), {"_fit": True}))
+        assert "must exist" in excinfo.value.message
+
+    # Scenario: Class with `_fit` and `_posterior` but not methods
+    with pytest.raises(AssertionError):
+        register_custom_architecture()(
+            type("PartialArch", (), {"_fit": True, "_posterior": True})
+        )
+        assert "must be methods" in excinfo.value.message
+
+    # Scenario: Class with invalid `_fit` and `_posterior` methods
+    def _invalid_func(invalid_param1, invalid_param2=1):
+        return invalid_param1 + invalid_param2
+
+    with pytest.raises(AssertionError):
+        register_custom_architecture()(
+            type(
+                "InvalidArch", (), {"_fit": _invalid_func, "_posterior": _invalid_func}
+            )
+        )
+        assert "Invalid Arguments" in excinfo.value.message
+
+    # Scenario: Class with valid `_fit` but invalid `_posterior` methods
+    def _valid_fit(self, searchspace, train_x, train_y):
+        return self and searchspace and train_x and train_y
+
+    with pytest.raises(AssertionError):
+        register_custom_architecture()(
+            type("InvalidArch", (), {"_fit": _valid_fit, "_posterior": _invalid_func})
+        )
+        assert "Invalid Arguments" in excinfo.value.message
+
+    # Scenario: Both Valid
+    def _valid_posterior(self, candidates):
+        return self and candidates
+
+    register_custom_architecture()(
+        type("ValidArch", (), {"_fit": _valid_fit, "_posterior": _valid_posterior})
+    )
