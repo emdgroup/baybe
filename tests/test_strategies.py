bc9b7ca3c3b1e3867d1c9a5dde59a18d747f2819
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-11-11 20:47:44 +0100

    Add test for TwoPhaseStrategy

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,7 +1,10 @@
 """Tests for the strategies package."""
 
 from contextlib import nullcontext
+from typing import Tuple
 
+import numpy as np
+import pandas as pd
 import pytest
 
 from baybe.exceptions import NoRecommendersLeftError
@@ -13,17 +16,49 @@ from baybe.recommenders import (
 )
 from baybe.searchspace import SearchSpace
 from baybe.strategies.base import Strategy
-from baybe.strategies.composite import SequentialStrategy, StreamingSequentialStrategy
+from baybe.strategies.composite import (
+    SequentialStrategy,
+    StreamingSequentialStrategy,
+    TwoPhaseStrategy,
+)
 
 RECOMMENDERS = [RandomRecommender(), FPSRecommender(), SequentialGreedyRecommender()]
 
 
+def get_dummy_training_data(length: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Create column-less input and target dataframes of specified length."""
+    df = pd.DataFrame(np.empty((length, 0)))
+    return df, df
+
+
+@pytest.fixture(name="searchspace")
+def get_dummy_searchspace() -> SearchSpace:
+    """Create a dummy searchspace whose actual content is irrelevant."""
+    parameters = [NumericalDiscreteParameter(name="test", values=[0, 1])]
+    return SearchSpace.from_product(parameters)
+
+
+def test_twophase_strategy(searchspace):
+    """The strategy switches the recommender at the requested point."""
+    initial_recommender = RandomRecommender()
+    recommender = RandomRecommender()
+    switch_after = 3
+    strategy = TwoPhaseStrategy(
+        initial_recommender=initial_recommender,
+        recommender=recommender,
+        switch_after=switch_after,
+    )
+    for size in range(6):
+        train_x, train_y = get_dummy_training_data(size)
+        rec = strategy.select_recommender(searchspace, train_x=train_x, train_y=train_y)
+        target = initial_recommender if size < switch_after else recommender
+        assert rec is target
+
+
 @pytest.mark.parametrize("reuse_last", [False, True])
 @pytest.mark.parametrize("recommenders", [RECOMMENDERS])
-def test_sequential_strategy(recommenders, reuse_last):
+def test_sequential_strategy(searchspace, recommenders, reuse_last):
     """The strategy should provide its recommenders in the right order."""
-    parameters = [NumericalDiscreteParameter(name="test", values=[0, 1])]
-    searchspace = SearchSpace.from_product(parameters)
     strategy = SequentialStrategy(recommenders=recommenders, reuse_last=reuse_last)
 
     # The returned recommenders should coincide with what was put in
@@ -48,10 +83,8 @@ def test_sequential_strategy(recommenders, reuse_last):
         (rec for rec in RECOMMENDERS),  # generator
     ],
 )
-def test_streaming_sequential_strategy(recommenders):
+def test_streaming_sequential_strategy(searchspace, recommenders):
     """The strategy should provide its recommenders in the right order."""
-    parameters = [NumericalDiscreteParameter(name="test", values=[0, 1])]
-    searchspace = SearchSpace.from_product(parameters)
     strategy = StreamingSequentialStrategy(recommenders=recommenders)
 
     # The returned recommenders should coincide with what was put in
