eb7b0405603927fe4758382d86298fee3de4a34c
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-11-24 12:06:34 +0100

    Add encoding enum docstrings

--- baybe/parameters/enum.py
+++ baybe/parameters/enum.py
@@ -11,18 +11,27 @@ class CategoricalEncoding(ParameterEncoding):
     """Available encodings for categorical parameters."""
 
     OHE = "OHE"
+    """One-hot encoding."""
+
     INT = "INT"
+    """Integer encoding."""
 
 
 class SubstanceEncoding(ParameterEncoding):
     """Available encodings for substance parameters."""
 
     MORDRED = "MORDRED"
+    """Encoding based on Mordred chemical descriptors."""
+
     RDKIT = "RDKIT"
+    """Encoding based on RDKit chemical descriptors."""
+
     MORGAN_FP = "MORGAN_FP"
+    """Encoding based on Morgan molecule fingerprints."""
 
 
 class CustomEncoding(ParameterEncoding):
     """Available encodings for custom parameters."""
 
     CUSTOM = "CUSTOM"
+    """User-defined encoding."""
