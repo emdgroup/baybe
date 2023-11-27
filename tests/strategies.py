355e33eda95d924e3059bb3c137e1a85cfc9d814
Adrian Sosic <adrian.sosic@merckgroup.com>
2023-11-27 19:41:58 +0100

    Fix strategy docstrings

--- tests/strategies.py
+++ tests/strategies.py
@@ -32,18 +32,18 @@ _decorrelate = st.one_of(
     st.booleans(),
     st.floats(min_value=0.0, max_value=1.0, exclude_min=True, exclude_max=True),
 )
-"""A strategy that creates decorrelation settings."""
+"""A strategy that generates decorrelation settings."""
 
 parameter_name = st.text(min_size=1)
-"""A strategy that creates parameter names."""
+"""A strategy that generates parameter names."""
 
 categories = st.lists(st.text(min_size=1), min_size=2, unique=True)
-"""A strategy that creates parameter categories."""
+"""A strategy that generates parameter categories."""
 
 
 @st.composite
 def smiles(draw: st.DrawFn):
-    """Generates short SMILES strings."""
+    """Generate short SMILES strings."""
     n_atoms = draw(st.integers(min_value=0, max_value=19))
     string = "C"
     for _ in range(n_atoms):
@@ -54,7 +54,7 @@ def smiles(draw: st.DrawFn):
 
 @st.composite
 def substance_data(draw: st.DrawFn):
-    """Generates data for class:`baybe.parameters.substance.SubstanceParameter`."""
+    """Generate data for class:`baybe.parameters.substance.SubstanceParameter`."""
     names = draw(st.lists(st.text(min_size=1), min_size=2, max_size=10, unique=True))
     substances = draw(st.lists(smiles(), min_size=len(names), max_size=len(names)))
     return dict(zip(names, substances))
@@ -62,7 +62,7 @@ def substance_data(draw: st.DrawFn):
 
 @st.composite
 def custom_encodings(draw: st.DrawFn):
-    """Generates data for class:`baybe.parameters.custom.CustomDiscreteParameter`."""
+    """Generate data for class:`baybe.parameters.custom.CustomDiscreteParameter`."""
     index = st.lists(st.text(min_size=1), min_size=2, max_size=10, unique=True)
     cols = columns(
         names_or_number=10,
@@ -77,7 +77,7 @@ def custom_encodings(draw: st.DrawFn):
 def numerical_discrete_parameter(  # pylint: disable=inconsistent-return-statements
     draw: st.DrawFn,
 ):
-    """Generates class:`baybe.parameters.numerical.NumericalDiscreteParameter`."""
+    """Generate class:`baybe.parameters.numerical.NumericalDiscreteParameter`."""
     name = draw(parameter_name)
     values = draw(
         st.lists(
@@ -99,7 +99,7 @@ def numerical_discrete_parameter(  # pylint: disable=inconsistent-return-stateme
 
 @st.composite
 def numerical_continuous_parameter(draw: st.DrawFn):
-    """Generates class:`baybe.parameters.numerical.NumericalContinuousParameter`."""
+    """Generate class:`baybe.parameters.numerical.NumericalContinuousParameter`."""
     name = draw(parameter_name)
     lower = draw(st.floats(max_value=_largest_lower_interval, allow_infinity=False))
     upper = draw(st.floats(min_value=lower, exclude_min=True, allow_infinity=False))
@@ -108,7 +108,7 @@ def numerical_continuous_parameter(draw: st.DrawFn):
 
 @st.composite
 def categorical_parameter(draw: st.DrawFn):
-    """Generates class:`baybe.parameters.categorical.CategoricalParameter`."""
+    """Generate class:`baybe.parameters.categorical.CategoricalParameter`."""
     name = draw(parameter_name)
     values = draw(categories)
     encoding = draw(st.sampled_from(CategoricalEncoding))
@@ -117,7 +117,7 @@ def categorical_parameter(draw: st.DrawFn):
 
 @st.composite
 def task_parameter(draw: st.DrawFn):
-    """Generates class:`baybe.parameters.categorical.TaskParameter`."""
+    """Generate class:`baybe.parameters.categorical.TaskParameter`."""
     name = draw(parameter_name)
     values = draw(categories)
     active_values = random.sample(values, random.randint(0, len(values)))
@@ -126,7 +126,7 @@ def task_parameter(draw: st.DrawFn):
 
 @st.composite
 def substance_parameter(draw: st.DrawFn):
-    """Generates class:`baybe.parameters.substance.SubstanceParameter`."""
+    """Generate class:`baybe.parameters.substance.SubstanceParameter`."""
     name = draw(parameter_name)
     data = draw(substance_data())
     decorrelate = draw(_decorrelate)
@@ -138,7 +138,7 @@ def substance_parameter(draw: st.DrawFn):
 
 @st.composite
 def custom_parameter(draw: st.DrawFn):
-    """Generates class:`baybe.parameters.custom.CustomDiscreteParameter`."""
+    """Generate class:`baybe.parameters.custom.CustomDiscreteParameter`."""
     name = draw(parameter_name)
     data = draw(custom_encodings())
     decorrelate = draw(_decorrelate)
@@ -155,4 +155,4 @@ parameter = st.one_of(
         custom_parameter(),
     ]
 )
-"""A strategy that creates parameters."""
+"""A strategy that generates parameters."""
