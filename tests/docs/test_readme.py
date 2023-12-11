888ab3c9e0d36c191b54e3fc96c7d1a762096326
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-11 14:19:07 +0100

    Add tests for user guide pages

--- tests/docs/test_readme.py
+++
@@ -1,9 +0,0 @@
-"""Test the code provided in the README."""
-
-from .utils import extract_code_blocks
-
-
-def test_readme():
-    """The blocks in the README become a valid python script when concatenated."""
-    readme_code = extract_code_blocks("README.md")
-    exec(readme_code)
