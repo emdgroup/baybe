a3df853f54bbf0f8f0b1f25bb04f964cc36938cb
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-11 14:38:52 +0100

    Clean up example tests

--- tests/docs/test_examples.py
+++ tests/docs/test_examples.py
@@ -11,15 +11,11 @@ from baybe.utils.chemistry import _MORDRED_INSTALLED, _RDKIT_INSTALLED
 _CHEM_INSTALLED = _MORDRED_INSTALLED and _RDKIT_INSTALLED
 
 
-example_list = list(Path(".").glob("examples/*/*.py"))
-
-EXAMPLES = [str(file) for file in example_list]
-
-
-if _CHEM_INSTALLED and _ONNX_INSTALLED:
-
-    @pytest.mark.slow
-    @pytest.mark.parametrize("example", EXAMPLES)
-    def test_example(example: str):
-        """Test an individual example by running it."""
-        runpy.run_path(example)
+@pytest.mark.slow
+@pytest.mark.skipif(
+    not (_CHEM_INSTALLED and _ONNX_INSTALLED), reason="skipped for core tests"
+)
+@pytest.mark.parametrize("example", Path("examples/").rglob("*.py"))
+def test_example(example: str):
+    """Test an individual example by running it."""
+    runpy.run_path(example)
