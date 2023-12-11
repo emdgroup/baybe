462524591aecc3a88d255ba84a31935c6e9eb0c6
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-11 10:28:06 +0100

    Explicitly enumerate parameter types for roundtrip test

--- tests/serialization/test_parameter_serialization.py
+++ tests/serialization/test_parameter_serialization.py
@@ -1,15 +1,37 @@
 """Test serialization of parameters."""
 
+import hypothesis.strategies as st
+import pytest
 from hypothesis import given
+from pytest import param
 
 from baybe.parameters.base import Parameter
 
-from ..hypothesis_strategies import parameter
+from ..hypothesis_strategies import (
+    categorical_parameter,
+    custom_parameter,
+    numerical_continuous_parameter,
+    numerical_discrete_parameter,
+    substance_parameter,
+    task_parameter,
+)
 
 
-@given(parameter)
-def test_parameter_roundtrip(param: Parameter):
+@pytest.mark.parametrize(
+    "strategy",
+    [
+        param(numerical_discrete_parameter(), id="NumericalDiscreteParameter"),
+        param(numerical_continuous_parameter(), id="NumericalContinuousParameter"),
+        param(categorical_parameter(), id="CategoricalParameter"),
+        param(task_parameter(), id="TaskParameter"),
+        param(substance_parameter(), id="SubstanceParameter"),
+        param(custom_parameter(), id="CustomParameter"),
+    ],
+)
+@given(data=st.data())
+def test_parameter_roundtrip(strategy, data):
     """A serialization roundtrip yields an equivalent object."""
-    string = param.to_json()
-    param2 = Parameter.from_json(string)
-    assert param == param2, (param, param2)
+    parameter = data.draw(strategy)
+    string = parameter.to_json()
+    parameter2 = Parameter.from_json(string)
+    assert parameter == parameter2, (parameter, parameter2)
