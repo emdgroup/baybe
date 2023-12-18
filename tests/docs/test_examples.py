9a82e615a833c38fe37170b4064653bf85c9e5e4
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-18 09:44:06 +0100

    Fix path in example tests

--- tests/docs/test_examples.py
+++ tests/docs/test_examples.py
@@ -16,6 +16,6 @@ _CHEM_INSTALLED = _MORDRED_INSTALLED and _RDKIT_INSTALLED
     not (_CHEM_INSTALLED and _ONNX_INSTALLED), reason="skipped for core tests"
 )
 @pytest.mark.parametrize("example", Path("examples/").rglob("*.py"))
-def test_example(example: str):
+def test_example(example: Path):
     """Test an individual example by running it."""
-    runpy.run_path(example)
+    runpy.run_path(str(example))
