cffd621732cf5c003e085dbc01a6ab84a9e18a81
AdrianSosic <adrian.sosic@merckgroup.com>
2024-01-09 10:55:35 +0100

    Ignore code blocks with triple tilde when testing user guides

--- tests/docs/utils.py
+++ tests/docs/utils.py
@@ -5,9 +5,16 @@ from pathlib import Path
 from typing import List, Union
 
 
-def extract_code_blocks(path: Union[str, Path]) -> List[str]:
+def extract_code_blocks(
+    path: Union[str, Path], include_tilde: bool = True
+) -> List[str]:
     """Extract all python code blocks from the specified file."""
     contents = Path(path).read_text()
-    code_blocks = re.findall(r"```python\s+(.*?)\s+```", contents, flags=re.DOTALL)
+    pattern = (
+        r"(?:```|~~~)python\s+(.*?)\s+(?:```|~~~)"
+        if include_tilde
+        else r"```python\s+(.*?)\s+```"
+    )
+    code_blocks = re.findall(pattern, contents, flags=re.DOTALL)
 
     return code_blocks
