7d8c8863480179db1d37f01b6b3d742daccc3ff5
AdrianSosic <adrian.sosic@merckgroup.com>
2024-01-16 17:48:48 +0100

    Add utilities for dataframe serialization

--- baybe/serialization/__init__.py
+++ baybe/serialization/__init__.py
@@ -9,13 +9,16 @@ from baybe.serialization.core import (
     unstructure_base,
 )
 from baybe.serialization.mixin import SerialMixin
+from baybe.serialization.utils import deserialize_dataframe, serialize_dataframe
 
 __all__ = [
     "block_deserialization_hook",
     "block_serialization_hook",
     "converter",
+    "deserialize_dataframe",
     "get_base_structure_hook",
     "select_constructor_hook",
+    "serialize_dataframe",
     "unstructure_base",
     "SerialMixin",
 ]
