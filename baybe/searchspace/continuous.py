35f8fa585c78350e885881b4ecf775b3a5f67404
AdrianSosic <adrian.sosic@merckgroup.com>
2024-01-17 11:01:25 +0100

    Add serialization mixin to subspace classes

--- baybe/searchspace/continuous.py
+++ baybe/searchspace/continuous.py
@@ -16,12 +16,12 @@ from baybe.constraints import (
 )
 from baybe.parameters import NumericalContinuousParameter
 from baybe.searchspace.validation import validate_parameter_names
-from baybe.serialization import converter, select_constructor_hook
+from baybe.serialization import SerialMixin, converter, select_constructor_hook
 from baybe.utils import DTypeFloatTorch
 
 
 @define
-class SubspaceContinuous:
+class SubspaceContinuous(SerialMixin):
     """Class for managing continuous subspaces.
 
     Builds the subspace from parameter definitions, keeps
--- baybe/searchspace/discrete.py
+++ baybe/searchspace/discrete.py
@@ -19,14 +19,14 @@ from baybe.parameters import (
 )
 from baybe.parameters.base import DiscreteParameter, Parameter
 from baybe.searchspace.validation import validate_parameter_names
-from baybe.serialization import converter, select_constructor_hook
+from baybe.serialization import SerialMixin, converter, select_constructor_hook
 from baybe.utils import df_drop_single_value_columns, eq_dataframe, fuzzy_row_match
 
 _METADATA_COLUMNS = ["was_recommended", "was_measured", "dont_recommend"]
 
 
 @define
-class SubspaceDiscrete:
+class SubspaceDiscrete(SerialMixin):
     """Class for managing discrete subspaces.
 
     Builds the subspace from parameter definitions and optional constraints, keeps
