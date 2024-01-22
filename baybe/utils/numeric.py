923c898a4bbd7c066d0b55314f43d66d015cd562
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-01-22 10:17:16 +0100

    Remove default

--- baybe/utils/numeric.py
+++ baybe/utils/numeric.py
@@ -21,7 +21,7 @@ There is no clear documentation but some references can be found here (version 1
 """  # noqa: E501
 
 
-def geom_mean(arr: np.ndarray, weights: List[float] = None) -> np.ndarray:
+def geom_mean(arr: np.ndarray, weights: List[float]) -> np.ndarray:
     """Calculate the (weighted) geometric mean along the second axis of a 2-D array.
 
     Alternative to ``gmean`` from scipy that avoids logarithms and division errors.
