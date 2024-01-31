cce61cc98e6451bdece01769efd274f4a945fcda
AdrianSosic <adrian.sosic@merckgroup.com>
2024-01-31 22:03:15 +0100

    Fix dataframe hypothesis strategy
    https://github.com/HypothesisWorks/hypothesis/issues/3859

--- tests/hypothesis_strategies/dataframes.py
+++ tests/hypothesis_strategies/dataframes.py
@@ -1,34 +1,16 @@
 """Hypothesis strategies for dataframes."""
 
 from hypothesis import strategies as st
-from hypothesis.extra.pandas import column, data_frames
-
-_index_strategy = st.one_of(st.text(), st.integers(), st.floats())
-"""A strategy for generating dataframe indexes."""
+from hypothesis.extra.pandas import column, data_frames, indexes
 
 
 @st.composite
 def random_dataframes(draw: st.DrawFn):
     """Generate pandas dataframes of random shape and content."""
-    # Generate the dataframe shape
-    num_rows = draw(st.integers(min_value=0, max_value=2))
-    num_cols = draw(st.integers(min_value=0, max_value=5))
-
-    # Generate the column names/types and index
-    col_names = draw(
-        st.lists(_index_strategy, min_size=num_cols, max_size=num_cols, unique=True)
+    index_elements = st.one_of(st.text(), st.integers(), st.floats())
+    cols = st.builds(
+        column, name=index_elements, dtype=st.sampled_from([int, float, str])
     )
-    col_types = draw(
-        st.lists(
-            st.sampled_from([int, float, str]), min_size=num_cols, max_size=num_cols
-        )
-    )
-    index = st.lists(_index_strategy, min_size=num_rows, max_size=num_rows)
-
-    # Define the column content
-    columns = [
-        column(name=name, dtype=dtype) for name, dtype in zip(col_names, col_types)
-    ]
-
-    # Generate the dataframe
-    return draw(data_frames(columns=columns, index=index))
+    columns = draw(st.lists(cols, max_size=5, unique_by=lambda c: c.name))
+    index_strategy = indexes(elements=index_elements, max_size=2)
+    return draw(data_frames(columns=columns, index=index_strategy))
--- tests/serialization/test_dataframe_serialization.py
+++ tests/serialization/test_dataframe_serialization.py
@@ -1,16 +1,13 @@
 """Test serialization of dataframes."""
 
-import os
-
 import pandas as pd
-from hypothesis import HealthCheck, given, settings
+from hypothesis import given
 
 from baybe.serialization import deserialize_dataframe, serialize_dataframe
 from tests.hypothesis_strategies.dataframes import random_dataframes
 
 
 @given(random_dataframes())
-@settings(suppress_health_check=[HealthCheck.too_slow] if "CI" in os.environ else [])
 def test_dataframe_roundtrip(df: pd.DataFrame):
     """A serialization roundtrip yields an equivalent object."""
     string = serialize_dataframe(df)
