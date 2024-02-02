6e8c7f4f07bac4697bdcac97253c9bcc58b78136
AdrianSosic <adrian.sosic@merckgroup.com>
2024-02-02 18:28:04 +0100

    Adjust tolerance logic of numerical discrete parameter
    Before, in-range values were defined by the distance to the closest parameter point. Now, everything inside the convex hull is automatically considered in range.

--- baybe/parameters/numerical.py
+++ baybe/parameters/numerical.py
@@ -4,15 +4,13 @@ from functools import cached_property
 from typing import Any, ClassVar, Tuple
 
 import cattrs
-import numpy as np
 import pandas as pd
 from attrs import define, field
-from attrs.validators import min_len
+from attrs.validators import ge, min_len
 
-from baybe.exceptions import NumericalUnderflowError
 from baybe.parameters.base import DiscreteParameter, Parameter
 from baybe.parameters.validation import validate_is_finite, validate_unique_values
-from baybe.utils import DTypeFloatNumpy, InfiniteIntervalError, Interval, convert_bounds
+from baybe.utils import InfiniteIntervalError, Interval, convert_bounds
 
 
 @define(frozen=True, slots=False)
@@ -37,43 +35,11 @@ class NumericalDiscreteParameter(DiscreteParameter):
     )
     """The values the parameter can take."""
 
-    tolerance: float = field(default=0.0)
-    """The absolute tolerance used for deciding whether a value is in range. A tolerance
-        larger than half the minimum distance between parameter values is not allowed
-        because that could cause ambiguity when inputting data points later."""
-
-    @tolerance.validator
-    def _validate_tolerance(  # noqa: DOC101, DOC103
-        self, _: Any, tolerance: float
-    ) -> None:
-        """Validate that the given tolerance is safe.
-
-        The tolerance is the allowed experimental uncertainty when
-        reading in measured values. A tolerance larger than half the minimum
-        distance between parameter values is not allowed because that could cause
-        ambiguity when inputting data points later.
-
-        Raises:
-            ValueError: If the tolerance is not safe.
-        """
-        # For zero tolerance, the only left requirement is that all parameter values
-        # are distinct, which is already ensured by the corresponding validator.
-        if tolerance == 0.0:
-            return
-
-        min_dist = np.diff(self.values).min()
-        if min_dist == (eps := np.nextafter(0, 1, dtype=DTypeFloatNumpy)):
-            raise NumericalUnderflowError(
-                f"The distance between any two parameter values must be at least "
-                f"twice the size of the used floating point resolution of {eps}."
-            )
-
-        if tolerance >= (max_tol := min_dist / 2.0):
-            raise ValueError(
-                f"Parameter '{self.name}' is initialized with tolerance {tolerance} "
-                f"but due to the given parameter values {self.values}, the specified "
-                f"tolerance must be smaller than {max_tol} to avoid ambiguity."
-            )
+    tolerance: float = field(default=0.0, validator=ge(0.0))
+    """The absolute tolerance used for deciding whether a value is considered in range.
+        Everything inside the convex hull of the parameter values is automatically
+        considered in range. If non-zero, the tolerance expands the parameter range
+        at the boundary values."""
 
     @property
     def values(self) -> tuple:  # noqa: D102
@@ -88,10 +54,9 @@ class NumericalDiscreteParameter(DiscreteParameter):
 
     def is_in_range(self, item: float) -> bool:  # noqa: D102
         # See base class.
-        differences_acceptable = [
-            np.abs(val - item) <= self.tolerance for val in self.values
-        ]
-        return any(differences_acceptable)
+        lower = min(self.values) - self.tolerance
+        upper = max(self.values) + self.tolerance
+        return lower <= item <= upper
 
 
 @define(frozen=True, slots=False)
