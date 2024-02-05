4c92d995bf0022f20019f9270ef61cba8e8dc13f
AdrianSosic <adrian.sosic@merckgroup.com>
2024-02-05 16:37:36 +0100

    Add TODO notes

--- baybe/serialization/core.py
+++ baybe/serialization/core.py
@@ -9,6 +9,8 @@ from cattrs.gen import make_dict_structure_fn, make_dict_unstructure_fn
 
 _T = TypeVar("_T")
 
+# TODO: This urgently needs the `forbid_extra_keys=True` flag, which requires us to
+#   switch to the cattrs built-in subclass strategy.
 converter = cattrs.Converter()
 """The default converter for (de-)serializing BayBE-related objects."""
 
