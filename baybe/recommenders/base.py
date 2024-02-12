6d36472040139078e6989cefa3e8cc220430c798
AdrianSosic <adrian.sosic@merckgroup.com>
2024-02-12 10:52:54 +0100

    Move `_select_candidates_and_recommend` to class body
    Temporarily as a staticmethod to avoid making functional changes

--- baybe/recommenders/base.py
+++ baybe/recommenders/base.py
@@ -20,73 +20,6 @@ from baybe.serialization import (
 )
 
 
-def _select_candidates_and_recommend(
-    searchspace: SearchSpace,
-    recommend_discrete: Callable[[SubspaceDiscrete, pd.DataFrame, int], pd.Index],
-    batch_size: int = 1,
-    allow_repeated_recommendations: bool = False,
-    allow_recommending_already_measured: bool = True,
-) -> pd.DataFrame:
-    """Select candidates in a discrete search space and recommend them.
-
-    This function is a workaround as this functionality is required for all purely
-    discrete recommenders and avoids the introduction of complicate class hierarchies.
-    It is also used to select candidates in the discrete part of hybrid search spaces,
-    ignoring the continuous part.
-
-    Args:
-        searchspace: The search space.
-        recommend_discrete: The Callable representing the discrete recommendation
-            function.
-        batch_size: The chosen batch size.
-        allow_repeated_recommendations: Allow to make recommendations that were already
-            recommended earlier.
-        allow_recommending_already_measured: Allow to output recommendations that were
-            measured previously.
-
-    Returns:
-        The recommendation in experimental representation.
-
-    Raises:
-        NotEnoughPointsLeftError: If there are fewer than ``batch_size`` points
-            left for potential recommendation.
-    """
-    # IMPROVE: See if the there is a more elegant way to share this functionality
-    #   among all purely discrete recommenders (without introducing complicates class
-    #   hierarchies).
-
-    # Get discrete candidates. The metadata flags are ignored if the search space
-    # has a continuous component.
-    _, candidates_comp = searchspace.discrete.get_candidates(
-        allow_repeated_recommendations=allow_repeated_recommendations
-        or not searchspace.continuous.is_empty,
-        allow_recommending_already_measured=allow_recommending_already_measured
-        or not searchspace.continuous.is_empty,
-    )
-
-    # Check if enough candidates are left
-    # TODO [15917]: This check is not perfectly correct.
-    if len(candidates_comp) < batch_size:
-        raise NotEnoughPointsLeftError(
-            f"Using the current settings, there are fewer than {batch_size} "
-            "possible data points left to recommend. This can be "
-            "either because all data points have been measured at some point "
-            "(while 'allow_repeated_recommendations' or "
-            "'allow_recommending_already_measured' being False) "
-            "or because all data points are marked as 'dont_recommend'."
-        )
-
-    # Get recommendations
-    idxs = recommend_discrete(searchspace.discrete, candidates_comp, batch_size)
-    rec = searchspace.discrete.exp_rep.loc[idxs, :]
-
-    # Update metadata
-    searchspace.discrete.metadata.loc[idxs, "was_recommended"] = True
-
-    # Return recommendations
-    return rec
-
-
 class RecommenderProtocol(Protocol):
     """Type protocol specifying the interface recommenders need to implement."""
 
@@ -127,6 +60,73 @@ class Recommender(ABC, RecommenderProtocol):
     allow_recommending_already_measured: bool = field(default=True, kw_only=True)
     """Allow to output recommendations that were measured previously. This only has an
     influence in discrete search spaces."""
+
+    @staticmethod
+    def _select_candidates_and_recommend(
+        searchspace: SearchSpace,
+        recommend_discrete: Callable[[SubspaceDiscrete, pd.DataFrame, int], pd.Index],
+        batch_size: int = 1,
+        allow_repeated_recommendations: bool = False,
+        allow_recommending_already_measured: bool = True,
+    ) -> pd.DataFrame:
+        """Select candidates in a discrete search space and recommend them.
+
+        This function is a workaround as this functionality is required for all purely
+        discrete recommenders and avoids the introduction of complicate class
+        hierarchies. It is also used to select candidates in the discrete part of hybrid
+        search spaces, ignoring the continuous part.
+
+        Args:
+            searchspace: The search space.
+            recommend_discrete: The Callable representing the discrete recommendation
+                function.
+            batch_size: The chosen batch size.
+            allow_repeated_recommendations: Allow to make recommendations that were
+                already recommended earlier.
+            allow_recommending_already_measured: Allow to output recommendations that
+                were measured previously.
+
+        Returns:
+            The recommendation in experimental representation.
+
+        Raises:
+            NotEnoughPointsLeftError: If there are fewer than ``batch_size`` points
+                left for potential recommendation.
+        """
+        # IMPROVE: See if the there is a more elegant way to share this functionality
+        #   among all purely discrete recommenders (without introducing complicates
+        #   class hierarchies).
+
+        # Get discrete candidates. The metadata flags are ignored if the search space
+        # has a continuous component.
+        _, candidates_comp = searchspace.discrete.get_candidates(
+            allow_repeated_recommendations=allow_repeated_recommendations
+            or not searchspace.continuous.is_empty,
+            allow_recommending_already_measured=allow_recommending_already_measured
+            or not searchspace.continuous.is_empty,
+        )
+
+        # Check if enough candidates are left
+        # TODO [15917]: This check is not perfectly correct.
+        if len(candidates_comp) < batch_size:
+            raise NotEnoughPointsLeftError(
+                f"Using the current settings, there are fewer than {batch_size} "
+                "possible data points left to recommend. This can be "
+                "either because all data points have been measured at some point "
+                "(while 'allow_repeated_recommendations' or "
+                "'allow_recommending_already_measured' being False) "
+                "or because all data points are marked as 'dont_recommend'."
+            )
+
+        # Get recommendations
+        idxs = recommend_discrete(searchspace.discrete, candidates_comp, batch_size)
+        rec = searchspace.discrete.exp_rep.loc[idxs, :]
+
+        # Update metadata
+        searchspace.discrete.metadata.loc[idxs, "was_recommended"] = True
+
+        # Return recommendations
+        return rec
 
 
 # Register (un-)structure hooks
