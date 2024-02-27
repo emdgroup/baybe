9f151bc550fc851c0b140ce06fd7423998f11bc3
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-02-27 14:56:50 +0100

    cast to_tensor types

--- baybe/recommenders/naive.py
+++ baybe/recommenders/naive.py
@@ -1,10 +1,11 @@
 """Naive recommender for hybrid spaces."""
 
 import warnings
-from typing import ClassVar, Optional
+from typing import ClassVar, Optional, cast
 
 import pandas as pd
 from attrs import define, evolve, field, fields
+from torch import Tensor
 
 from baybe.acquisition import PartialAcquisitionFunction
 from baybe.recommenders.pure.base import PureRecommender
@@ -115,7 +116,7 @@ class NaiveHybridSpaceRecommender(PureRecommender):
         # will then be attached to every discrete point when the acquisition function
         # is evaluated.
         cont_part = searchspace.continuous.samples_random(1)
-        cont_part_tensor = to_tensor(cont_part).unsqueeze(-2)
+        cont_part_tensor = cast(Tensor, to_tensor(cont_part)).unsqueeze(-2)
 
         # Get discrete candidates. The metadata flags are ignored since the search space
         # is hybrid
@@ -152,7 +153,7 @@ class NaiveHybridSpaceRecommender(PureRecommender):
         # Get one random discrete point that will be attached when evaluating the
         # acquisition function in the discrete space.
         disc_part = searchspace.discrete.comp_rep.loc[disc_rec_idx].sample(1)
-        disc_part_tensor = to_tensor(disc_part).unsqueeze(-2)
+        disc_part_tensor = cast(Tensor, to_tensor(disc_part)).unsqueeze(-2)
 
         # Setup a fresh acquisition function for the continuous recommender
         self.cont_recommender.setup_acquisition_function(searchspace, train_x, train_y)
