2bf16833a0392ddc28bf12df34f3d7446bef243a
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-02-27 14:48:54 +0100

    Add clustering model type hint

--- baybe/recommenders/pure/nonpredictive/clustering.py
+++ baybe/recommenders/pure/nonpredictive/clustering.py
@@ -1,7 +1,7 @@
 """Recommendation strategies based on clustering."""
 
 from abc import ABC
-from typing import ClassVar, List, Type, TypeVar
+from typing import ClassVar, List, Type, Union
 
 import numpy as np
 import pandas as pd
@@ -16,8 +16,6 @@ from sklearn_extra.cluster import KMedoids
 
 from baybe.recommenders.pure.nonpredictive.base import NonPredictiveRecommender
 from baybe.searchspace import SearchSpaceType, SubspaceDiscrete
-
-_SKLearnClusterModel = TypeVar("_SKLearnClusterModel", bound=ClusterMixin)
 
 
 @define
@@ -42,7 +40,7 @@ class SKLearnClusteringRecommender(NonPredictiveRecommender, ABC):
     #   that checks if a custom mechanism is implemented and uses default otherwise
     #   (similar to what is done in the recommenders)
 
-    model_class: ClassVar[Type[_SKLearnClusterModel]]
+    model_class: ClassVar[Type[ClusterMixin]]
     """Class variable describing the model class."""
 
     model_cluster_num_parameter_name: ClassVar[str]
@@ -58,8 +56,8 @@ class SKLearnClusteringRecommender(NonPredictiveRecommender, ABC):
 
     def _make_selection_default(
         self,
-        model: _SKLearnClusterModel,
-        candidates_scaled: pd.DataFrame,
+        model: ClusterMixin,
+        candidates_scaled: Union[pd.DataFrame, np.ndarray],
     ) -> List[int]:
         """Select one candidate from each cluster uniformly at random.
 
@@ -81,8 +79,8 @@ class SKLearnClusteringRecommender(NonPredictiveRecommender, ABC):
 
     def _make_selection_custom(
         self,
-        model: _SKLearnClusterModel,
-        candidates_scaled: pd.DataFrame,
+        model: ClusterMixin,
+        candidates_scaled: Union[pd.DataFrame, np.ndarray],
     ) -> List[int]:
         """Select candidates from the computed clustering.
 
@@ -137,7 +135,7 @@ class SKLearnClusteringRecommender(NonPredictiveRecommender, ABC):
 class PAMClusteringRecommender(SKLearnClusteringRecommender):
     """Partitioning Around Medoids (PAM) clustering recommender."""
 
-    model_class: ClassVar[Type[_SKLearnClusterModel]] = KMedoids
+    model_class: ClassVar[Type[ClusterMixin]] = KMedoids
     # See base class.
 
     model_cluster_num_parameter_name: ClassVar[str] = "n_clusters"
@@ -157,8 +155,8 @@ class PAMClusteringRecommender(SKLearnClusteringRecommender):
 
     def _make_selection_custom(
         self,
-        model: _SKLearnClusterModel,
-        candidates_scaled: pd.DataFrame,
+        model: ClusterMixin,
+        candidates_scaled: Union[pd.DataFrame, np.ndarray],
     ) -> List[int]:
         """Select candidates from the computed clustering.
 
@@ -181,7 +179,7 @@ class KMeansClusteringRecommender(SKLearnClusteringRecommender):
     """K-means clustering recommender."""
 
     # Class variables
-    model_class: ClassVar[Type[_SKLearnClusterModel]] = KMeans
+    model_class: ClassVar[Type[ClusterMixin]] = KMeans
     # See base class.
 
     model_cluster_num_parameter_name: ClassVar[str] = "n_clusters"
@@ -201,8 +199,8 @@ class KMeansClusteringRecommender(SKLearnClusteringRecommender):
 
     def _make_selection_custom(
         self,
-        model: _SKLearnClusterModel,
-        candidates_scaled: pd.DataFrame,
+        model: ClusterMixin,
+        candidates_scaled: Union[pd.DataFrame, np.ndarray],
     ) -> List[int]:
         """Select candidates from the computed clustering.
 
@@ -233,7 +231,7 @@ class GaussianMixtureClusteringRecommender(SKLearnClusteringRecommender):
     """Gaussian mixture model (GMM) clustering recommender."""
 
     # Class variables
-    model_class: ClassVar[Type[_SKLearnClusterModel]] = GaussianMixture
+    model_class: ClassVar[Type[ClusterMixin]] = GaussianMixture
     # See base class.
 
     model_cluster_num_parameter_name: ClassVar[str] = "n_components"
@@ -241,8 +239,8 @@ class GaussianMixtureClusteringRecommender(SKLearnClusteringRecommender):
 
     def _make_selection_custom(
         self,
-        model: _SKLearnClusterModel,
-        candidates_scaled: pd.DataFrame,
+        model: ClusterMixin,
+        candidates_scaled: Union[pd.DataFrame, np.ndarray],
     ) -> List[int]:
         """Select candidates from the computed clustering.
 
