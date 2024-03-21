a0ae752c9d80deb9432c609b9eb8511346ffc2e7
AdrianSosic <adrian.sosic@merckgroup.com>
2024-03-21 18:03:21 +0100

    Make parameters of constraint selectable

--- tests/hypothesis_strategies/constraints.py
+++ tests/hypothesis_strategies/constraints.py
@@ -1,5 +1,7 @@
 """Hypothesis strategies for constraints."""
 
+from typing import List, Optional
+
 import hypothesis.strategies as st
 
 from baybe.constraints.conditions import (
@@ -28,18 +30,23 @@ threshold_conditions = st.builds(
 
 
 @st.composite
-def discrete_excludes_constraints(draw: st.DrawFn):
+def discrete_excludes_constraints(
+    draw: st.DrawFn, parameters: Optional[List[DiscreteParameter]] = None
+):
     """Generate :class:`baybe.constraints.discrete.DiscreteExcludeConstraint`."""
-    params = draw(st.lists(discrete_parameters, min_size=1, unique_by=lambda p: p.name))
-    param_names = [p.name for p in params]
+    if parameters is None:
+        parameters = draw(
+            st.lists(discrete_parameters, min_size=1, unique_by=lambda p: p.name)
+        )
+    parameter_names = [p.name for p in parameters]
 
     # Threshold conditions only make sense for numerical parameters
     conditions = [
         draw(st.one_of([sub_selection_conditions(p), threshold_conditions]))
         if isinstance(p, NumericalDiscreteParameter)
         else draw(sub_selection_conditions(p))
-        for p in params
+        for p in parameters
     ]
 
     combiner = draw(st.sampled_from(list(_valid_logic_combiners)))
-    return DiscreteExcludeConstraint(param_names, conditions, combiner)
+    return DiscreteExcludeConstraint(parameter_names, conditions, combiner)
