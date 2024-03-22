1b81e99be871b66af93e693cf0eb6f331a5b5cd9
AdrianSosic <adrian.sosic@merckgroup.com>
2024-03-22 10:37:31 +0100

    Remove serialization test of handcrafted constraints

--- tests/serialization/test_constraint_serialization.py
+++ tests/serialization/test_constraint_serialization.py
@@ -53,19 +53,6 @@ def test_constraint_roundtrip(constraint_strategy, data):
 
 @pytest.mark.parametrize(
     "constraint_names",
-    [[f"Constraint_{k}"] for k in range(1, 13)]  # Constraint_13 is expected to fail
-    + [[f"ContiConstraint_{k}"] for k in range(1, 5)],
-)
-@pytest.mark.parametrize("n_grid_points", [5])
-def test_constraint_serialization(constraints):
-    constraint = constraints[0]
-    string = constraint.to_json()
-    constraint2 = Constraint.from_json(string)
-    assert constraint == constraint2
-
-
-@pytest.mark.parametrize(
-    "constraint_names",
     [["Constraint_13"]],
 )
 @pytest.mark.parametrize("n_grid_points", [5])
