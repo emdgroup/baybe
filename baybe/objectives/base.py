681e17254f68f0d964f3fc7a3e7d0169bff7e122
AdrianSosic <adrian.sosic@merckgroup.com>
2024-03-27 13:31:33 +0100

    Add Objective base class

---
+++ baybe/objectives/base.py
@@ -0,0 +1,40 @@
+"""Base classes for all objectives."""
+
+from abc import ABC, abstractmethod
+
+import pandas as pd
+from attrs import define
+
+from baybe.serialization.core import (
+    converter,
+    get_base_structure_hook,
+    unstructure_base,
+)
+from baybe.serialization.mixin import SerialMixin
+from baybe.targets.base import Target
+
+
+@define(frozen=True)
+class Objective(ABC, SerialMixin):
+    """Abstract base class for all objectives."""
+
+    @property
+    @abstractmethod
+    def targets(self) -> tuple[Target]:
+        """The targets included in the objective."""
+
+    @abstractmethod
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Transform targets from experimental to computational representation.
+
+        Args:
+            data: The data to be transformed. Must contain columns for all targets
+                but can contain additional columns.
+
+        Returns:
+            A new dataframe with the targets in computational representation.
+        """
+
+
+converter.register_structure_hook(Objective, get_base_structure_hook(Objective))
+converter.register_unstructure_hook(Objective, unstructure_base)
