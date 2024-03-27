d0879bd27854815fdf6408cd22a51e9bf92c3378
AdrianSosic <adrian.sosic@merckgroup.com>
2024-03-27 15:35:53 +0100

    Implement dispatching mechanism for backward compatibility

---
+++ baybe/objective.py
@@ -0,0 +1,38 @@
+"""Temporary dispatching functionality for backward compatibility."""
+
+import warnings
+from typing import Literal, Optional
+
+from baybe.objectives.desirability import DesirabilityObjective
+from baybe.objectives.single import SingleTargetObjective
+from baybe.targets.base import Target
+
+
+def Objective(
+    mode: Literal["SINGLE", "DESIRABILITY"],
+    targets: list[Target],
+    weights: Optional[list[float]] = None,
+    combine_func: Optional[Literal["MEAN", "GEOM_MEAN"]] = None,
+):
+    """Return the appropriate new-style class depending on the mode."""
+    warnings.warn(
+        "The use of `baybe.objective` is deprecated and will be disabled in "
+        "a future version. Use the classes defined in `baybe.objectives` instead.",
+        DeprecationWarning,
+    )
+
+    if mode == "SINGLE":
+        if len(targets) != 1:
+            raise ValueError("In 'SINGLE' mode, you must provide exactly one target.")
+        return SingleTargetObjective(targets[0])
+
+    elif mode == "DESIRABILITY":
+        kwargs = {}
+        if weights is not None:
+            kwargs["weights"] = weights
+        if combine_func is not None:
+            kwargs["combine_func"] = combine_func
+        return DesirabilityObjective(targets, **kwargs)
+
+    else:
+        raise ValueError(f"Unknown mode: {mode}")
