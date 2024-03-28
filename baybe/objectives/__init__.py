4afcec88b11b8c8ee78911238d3cf2afbd0e81c0
AdrianSosic <adrian.sosic@merckgroup.com>
2024-03-28 15:18:30 +0100

    Add __init__.py

---
+++ baybe/objectives/__init__.py
@@ -0,0 +1,9 @@
+"""BayBE objectives."""
+
+from baybe.objectives.desirability import DesirabilityObjective
+from baybe.objectives.single import SingleTargetObjective
+
+__all__ = [
+    "SingleTargetObjective",
+    "DesirabilityObjective",
+]
