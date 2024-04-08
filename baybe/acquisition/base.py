f8bae6e5d24abf684715e84c2f2effc2781a9b9c
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-04-08 08:20:35 +0200

    Add UCB deprecations

--- baybe/acquisition/base.py
+++ baybe/acquisition/base.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
+import warnings
 from abc import ABC
-from typing import ClassVar
+from typing import ClassVar, Union
 
 from attrs import define
 
@@ -34,8 +35,37 @@ class AcquisitionFunction(ABC, SerialMixin):
 
 
 # Register de-/serialization hooks
+def _add_deprecation_hook(hook):
+    """Adjust the structuring hook such that it auto-fills missing target types.
+
+    Used for backward compatibility only and will be removed in future versions.
+    """
+
+    def added_deprecation_hook(val: Union[dict, str], cls):
+        if isinstance(val, str):
+            if val == "VarUCB":
+                warnings.warn(
+                    "The use of `VarUCB` is deprecated and will be disabled in a "
+                    "future version. The get the same outcome, use the new UCB class "
+                    "instead with a beta of 100.0.",
+                    DeprecationWarning,
+                )
+                return hook({"type": "UpperConfidenceBound", "beta": 100.0}, cls)
+            elif val == "qVarUCB":
+                warnings.warn(
+                    "The use of `qVarUCB` is deprecated and will be disabled in a "
+                    "future version. The get the same outcome, use the new qUCB class "
+                    "instead with a beta of 100.0.",
+                    DeprecationWarning,
+                )
+                return hook({"type": "qUpperConfidenceBound", "beta": 100.0}, cls)
+        return hook(val, cls)
+
+    return added_deprecation_hook
+
+
 converter.register_structure_hook(
     AcquisitionFunction,
-    get_base_structure_hook(AcquisitionFunction),
+    _add_deprecation_hook(get_base_structure_hook(AcquisitionFunction)),
 )
 converter.register_unstructure_hook(AcquisitionFunction, unstructure_base)
--- baybe/acquisition/utils.py
+++ baybe/acquisition/utils.py
@@ -1,15 +1,41 @@
 """Utilities for acquisition functions."""
-
+import warnings
 from typing import Union
 
+from baybe.acquisition import UpperConfidenceBound, qUpperConfidenceBound
 from baybe.acquisition.base import AcquisitionFunction
 from baybe.utils.basic import get_subclasses
 
 
 def str_to_acqf(name: str, /) -> AcquisitionFunction:
     """Create an ACQF object from a given ACQF name."""
+    if name == "VarUCB":
+        warnings.warn(
+            "The use of `VarUCB` is deprecated and will be disabled in a future "
+            "version. The get the same outcome, use the new UCB class instead with a "
+            "beta of 100.0.",
+            DeprecationWarning,
+        )
+        return UpperConfidenceBound(beta=100.0)
+    elif name == "qVarUCB":
+        warnings.warn(
+            "The use of `qVarUCB` is deprecated and will be disabled in a future "
+            "version. The get the same outcome, use the new qUCB class instead with a "
+            "beta of 100.0.",
+            DeprecationWarning,
+        )
+        return qUpperConfidenceBound(beta=100.0)
+
     acqfs = get_subclasses(AcquisitionFunction)
-    return next(acqf for acqf in acqfs if name in (acqf.__name__, acqf._abbreviation))()
+    acqf = next(
+        (acqf for acqf in acqfs if name in (acqf.__name__, acqf._abbreviation)), None
+    )
+    if acqf is None:
+        raise ValueError(
+            f"{name} is not a recognized name or abbreviation for a BayBE acquisition "
+            f"function."
+        )
+    return acqf()
 
 
 def convert_acqf(acqf: Union[AcquisitionFunction, str], /) -> AcquisitionFunction:
