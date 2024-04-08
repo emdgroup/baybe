bd00a0d56eeb7f04377dfa2fce38feaff5433c93
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-04-08 08:20:56 +0200

    Add simple acqf tests

---
+++ tests/test_acquisition.py
@@ -0,0 +1,22 @@
+"""Acquisition function tests."""
+
+import pytest
+from pytest import param
+
+from baybe.acquisition.base import AcquisitionFunction
+from baybe.recommenders import SequentialGreedyRecommender
+from baybe.utils.basic import get_subclasses
+
+acqfs = [  # by object
+    param(cl(), id=f"{cl.__name__}_obj") for cl in get_subclasses(AcquisitionFunction)
+]
+acqfs += [cl.__name__ for cl in get_subclasses(AcquisitionFunction)]  # by long name
+acqfs += [  # by abbreviation
+    cl._abbreviation for cl in get_subclasses(AcquisitionFunction)
+]
+
+
+@pytest.mark.parametrize("acqf", acqfs)
+def test_deprecated_acqfs2(acqf):
+    """Using the deprecated strategy keyword raises an error."""
+    SequentialGreedyRecommender(acquisition_function_cls=acqf)
