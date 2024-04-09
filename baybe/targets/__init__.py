b7b9b930ea310c48163fe0e3bb86cf1ec6bf46d6
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-09 11:43:55 +0200

    Expire baybe.targets.Objective deprecation

--- baybe/targets/__init__.py
+++ baybe/targets/__init__.py
@@ -1,12 +1,10 @@
 """BayBE targets."""
 
-from baybe.targets.deprecation import Objective
 from baybe.targets.enum import TargetMode, TargetTransformation
 from baybe.targets.numerical import NumericalTarget
 
 __all__ = [
     "NumericalTarget",
-    "Objective",
     "TargetMode",
     "TargetTransformation",
 ]
