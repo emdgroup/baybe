e65e3d2cb24dbf61bab5038e29228bf33fa7ffed
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-09 15:57:01 +0200

    Make interval hypothesis strategy more efficient

--- tests/hypothesis_strategies/utils.py
+++ tests/hypothesis_strategies/utils.py
@@ -1,9 +1,19 @@
 """Hypothesis strategies for generating utility objects."""
 
+from enum import Enum, auto
+
+import hypothesis.extra.numpy as hnp
 import hypothesis.strategies as st
-from hypothesis import assume
 
 from baybe.utils.interval import Interval
+
+
+class IntervalType(Enum):
+    """The possible types of an interval on the real number line."""
+
+    FULLY_UNBOUNDED = auto()
+    HALF_BOUNDED = auto()
+    BOUNDED = auto()
 
 
 @st.composite
@@ -19,18 +29,40 @@ def intervals(
         (exclude_bounded, exclude_half_bounded, exclude_fully_unbounded)
     ), "At least one Interval type must be allowed."
 
-    # Create interval from ordered pair of floats
-    bounds = (
-        st.tuples(st.floats(), st.floats()).map(sorted).filter(lambda x: x[0] < x[1])
-    )
-    interval = Interval.create(draw(bounds))
+    # Draw the interval type from the allowed types
+    type_gate = {
+        IntervalType.FULLY_UNBOUNDED: not exclude_fully_unbounded,
+        IntervalType.HALF_BOUNDED: not exclude_half_bounded,
+        IntervalType.BOUNDED: not exclude_bounded,
+    }
+    allowed_types = [t for t, b in type_gate.items() if b]
+    interval_type = draw(st.sampled_from(allowed_types))
 
-    # Filter excluded intervals
-    if exclude_bounded:
-        assume(not interval.is_bounded)
-    if exclude_half_bounded:
-        assume(not interval.is_half_bounded)
-    if exclude_fully_unbounded:
-        assume(not interval.is_fully_unbounded)
+    # A strategy producing finite floats
+    ffloats = st.floats(allow_infinity=False, allow_nan=False)
 
-    return interval
+    # Draw the bounds depending on the interval type
+    if interval_type is IntervalType.FULLY_UNBOUNDED:
+        bounds = (None, None)
+    elif interval_type is IntervalType.HALF_BOUNDED:
+        bounds = draw(
+            st.sampled_from(
+                [
+                    (None, draw(ffloats)),
+                    (draw(ffloats), None),
+                ]
+            )
+        )
+    elif interval_type is IntervalType.BOUNDED:
+        bounds = draw(
+            hnp.arrays(
+                dtype=float,
+                shape=(2,),
+                elements=ffloats,
+                unique=True,
+            ).map(sorted)
+        )
+    else:
+        raise RuntimeError("This line should be unreachable.")
+
+    return Interval.create(bounds)
