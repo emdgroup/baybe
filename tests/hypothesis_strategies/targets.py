05248e141fb4d1d05bf48c2ad037d6a5b6f1497c
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-11 14:24:32 +0200

    Add type hint and docstring to numerical_targets strategy

--- tests/hypothesis_strategies/targets.py
+++ tests/hypothesis_strategies/targets.py
@@ -1,9 +1,12 @@
 """Hypothesis strategies for targets."""
 
+from typing import Optional
+
 import hypothesis.strategies as st
 
 from baybe.targets.enum import TargetMode
 from baybe.targets.numerical import _VALID_TRANSFORMATIONS, NumericalTarget
+from baybe.utils.interval import Interval
 
 from .utils import intervals as st_intervals
 
@@ -12,15 +15,25 @@ target_name = st.text(min_size=1)
 
 
 @st.composite
-def numerical_targets(draw: st.DrawFn, intervals=None):
-    """Generate :class:`baybe.targets.numerical.NumericalTarget`."""
+def numerical_targets(
+    draw: st.DrawFn, bounds_strategy: Optional[st.SearchStrategy[Interval]] = None
+):
+    """Generate :class:`baybe.targets.numerical.NumericalTarget`.
+
+    Args:
+        draw: Hypothesis draw object.
+        bounds_strategy: An optional strategy for generating the target bounds.
+
+    Returns:
+        _type_: _description_
+    """
     name = draw(target_name)
     mode = draw(st.sampled_from(TargetMode))
-    if intervals is None:
-        intervals = st_intervals(
+    if bounds_strategy is None:
+        bounds_strategy = st_intervals(
             exclude_half_bounded=True, exclude_fully_unbounded=mode is TargetMode.MATCH
         )
-    bounds = draw(intervals)
+    bounds = draw(bounds_strategy)
     transformation = draw(st.sampled_from(_VALID_TRANSFORMATIONS[mode]))
 
     return NumericalTarget(
