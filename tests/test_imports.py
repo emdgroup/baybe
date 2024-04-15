33599288045d1db641498745e2909d08e68fec0b
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-15 09:42:35 +0200

    Add test that asserts torch is lazy-loaded

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -2,6 +2,8 @@
 
 import importlib
 import pkgutil
+import subprocess
+import sys
 
 import pytest
 
@@ -21,3 +23,27 @@ def find_modules() -> list[str]:
 def test_imports(module: str):
     """All modules can be imported without throwing errors."""
     importlib.import_module(module)
+
+
+WHITELIST = [
+    "baybe.acquisition.adapter",
+    "baybe.acquisition.partial",
+    "baybe.utils.botorch_wrapper",
+    "baybe.utils.torch",
+]
+"""List of modules that are allowed to import torch."""
+
+
+def test_torch_lazy_loading():
+    """Torch does not appear in the module list after loading BayBE modules."""
+    imps = "\n".join([f"import {i}" for i in find_modules() if i not in WHITELIST])
+    code = "\n".join(
+        [
+            "import sys",
+            f"{imps}",
+            "exit(int('torch' in sys.modules.keys()))",
+        ]
+    )
+    python_interpreter = sys.executable
+    result = subprocess.call([python_interpreter, "-c", code])
+    assert result == 0
