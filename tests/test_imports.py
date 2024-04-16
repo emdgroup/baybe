07422025602df353e85d2d1bb70fe6f82b8b89af
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-16 10:55:50 +0200

    Extract function for code generation of import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -4,8 +4,11 @@ import importlib
 import pkgutil
 import subprocess
 import sys
+from collections.abc import Sequence
 
 import pytest
+
+_EAGER_LOADING_EXIT_CODE = 42
 
 
 def find_modules() -> list[str]:
@@ -17,6 +20,27 @@ def find_modules() -> list[str]:
             path=package.__path__, prefix=package.__name__ + "."
         )
     ]
+
+
+def make_import_check(modules: Sequence[str], target: str) -> str:
+    """Create code that tests if importing the given modules also imports the target.
+
+    Args:
+        modules: The modules to be imported by the created code.
+        target: The target module whose presence is to be checked after the import.
+
+    Returns:
+        str: Code that signals the presence of the target via a non-zero exit code.
+    """
+    imports = "\n".join([f"import {module}" for module in modules])
+    return "\n".join(
+        [
+            "import sys",
+            f"{imports}",
+            f"hit = '{target}' in sys.modules.keys()",
+            f"exit({_EAGER_LOADING_EXIT_CODE} if hit else 0)",
+        ]
+    )
 
 
 @pytest.mark.parametrize("module", find_modules())
@@ -36,14 +60,8 @@ WHITELIST = [
 
 def test_torch_lazy_loading():
     """Torch does not appear in the module list after loading BayBE modules."""
-    imps = "\n".join([f"import {i}" for i in find_modules() if i not in WHITELIST])
-    code = "\n".join(
-        [
-            "import sys",
-            f"{imps}",
-            "exit(int('torch' in sys.modules.keys()))",
-        ]
-    )
+    modules = [i for i in find_modules() if i not in WHITELIST]
+    code = make_import_check(modules, "torch")
     python_interpreter = sys.executable
     result = subprocess.call([python_interpreter, "-c", code])
     assert result == 0
