df0c5582fb7b314329edcf4275c8d45df463fcba
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-17 14:42:00 +0200

    Add ONNX serialization test

--- tests/serialization/test_surrogate_serialization.py
+++ tests/serialization/test_surrogate_serialization.py
@@ -5,14 +5,15 @@ import pytest
 from baybe.surrogates.base import Surrogate
 from baybe.utils.basic import get_subclasses
 
-# TODO: Add serialization test for ONNX surrogate
-surrogates = [
-    cls() for cls in get_subclasses(Surrogate) if cls.__name__ != "CustomONNXSurrogate"
-]
 
+@pytest.mark.parametrize("surrogate_cls", get_subclasses(Surrogate))
+def test_surrogate_serialization(surrogate_cls, onnx_surrogate):
+    """A serialization roundtrip yields an equivalent object."""
+    if surrogate_cls.__name__ == "CustomONNXSurrogate":
+        surrogate = onnx_surrogate
+    else:
+        surrogate = surrogate_cls()
 
-@pytest.mark.parametrize("surrogate", surrogates)
-def test_surrogate_serialization(surrogate):
     string = surrogate.to_json()
     surrogate2 = Surrogate.from_json(string)
-    assert surrogate == surrogate2
+    assert surrogate == surrogate2, (surrogate, surrogate2)
