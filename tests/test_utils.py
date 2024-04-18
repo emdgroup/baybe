0d2cf7b4695f7b419ab7970a09f8330cf09e2e7b
AdrianSosic <adrian.sosic@merckgroup.com>
2024-04-18 14:44:19 +0200

    Refactor test parametrization and use nonzero target

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,21 +6,24 @@ from pytest import param
 
 from baybe.utils.numerical import closest_element
 
+_TARGET = 1337
+_CLOSEST = _TARGET + 0.1
+
 
 @pytest.mark.parametrize(
     "as_ndarray", [param(False, id="list"), param(True, id="array")]
 )
 @pytest.mark.parametrize(
-    ("array", "target", "expected"),
+    "array",
     [
-        param(0, 0.1, 0, id="0D"),
-        param([0, 1], 0.1, 0, id="1D"),
-        param([[2, 3], [0, 1]], 0.1, 0, id="2D"),
+        param(_CLOSEST, id="0D"),
+        param([0, _CLOSEST], id="1D"),
+        param([[2, 3], [0, _CLOSEST]], id="2D"),
     ],
 )
-def test_closest_element(as_ndarray, array, target, expected):
+def test_closest_element(as_ndarray, array):
     """The closest element can be found irrespective of the input type."""
     if as_ndarray:
         array = np.asarray(array)
-    actual = closest_element(array, target)
-    assert actual == expected, (actual, expected)
+    actual = closest_element(array, _TARGET)
+    assert actual == _CLOSEST, (actual, _CLOSEST)
