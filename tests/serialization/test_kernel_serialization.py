12a80f16b9316b5c6973836687e061cbe17b070f
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2024-04-26 15:23:58 +0200

    Include new kernel strategies in tests

--- tests/serialization/test_kernel_serialization.py
+++ tests/serialization/test_kernel_serialization.py
@@ -2,12 +2,12 @@
 
 from hypothesis import given
 
-from baybe.kernels import MaternKernel
-from tests.hypothesis_strategies.kernels import matern_kernels
+from baybe.kernels.base import Kernel
+from tests.hypothesis_strategies.kernels import kernels
 
 
-@given(matern_kernels)
-def test_matern_kernel_roundtrip(kernel: MaternKernel):
+@given(kernels)
+def test_matern_kernel_roundtrip(kernel: Kernel):
     string = kernel.to_json()
-    kernel2 = MaternKernel.from_json(string)
+    kernel2 = Kernel.from_json(string)
     assert kernel == kernel2, (kernel, kernel2)
