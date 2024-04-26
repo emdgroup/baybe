81b422a9bc9b56c055daedf73c2ce38a4bb55021
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-04-26 14:17:26 +0200

    Reorder acqf hypothesis according to complexity

--- tests/hypothesis_strategies/acquisition.py
+++ tests/hypothesis_strategies/acquisition.py
@@ -4,6 +4,8 @@ import hypothesis.strategies as st
 
 from baybe.acquisition import (
     ExpectedImprovement,
+    LogExpectedImprovement,
+    PosteriorMean,
     ProbabilityOfImprovement,
     UpperConfidenceBound,
     qExpectedImprovement,
@@ -15,19 +17,22 @@ from baybe.acquisition import (
     qUpperConfidenceBound,
 )
 
+# These acqfs are ordered roughly according to increasing complexity
 acquisition_functions = st.one_of(
     st.builds(ExpectedImprovement),
-    st.builds(qExpectedImprovement),
-    st.builds(qLogExpectedImprovement),
-    st.builds(qNoisyExpectedImprovement),
-    st.builds(qLogNoisyExpectedImprovement),
     st.builds(ProbabilityOfImprovement),
-    st.builds(qProbabilityOfImprovement),
-    st.builds(qSimpleRegret),
     st.builds(
         UpperConfidenceBound, beta=st.floats(min_value=0.0, allow_infinity=False)
     ),
+    st.builds(PosteriorMean),
+    st.builds(LogExpectedImprovement),
+    st.builds(qExpectedImprovement),
+    st.builds(qProbabilityOfImprovement),
     st.builds(
         qUpperConfidenceBound, beta=st.floats(min_value=0.0, allow_infinity=False)
     ),
+    st.builds(qSimpleRegret),
+    st.builds(qLogExpectedImprovement),
+    st.builds(qNoisyExpectedImprovement),
+    st.builds(qLogNoisyExpectedImprovement),
 )
