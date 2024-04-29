16db826c002a66ddc733f495560cc9845bff4221
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-04-29 12:05:46 +0200

    Adjust seq greedy continuous error handling

--- baybe/recommenders/pure/bayesian/sequential_greedy.py
+++ baybe/recommenders/pure/bayesian/sequential_greedy.py
@@ -118,34 +118,47 @@ class SequentialGreedyRecommender(BayesianRecommender):
         subspace_continuous: SubspaceContinuous,
         batch_size: int,
     ) -> pd.DataFrame:
-        # See base class.
+        """Generate recommendations from a continuous search space.
+
+        Args:
+            subspace_continuous: The continuous subspace from which to generate
+                recommendations.
+            batch_size: The size of the recommendation batch.
+
+        Raises:
+            NoMCAcquisitionFunctionError: If a non-Monte Carlo acquisition function is
+                used with a batch size > 1.
+
+        Returns:
+            A dataframe containing the recommendations as individual rows.
+        """
+        # For batch size > 1, this optimizer needs a MC acquisition function
+        if batch_size > 1 and not self.acquisition_function.is_mc:
+            raise NoMCAcquisitionFunctionError(
+                f"The '{self.__class__.__name__}' only works with Monte Carlo "
+                f"acquisition functions for batch sizes > 1."
+            )
 
         import torch
         from botorch.optim import optimize_acqf
 
-        try:
-            points, _ = optimize_acqf(
-                acq_function=self._botorch_acqf,
-                bounds=torch.from_numpy(subspace_continuous.param_bounds_comp),
-                q=batch_size,
-                num_restarts=5,  # TODO make choice for num_restarts
-                raw_samples=10,  # TODO make choice for raw_samples
-                equality_constraints=[
-                    c.to_botorch(subspace_continuous.parameters)
-                    for c in subspace_continuous.constraints_lin_eq
-                ]
-                or None,  # TODO: https://github.com/pytorch/botorch/issues/2042
-                inequality_constraints=[
-                    c.to_botorch(subspace_continuous.parameters)
-                    for c in subspace_continuous.constraints_lin_ineq
-                ]
-                or None,  # TODO: https://github.com/pytorch/botorch/issues/2042
-            )
-        except AttributeError as ex:
-            raise NoMCAcquisitionFunctionError(
-                f"The '{self.__class__.__name__}' only works with Monte Carlo "
-                f"acquisition functions."
-            ) from ex
+        points, _ = optimize_acqf(
+            acq_function=self._botorch_acqf,
+            bounds=torch.from_numpy(subspace_continuous.param_bounds_comp),
+            q=batch_size,
+            num_restarts=5,  # TODO make choice for num_restarts
+            raw_samples=10,  # TODO make choice for raw_samples
+            equality_constraints=[
+                c.to_botorch(subspace_continuous.parameters)
+                for c in subspace_continuous.constraints_lin_eq
+            ]
+            or None,  # TODO: https://github.com/pytorch/botorch/issues/2042
+            inequality_constraints=[
+                c.to_botorch(subspace_continuous.parameters)
+                for c in subspace_continuous.constraints_lin_ineq
+            ]
+            or None,  # TODO: https://github.com/pytorch/botorch/issues/2042
+        )
 
         # Return optimized points as dataframe
         rec = pd.DataFrame(points, columns=subspace_continuous.param_names)
