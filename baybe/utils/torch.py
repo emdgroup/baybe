d6e75daff3d7fd6822667f9ca5b5788e902d58b3
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-05-02 18:37:33 +0200

    Add torch env var

--- baybe/utils/torch.py
+++ baybe/utils/torch.py
@@ -1,6 +1,18 @@
 """Torch utilities shipped as separate module for lazy-loading."""
 
+
+import os
+
 import torch
 
-DTypeFloatTorch = torch.float64
+from baybe.utils.boolean import strtobool
+
+VARNAME_TORCH_USE_SINGLE_PRECISION = "BAYBE_TORCH_USE_SINGLE_PRECISION"
+"""Environment variable name for enforcing single precision in torch."""
+
+DTypeFloatTorch = (
+    torch.float32
+    if strtobool(os.environ.get(VARNAME_TORCH_USE_SINGLE_PRECISION, "False"))
+    else torch.float64
+)
 """Floating point data type used for torch tensors."""
