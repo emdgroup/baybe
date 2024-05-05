1fbc7bed8dacbf1c604d8c2174430d71468f7bbf
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-05-06 00:05:06 +0200

    Add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 from pytest import param
 
+from baybe.utils.memory import bytes_to_human_readable, estimate_searchspace_size
 from baybe.utils.numerical import closest_element
 
 _TARGET = 1337
@@ -27,3 +28,30 @@ def test_closest_element(as_ndarray, array):
         array = np.asarray(array)
     actual = closest_element(array, _TARGET)
     assert actual == _CLOSEST, (actual, _CLOSEST)
+
+
+def test_searchspace_memory_estimate(searchspace):
+    """The memory estimate doesn't differ by more than 5% from the actual memory."""
+    estimate = estimate_searchspace_size(searchspace.parameters)
+    estimate_exp = estimate["Exp_Rep_Size"]
+    estimate_comp = estimate["Comp_Rep_Size"]
+    estimate_exp_unit = estimate["Exp_Rep_Unit"]
+    estimate_comp_unit = estimate["Comp_Rep_Unit"]
+
+    actual_exp, actual_exp_unit = bytes_to_human_readable(
+        searchspace.discrete.exp_rep.memory_usage(deep=True, index=False).sum()
+    )
+    actual_comp, actual_comp_unit = bytes_to_human_readable(
+        searchspace.discrete.comp_rep.memory_usage(deep=True, index=False).sum()
+    )
+
+    assert estimate_exp_unit == actual_exp_unit, "Exp units differ!"
+    assert estimate_comp_unit == actual_comp_unit, "Comp units differ!"
+    assert 0.95 <= float(estimate_exp) / float(actual_exp) <= 1.05, (
+        estimate_exp,
+        actual_exp,
+    )
+    assert 0.95 <= float(estimate_comp) / float(actual_comp) <= 1.05, (
+        estimate_comp,
+        actual_comp,
+    )
