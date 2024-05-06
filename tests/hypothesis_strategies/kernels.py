c5ff6a8e79baf7ce71cc7e4c03d4efe65ebc228f
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2024-05-06 17:00:21 +0200

    Improve hypothesis strategy

--- tests/hypothesis_strategies/kernels.py
+++ tests/hypothesis_strategies/kernels.py
@@ -9,17 +9,20 @@ from ..hypothesis_strategies.priors import priors
 matern_kernels = st.builds(
     MaternKernel,
     nu=st.sampled_from((0.5, 1.5, 2.5)),
-    lengthscale_prior=st.one_of(st.none(), priors),
+    lengthscale_prior=priors,
+    lengthscale_prior_initial_value=st.floats(min_value=0, exclude_min=True),
 )
-"""A strategy that generates Matern kernels."""
+"""A strategy that generates matern kernels."""
 
-base_kernels = matern_kernels
+
+base_kernels = st.one_of([matern_kernels])
 """A strategy that generates base kernels to be used within more complex kernels."""
 
 scale_kernels = st.builds(
     ScaleKernel,
     base_kernel=base_kernels,
-    outputscale_prior=st.one_of(st.none(), priors),
+    outputscale_prior=priors,
+    outputscale_prior_initial_value=st.floats(min_value=0, exclude_min=True),
 )
 """A strategy that generates scale kernels."""
 
