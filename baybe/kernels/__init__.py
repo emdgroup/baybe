d28ef32c9b7da38808857e7984e012db9ee3940c
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-05-17 18:05:16 +0200

    Update kernel __init__.py

--- baybe/kernels/__init__.py
+++ baybe/kernels/__init__.py
@@ -1,11 +1,29 @@
 """Kernels for Gaussian process surrogate models."""
 
-from baybe.kernels.basic import MaternKernel
+from baybe.kernels.basic import (
+    CosineKernel,
+    LinearKernel,
+    MaternKernel,
+    PeriodicKernel,
+    PiecewisePolynomialKernel,
+    PolynomialKernel,
+    RBFKernel,
+    RFFKernel,
+    RQKernel,
+)
 from baybe.kernels.composite import AdditiveKernel, ProductKernel, ScaleKernel
 
 __all__ = [
     "AdditiveKernel",
+    "CosineKernel",
+    "LinearKernel",
     "MaternKernel",
+    "PeriodicKernel",
+    "PiecewisePolynomialKernel",
+    "PolynomialKernel",
     "ProductKernel",
+    "RBFKernel",
+    "RFFKernel",
+    "RQKernel",
     "ScaleKernel",
 ]
