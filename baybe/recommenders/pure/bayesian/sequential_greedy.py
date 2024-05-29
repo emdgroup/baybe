c11f46e421f511048bb87960a48a9373541d5d5f
AdrianSosic <adrian.sosic@merckgroup.com>
2024-05-29 11:52:45 +0200

    Use class instead of function for deprecation
    Using a class automatically handles serialization

--- baybe/recommenders/pure/bayesian/sequential_greedy.py
+++ baybe/recommenders/pure/bayesian/sequential_greedy.py
@@ -2,15 +2,19 @@
 
 import warnings
 
+from attrs import define
+
 from baybe.recommenders.pure.bayesian.botorch import BotorchRecommender
 
 
-def SequentialGreedyRecommender(*args, **kwargs):
+@define
+class SequentialGreedyRecommender(BotorchRecommender):
     """A :class:`baybe.recommenders.pure.bayesian.botorch.BotorchRecommender` alias for backward compatibility."""  # noqa: D401, E501
-    warnings.warn(
-        f"The class `SequentialGreedyRecommender` has been deprecated and will be "
-        f"removed in a future version. Please use `{BotorchRecommender.__name__}` for "
-        f"a one-to-one replacement.",
-        DeprecationWarning,
-    )
-    return BotorchRecommender(*args, **kwargs)
+
+    def __attrs_pre_init__(self):
+        warnings.warn(
+            f"The class `SequentialGreedyRecommender` has been deprecated and will be "
+            f"removed in a future version. Please use `{BotorchRecommender.__name__}` "
+            f"for a one-to-one replacement.",
+            DeprecationWarning,
+        )
