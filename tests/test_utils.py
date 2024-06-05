8e48e212b47e0c734b958c8f7e58805171d49a86
Rim Rihana <rim.rihana@merckgroup.com>
2024-06-05 10:38:10 +0200

    Add register_hook tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,37 @@
 """Tests for utilities."""
 
+from contextlib import nullcontext
+
 import numpy as np
 import pytest
 from pytest import param
 
+from baybe.utils.basic import register_hook
 from baybe.utils.memory import bytes_to_human_readable
 from baybe.utils.numerical import closest_element
 
 _TARGET = 1337
 _CLOSEST = _TARGET + 0.1
+
+
+def base_func(arg1: str, arg2: int):
+    pass
+
+
+def func_with_default_values(arg1: str, arg2: int = 1):
+    pass
+
+
+def func_without_annotations(arg1, arg2):
+    pass
+
+
+def func_with_different_order(arg2: int, arg1: str):
+    pass
+
+
+def func_with_different_names(arg: int, arg3: str):
+    pass
 
 
 @pytest.mark.parametrize(
@@ -35,3 +58,43 @@ def test_memory_human_readable_conversion():
     assert bytes_to_human_readable(1024) == (1.0, "KB")
     assert bytes_to_human_readable(1024**2) == (1.0, "MB")
     assert bytes_to_human_readable(4.3 * 1024**4) == (4.3, "TB")
+
+
+@pytest.mark.parametrize(
+    ("target, hook, error"),
+    [
+        param(base_func, func_with_default_values, None, id="hook_with_default_values"),
+        param(
+            func_with_default_values, base_func, None, id="target_with_default_values"
+        ),
+        param(base_func, func_without_annotations, None, id="hook_has_no_annotations"),
+        param(
+            base_func,
+            func_with_different_order,
+            TypeError,
+            id="hook_has_different_signature_order",
+        ),
+        param(
+            func_with_different_order,
+            base_func,
+            TypeError,
+            id="target_has_different_signature_order",
+        ),
+        param(
+            base_func,
+            func_with_different_names,
+            TypeError,
+            id="hook_has_different_names",
+        ),
+        param(
+            func_with_different_names,
+            base_func,
+            TypeError,
+            id="target_has_different_names",
+        ),
+    ],
+)
+def test_register_hook(target, hook, error):
+    """Test the behavior of the register_hook function with different scenarios."""
+    with pytest.raises(error) if error is not None else nullcontext():
+        register_hook(target, hook)
