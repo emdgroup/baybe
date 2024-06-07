f1379abae8cce49040345d0b9ada1a67b467f9c5
AdrianSosic <adrian.sosic@merckgroup.com>
2024-06-07 10:22:43 +0200

    Refactor cardinality tests

--- tests/test_cardinality_constraint.py
+++ tests/test_cardinality_constraint.py
@@ -1,6 +1,9 @@
 """Tests for the continuous cardinality constraint."""
 
+from itertools import combinations
+
 import numpy as np
+import pytest
 
 from baybe.constraints.continuous import (
     ContinuousCardinalityConstraint,
@@ -12,111 +15,105 @@ from baybe.recommenders.pure.nonpredictive.sampling import RandomRecommender
 from baybe.searchspace.core import SearchSpace
 
 
-def get_searchspace(n_param: int, min_nonzeros: int, max_nonzeros: int) -> SearchSpace:
-    """Prepare a searchspace with cardinality constraints."""
-    # parameters and constraints
+def _get_searchspace(
+    n_parameters: int, min_cardinality: int, max_cardinality: int
+) -> SearchSpace:
+    """Create a unit-cube searchspace with cardinality constraint on all parameters."""
     parameters = [
         NumericalContinuousParameter(name=f"x_{i}", bounds=(0, 1))
-        for i in range(n_param)
+        for i in range(n_parameters)
     ]
     constraints = [
         ContinuousCardinalityConstraint(
-            parameters=[f"x_{i}" for i in range(n_param)],
-            min_cardinality=min_nonzeros,
-            max_cardinality=max_nonzeros,
+            parameters=[f"x_{i}" for i in range(n_parameters)],
+            min_cardinality=min_cardinality,
+            max_cardinality=max_cardinality,
         )
     ]
-
-    # search space
     searchspace = SearchSpace.from_product(parameters, constraints)
     return searchspace
 
 
-def test_samples_random():
-    """Random samples under cardinality and linear equality constraints."""
-    # Settings
+def test_sampling():
+    """
+    Polytope sampling with cardinality constraints respects all involved constraints
+    and produces distinct samples.
+    """  # noqa
     N_PARAMETERS = 6
-    MAX_NONZERO = 1
-    MIN_NONZERO = 0
-    N_POINTS = 10
+    MAX_NONZERO = 4
+    MIN_NONZERO = 2
+    N_POINTS = 20
     TOLERANCE = 1e-3
-    RHS = 1.0
 
-    # prepare search space
     parameters = [
         NumericalContinuousParameter(name=f"x_{i+1}", bounds=(0, 1))
         for i in range(N_PARAMETERS)
     ]
-
-    # constraints
+    params_equality = ["x_1", "x_2", "x_3", "x_4"]
+    coeffs_equality = [0.9, 0.6, 2.8, 6.1]
+    rhs_equality = 4.2
+    params_inequality = ["x_1", "x_2", "x_5", "x_6"]
+    coeffs_inequality = [4.7, 1.4, 4.6, 8.6]
+    rhs_inequality = 1.3
+    params_cardinality = ["x_1", "x_2", "x_3", "x_5"]
     constraints = [
         ContinuousLinearEqualityConstraint(
-            parameters=["x_1", "x_2", "x_3", "x_4"],
-            coefficients=[1.0, 1.0, 1.0, 1.0],
-            rhs=RHS,
+            parameters=params_equality, coefficients=coeffs_equality, rhs=rhs_equality
         ),
         ContinuousLinearInequalityConstraint(
-            parameters=["x_1", "x_2", "x_5", "x_6"],
-            coefficients=[1.0, 1.0, 1.0, 1.0],
-            rhs=RHS,
+            parameters=params_inequality,
+            coefficients=coeffs_inequality,
+            rhs=rhs_equality,
         ),
         ContinuousCardinalityConstraint(
-            parameters=["x_1", "x_2"],
+            parameters=params_cardinality,
             max_cardinality=MAX_NONZERO,
             min_cardinality=MIN_NONZERO,
         ),
     ]
-
     searchspace = SearchSpace.from_product(parameters, constraints)
 
-    # draw samples
     samples = searchspace.continuous.sample(N_POINTS)
 
     # Assert that cardinality constraint is fulfilled
-    n_nonzero = np.sum(~np.isclose(samples[["x_1", "x_2"]], 0.0), axis=1)
+    n_nonzero = np.sum(~np.isclose(samples[params_cardinality], 0.0), axis=1)
     assert np.all(n_nonzero >= MIN_NONZERO) and np.all(n_nonzero <= MAX_NONZERO)
 
-    # linear equality constraint is fulfilled
+    # Assert that linear equality constraint is fulfilled
     assert np.allclose(
-        1.0 * samples["x_1"]
-        + 1.0 * samples["x_2"]
-        + 1.0 * samples["x_3"]
-        + 1.0 * samples["x_4"],
-        RHS,
+        np.sum(samples[params_equality] * coeffs_equality, axis=1),
+        rhs_equality,
         atol=TOLERANCE,
     )
 
-    # linear non-equality constraint is fulfilled
+    # Assert that linear non-equality constraint is fulfilled
     assert (
-        (
-            1.0 * samples["x_1"]
-            + 1.0 * samples["x_2"]
-            + 1.0 * samples["x_5"]
-            + 1.0 * samples["x_6"]
-        )
-        .ge(RHS - TOLERANCE)
+        np.sum(samples[params_inequality] * coeffs_inequality, axis=1)
+        .ge(rhs_inequality - TOLERANCE)
         .all()
     )
 
-    # samples are not identical
-    assert not (samples.nunique(axis=0) == 1).all()
+    # Assert that samples are not identical
+    assert len(samples.drop_duplicates()) == N_POINTS
 
 
-def test_random_recommender_for_cardinality_constraint():
+# Combinations of cardinalities to be tested
+_cardinalities = sorted(combinations(range(0, 10), 2))
+
+
+@pytest.mark.parametrize(
+    "cardinalities", _cardinalities, ids=[str(x) for x in _cardinalities]
+)
+def test_random_recommender_with_cardinality_constraint(cardinalities):
     """
-    Recommendations generated by a Random recommendor under a cardinality constraint
+    Recommendations generated by a `RandomRecommender` under a cardinality constraint
     have the expected number of nonzero elements.
     """  # noqa
-    # Settings
-    N_PARAMETERS = 5
-    MAX_NONZERO = 3
-    MIN_NONZERO = 1
+    N_PARAMETERS = 10
     BATCH_SIZE = 10
+    min_cardinality, max_cardinality = cardinalities
 
-    # prepare search space
-    searchspace = get_searchspace(N_PARAMETERS, MIN_NONZERO, MAX_NONZERO)
-
-    # random recommender
+    searchspace = _get_searchspace(N_PARAMETERS, min_cardinality, max_cardinality)
     recommender = RandomRecommender()
     rec = recommender.recommend(
         searchspace=searchspace,
@@ -125,4 +122,4 @@ def test_random_recommender_for_cardinality_constraint():
 
     # Assert that cardinality constraint is fulfilled
     n_nonzero = np.sum(~np.isclose(rec, 0.0), axis=1)
-    assert np.all(n_nonzero >= MIN_NONZERO) and np.all(n_nonzero <= MAX_NONZERO)
+    assert np.all(n_nonzero >= min_cardinality) and np.all(n_nonzero <= max_cardinality)
