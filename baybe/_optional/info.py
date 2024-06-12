b2b36e1ba787ffffb82e8239bb808c2e4050879a
AdrianSosic <adrian.sosic@merckgroup.com>
2024-06-12 08:49:35 +0200

    Exclude cwd from sys.path for find_spec
    This avoids detecting the "streamlit" folder

--- baybe/_optional/info.py
+++ baybe/_optional/info.py
@@ -1,13 +1,34 @@
 """Information about availability of optional dependencies."""
 
+import os
+import sys
+from contextlib import contextmanager
 from importlib.util import find_spec
 
+
+@contextmanager
+def exclude_sys_path(path: str, /):  # noqa: DOC402, DOC404
+    """Temporarily remove a specified path from `sys.path`.
+
+    Args:
+        path: The path to exclude from the search.
+    """
+    original_sys_path = sys.path[:]
+    if path in sys.path:
+        sys.path.remove(path)
+    try:
+        yield
+    finally:
+        sys.path = original_sys_path
+
+
 # Individual packages
-MORDRED_INSTALLED = find_spec("mordred") is not None
-ONNX_INSTALLED = find_spec("onnxruntime") is not None
-RDKIT_INSTALLED = find_spec("rdkit") is not None
-STREAMLIT_INSTALLED = find_spec("streamlit") is not None
-XYZPY_INSTALLED = find_spec("xyzpy") is not None
+with exclude_sys_path(os.getcwd()):
+    MORDRED_INSTALLED = find_spec("mordred") is not None
+    ONNX_INSTALLED = find_spec("onnxruntime") is not None
+    RDKIT_INSTALLED = find_spec("rdkit") is not None
+    STREAMLIT_INSTALLED = find_spec("streamlit") is not None
+    XYZPY_INSTALLED = find_spec("xyzpy") is not None
 
 # Package combinations
 CHEM_INSTALLED = MORDRED_INSTALLED and RDKIT_INSTALLED
