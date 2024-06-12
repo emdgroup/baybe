7d14e1f6e7bc2038eb60bea823b667af5de77072
AdrianSosic <adrian.sosic@merckgroup.com>
2024-06-12 07:33:39 +0200

    Use issubclass checks for CustomONNXSurrogate

--- tests/serialization/test_surrogate_serialization.py
+++ tests/serialization/test_surrogate_serialization.py
@@ -4,13 +4,14 @@ import pytest
 
 from baybe._optional.info import ONNX_INSTALLED
 from baybe.surrogates.base import Surrogate
+from baybe.surrogates.custom import CustomONNXSurrogate
 from baybe.utils.basic import get_subclasses
 
 
 @pytest.mark.parametrize("surrogate_cls", get_subclasses(Surrogate))
 def test_surrogate_serialization(request, surrogate_cls):
     """A serialization roundtrip yields an equivalent object."""
-    if surrogate_cls.__name__ == "CustomONNXSurrogate":
+    if issubclass(surrogate_cls, CustomONNXSurrogate):
         if not ONNX_INSTALLED:
             pytest.skip("Optional onnx dependency not installed.")
         surrogate = request.getfixturevalue("onnx_surrogate")
