4eac455181c3be8c566afbee9ea1a87b2b94d6a4
AdrianSosic <adrian.sosic@merckgroup.com>
2024-06-17 18:31:28 +0200

    Fix optional dependency flags

--- baybe/_optional/info.py
+++ baybe/_optional/info.py
@@ -27,13 +27,20 @@ with exclude_sys_path(os.getcwd()):
     FLAKE8_INSTALLED = find_spec("flake8") is not None
     MORDRED_INSTALLED = find_spec("mordred") is not None
     ONNX_INSTALLED = find_spec("onnxruntime") is not None
-    PRE_COMMIT_INSTALLED = find_spec("pre-commit") is not None
+    PRE_COMMIT_INSTALLED = find_spec("pre_commit") is not None
     PYDOCLINT_INSTALLED = find_spec("pydoclint") is not None
     RDKIT_INSTALLED = find_spec("rdkit") is not None
     RUFF_INSTALLED = find_spec("ruff") is not None
     STREAMLIT_INSTALLED = find_spec("streamlit") is not None
-    TYPOS_INSTALLED = find_spec("typos") is not None
     XYZPY_INSTALLED = find_spec("xyzpy") is not None
+
+# TODO: `find_spec` does not work for `typos` (probably because it's a rust package),
+#   neither seem any versions of `pip show typos` / `pip freeze | grep typos` /
+#   `any(pkg.name == "typos" for pkg in pkgutils.iter_modules())` called as a subprocess
+#   work in CI (probably because stuff is running inside tox and/or paths need to be
+#   configured properly). As a temporary workaround – since the test collection does not
+#   directly depend on the flag – we thus simply set it to `True`.
+TYPOS_INSTALLED = True
 
 # Package combinations
 CHEM_INSTALLED = MORDRED_INSTALLED and RDKIT_INSTALLED
