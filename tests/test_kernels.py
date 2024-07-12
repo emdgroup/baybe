6e0d799dbe906d447b5dde5cb232f24716d31277
AdrianSosic <adrian.sosic@merckgroup.com>
2024-07-12 11:15:10 +0200

    Add validation for kernel keyword arguments

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -3,26 +3,39 @@
 from typing import Any
 
 import numpy as np
+import torch
 from attrs import asdict, has
 from hypothesis import given
 
 from baybe.kernels.base import Kernel
+from baybe.kernels.composite import AdditiveKernel, ProductKernel, ScaleKernel
 
 from .hypothesis_strategies.kernels import kernels
 
+# TODO: Consider deprecating these attribute names to avoid inconsistencies
 _RENAME_DICT: dict[str, str] = {
     "base_kernels": "kernels",
 }
 """Dictionary for resolving name differences between BayBE and GPyTorch attributes."""
 
 
-def validate_kernel_components(obj: Any, mapped: Any) -> None:
+def validate_kernel_components(obj: Any, mapped: Any, **kwargs) -> None:
     """Validate that all kernel components are translated correctly.
 
     Args:
         obj: An object occurring as part of a BayBE kernel.
         mapped: The corresponding object in the translated GPyTorch kernel.
+        **kwargs: Optional kernel arguments that were passed to the GPyTorch kernel.
     """
+    # Assert that the kernel kwargs are correctly mapped
+    # TODO: Consider introducing intermediate kernel classes or class variables
+    #   to differentiate basic kernels from composite ones
+    if isinstance(obj, Kernel) and not isinstance(
+        obj, (ScaleKernel, AdditiveKernel, ProductKernel)
+    ):
+        for k, v in kwargs.items():
+            assert torch.tensor(getattr(mapped, k)).equal(torch.tensor(v))
+
     # Compare attribute by attribute
     for name, component in asdict(obj, recurse=False).items():
         # If the BayBE component is `None`, the GPyTorch component might not even
@@ -40,18 +53,19 @@ def validate_kernel_components(obj: Any, mapped: Any) -> None:
         # we check only for approximate matches.
         if (mapped_component := getattr(mapped, mapped_name, None)) is None:
             assert name.endswith("_initial_value")
-            assert np.isclose(
-                component, getattr(mapped, name.removesuffix("_initial_value")).item()
+            assert np.allclose(
+                component,
+                getattr(mapped, name.removesuffix("_initial_value")).detach().numpy(),
             )
 
         # If the component is itself another attrs object, recurse
         elif has(component):
-            validate_kernel_components(component, mapped_component)
+            validate_kernel_components(component, mapped_component, **kwargs)
 
         # Same for collections of BayBE objects (coming from composite kernels)
         elif isinstance(component, tuple) and all(has(c) for c in component):
             for c, m in zip(component, mapped_component):
-                validate_kernel_components(c, m)
+                validate_kernel_components(c, m, **kwargs)
 
         # On the lowest component level, simply check for equality
         else:
@@ -62,5 +76,14 @@ def validate_kernel_components(obj: Any, mapped: Any) -> None:
 def test_kernel_assembly(kernel: Kernel):
     """Turning a BayBE kernel into a GPyTorch kernel raises no errors and all its
     components are translated correctly."""  # noqa
-    k = kernel.to_gpytorch()
-    validate_kernel_components(kernel, k)
+    # Create some arbitrary kernel kwargs to ensure that they are correctly translated
+    kwargs = dict(
+        ard_num_dims=np.random.randint(0, 32),
+        batch_shape=torch.Size(
+            [np.random.randint(0, 4) for _ in range(np.random.randint(0, 4))]
+        ),
+        active_dims=[np.random.randint(0, 4) for _ in range(np.random.randint(0, 4))],
+    )
+
+    k = kernel.to_gpytorch(**kwargs)
+    validate_kernel_components(kernel, k, **kwargs)
