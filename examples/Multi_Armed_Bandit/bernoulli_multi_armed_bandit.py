7f5a4b3ad6d7fef9d47536a628a16f2e9740f5b7
AdrianSosic <adrian.sosic@merckgroup.com>
2024-08-28 11:25:40 +0200

    Strip example to bare minimum version

--- examples/Multi_Armed_Bandit/bernoulli_multi_armed_bandit.py
+++ examples/Multi_Armed_Bandit/bernoulli_multi_armed_bandit.py
@@ -1,165 +1,175 @@
 ## Bernoulli Multi Armed Bandit
 
-# This example shows how to use the Bernoulli multi armed bandit surrogate for use cases like A/B Testing.
-# Imagin you have 5 versions of something (e.g., a marketing campaign) that you would like to test against each other.
-# The Multi-Armed Banit can help you to identify the effect size (e.g., click-through rate) or optimize the effect size
-# while the experiment is running. This is done by recommending a version of the marketing campaign to show to each new user.
+# The goal of this example is twofold:
+# - It shows how to use the {class}`~baybe.surrogates.bandit.BernoulliMultiArmedBanditSurrogate` for use cases like A/B testing.
+# - It demonstrates the difference between **optimization** and **active learning** in this context, by employing different acquisition functions chosen for each purpose.
+#   Results are compared in terms of the achieved optimality of the resulting policy and the induced system identification error.
 
-import os
+### Imports
+
+from dataclasses import dataclass
+from typing import NamedTuple
 
 import numpy as np
-from multi_armed_bandit_model import MultiArmedBanditModel
-from scipy.stats import bernoulli, beta
-from torch import Tensor
-from utils import create_mab_plot, max_rv_distribution
+from scipy.stats import bernoulli
 
 from baybe import Campaign
 from baybe.acquisition import PosteriorStandardDeviation, qThompsonSampling
-from baybe.objectives import SingleTargetObjective
+from baybe.acquisition.base import AcquisitionFunction
 from baybe.parameters import CategoricalParameter
 from baybe.recommenders import (
     BotorchRecommender,
-    FPSRecommender,
+    RandomRecommender,
     TwoPhaseMetaRecommender,
 )
-from baybe.searchspace import SearchSpace
 from baybe.surrogates import BernoulliMultiArmedBanditSurrogate
 from baybe.targets import BinaryTarget
 
+### An Imaginary Use Case
+
+# Bandit models are a great choice when being faced with multiple (finite) options to choose from.
+# Imagine yourself running a marketing campaign where you need to decide which version of marketing material to present to your customers.
+# As in all other Bayesian optimization contexts, you are confronted with the classic exploration-exploitation dilemma:
+# - **Exploitation:** On the one hand, you want to choose the most effective option right from the start.
+# - **Exploration:** On the other hand, you cannot know in advance how effective each option is until you have tested it.
+#
+# Bandit models offer a principled approach to handling this dilemma.
+# They can help you determine the effect size (e.g., the click-through rate) for your marketing options or optimize the effect size
+# while the campaign is still running. Both goals are achieved by dynamically selecting the version of the marketing campaign that is shown to each customer.
+
+### The Black Box Model
+
+# Abstractly, we can think of our problem as a black box system with a finite number of actions that can be triggered.
+# The image that comes to mind is that of a slot machine with multiple arms that can be pulled (hence the name "multi-armed bandit"), each offering a different win rate.
+# You can think of each arm as representing a version of the marketing campaign, and the corresponding win rates would be the true effect size of each marketing material version.
+
+# We can build such a black box system with just a few lines of code:
+
+
+@dataclass
+class MultiArmedBandit:
+    """A stateless black box system offering a finite number of actions (= arms)."""
+
+    win_rates: list[float]
+    """Win rates associated with the arms."""
+
+    def draw_arm(self, arm_index: int) -> bool:
+        """Draw a reward by pulling the specified arm."""
+        return bernoulli(self.win_rates[arm_index]).rvs()
+
+
 ### Setup
 
-# We are using a 5-armed bandit in this example. The bandit arms have a hard-coded win rate for now.
-# You can think of each arm representing one version of the marketing campaign, and the win rates would be
-# the true effect size of each version.
-# As acquisition functions, we are using ThompsonSampling for the online optimization. For the active learning
-# task, we are using the PosteriorStandardDeviation acquisition function.
+# For our example, we use a system with a fixed collection of win rates, which we
+# assume are unknown to us:
 
-N_ARMS = 5
-SMOKE_TEST = "SMOKE_TEST" in os.environ
-RENDER = not SMOKE_TEST
-N_MC_RUNS = 10 if not SMOKE_TEST else 3
-N_ITERATIONS = 300 if not SMOKE_TEST else 50
-acqfs = [
-    # Online optimization (maximizing the reward)
-    qThompsonSampling(),
-    # Active learning (learning the effect size per arm)
-    PosteriorStandardDeviation(),
+WIN_RATES = [0.3, 0.4, 0.6, 0.65, 0.8]
+BANDIT = MultiArmedBandit(WIN_RATES)
+
+# Given this target system, we consider the following two tasks:
+# - **Online optimization:** Maximizing the total reward accumulated during a campaign
+# - **Active learning:** Estimating the win rates of all arms
+
+# Solving these task can be achieved through an appropriate choice of acquisition function:
+# For online optimization, we use {class}`~baybe.acquisition.acqfs.qThompsonSampling`.
+# For active learning, we switch to {class}`~baybe.acquisition.acqfs.PosteriorStandardDeviation`.
+
+# To estimate the corresponding effects, we simulate each campaign for a certain number of steps and repeat this process in multiple Monte Carlo runs:
+
+ACQFS = [
+    qThompsonSampling(),  # Online optimization
+    PosteriorStandardDeviation(),  # Active learning
 ]
-real_means = Tensor([0.3, 0.4, 0.6, 0.65, 0.8])
-mab = MultiArmedBanditModel(
-    real_distributions=[bernoulli(real_mean) for real_mean in real_means]
-)
-print("real means", real_means)
+N_MC_RUNS = 10
+N_ITERATIONS = 200
 
-### Campaign
 
-# We are using the BinaryTarget as we are modeling a Bernoulli reward (click or no click).
-# The search space has one categorical parameter to model the arms of the bandit.
+### Building the Model
 
-# To test the approach we are running multiple Monte Carlo runs per acquisition function.
+# To model the rewards obtained from the bandit, we use a {class}`~baybe.targets.binary.BinaryTarget`.
+# The corresponding search space is spanned by a {class}`~baybe.parameters.categorical.CategoricalParameter` describing the bandit's arms.
 
-for acqf in acqfs:
-    np.random.seed(0)
+n_arms = len(WIN_RATES)
+objective = BinaryTarget(name="clicked").to_objective()
+searchspace = CategoricalParameter(
+    name="Bandit Arm", values=[f"arm_{i}" for i in range(n_arms)]
+).to_searchspace()
+surrogate = BernoulliMultiArmedBanditSurrogate()
 
-    mc_estimated_means_over_iterations = []
-    mc_regret_over_iterations = []
-    mc_rewards = []
 
-    # only taking posterior params for visualization of the last run
-    posterior_params = None
+# For each simulation, we report the trajectory of earned rewards and the estimated win rates of the bandit arms:
 
-    for mc_run_i in range(N_MC_RUNS):
-        target = BinaryTarget(name="clicked")
-        objective = SingleTargetObjective(target=target)
-        parameters = [
-            CategoricalParameter(
-                name="Bandit Arms",
-                values=[f"arm_{i}" for i in range(N_ARMS)],
-            )
-        ]
-        searchspace = SearchSpace.from_product(parameters)
-        surrogate = BernoulliMultiArmedBanditSurrogate()
-        recommender = TwoPhaseMetaRecommender(
-            initial_recommender=FPSRecommender(
-                allow_repeated_recommendations=True,
-                allow_recommending_already_measured=True,
-            ),
-            recommender=BotorchRecommender(
-                surrogate_model=surrogate,
-                allow_repeated_recommendations=True,
-                allow_recommending_already_measured=True,
-                acquisition_function=acqf,
-            ),
-        )
+
+class SimulationResult(NamedTuple):
+    """Result of a bandit campaign simulation."""
+
+    earned_rewards: np.ndarray
+    estimated_win_rates: np.ndarray
+
+
+# Putting all pieces together, we define a function to simulate our campaign under a given acquisition strategy:
+
+
+def simulate(acqf: AcquisitionFunction) -> SimulationResult:
+    """Simulate the campaign with with the given acquisition function."""
+    recommender = TwoPhaseMetaRecommender(
+        initial_recommender=RandomRecommender(
+            allow_repeated_recommendations=True,
+            allow_recommending_already_measured=True,
+        ),
+        recommender=BotorchRecommender(
+            surrogate_model=surrogate,
+            acquisition_function=acqf,
+            allow_repeated_recommendations=True,
+            allow_recommending_already_measured=True,
+        ),
+    )
+
+    # Containers for storing the rewards and win rate estimates
+    earned_rewards = np.zeros((N_MC_RUNS, N_ITERATIONS))
+    estimated_win_rates = np.zeros((N_MC_RUNS, n_arms))
+
+    for mc in range(N_MC_RUNS):
         campaign = Campaign(searchspace, objective, recommender)
 
-        total_reward = 0
-        estimated_means = []
-        posterior_params = []
-        regrets = []
         for i in range(N_ITERATIONS):
             df = campaign.recommend(batch_size=1)
-
-            reward = mab.sample(df.index.tolist())
-            total_reward += sum(reward)
+            recommended_arm_idx = int(df.iloc[0, 0].split("_")[1])
+            reward = BANDIT.draw_arm(recommended_arm_idx)
+            earned_rewards[mc, i] = reward
             df["clicked"] = reward
             campaign.add_measurements(df)
 
-            if i > 0:
-                regrets.append((i + 1) * max(real_means) - total_reward)
-                posterior_means = surrogate.posterior_means().tolist()
-                estimated_means.append(posterior_means)
-                posterior_params.append(surrogate._posterior_beta_parameters())
+        estimated_win_rates[mc] = surrogate.posterior_means()
 
-        mc_estimated_means_over_iterations.append(estimated_means)
-        mc_regret_over_iterations.append(regrets)
-        mc_rewards.append(total_reward)
+    return SimulationResult(earned_rewards, estimated_win_rates)
 
-    mc_estimated_means_over_iterations = Tensor(mc_estimated_means_over_iterations)
-    mc_regret_over_iterations = Tensor(mc_regret_over_iterations)
-    mc_regret_mean = mc_regret_over_iterations.mean(dim=0)
-    mc_regret_std = mc_regret_over_iterations.std(dim=0)
-    mc_estimated_means_squared_error = (
-        (mc_estimated_means_over_iterations - real_means).pow(2).sum(dim=-1)
-    )
-    mc_estimated_means_squared_error_mean = mc_estimated_means_squared_error.mean(dim=0)
-    mc_estimated_means_squared_error_std = mc_estimated_means_squared_error.std(dim=0)
 
-    max_rv_distribution_over_iterations = np.array(
-        [
-            max_rv_distribution(posterior_param.T, beta)
-            for posterior_param in posterior_params
-        ]
-    )
+### Running the Simulation
 
-    print("-" * 10)
-    print(f"Ran for {N_MC_RUNS} MC runs with {acqf.__class__.__name__}.")
-    print("Optimal expected reward", (max(real_means) * N_ITERATIONS).item())
-    print("Mean reward", sum(mc_rewards) / N_MC_RUNS)
-    print("Mean regret last iteration", mc_regret_mean[-1].item())
-    print("MSE last iteration", mc_estimated_means_squared_error_mean[-1].item())
+# With our model in place, we can now simulate the campaign for both acquisition functions.
+# The success of our two objectives is quantified using the following metrics:
+# - For online optimization, we look at the **empirically achieved win rate** and
+#   compare it to the win rate of the best arm.
+# - For active learning, we report the **mean squared error of the win rates**
+#   estimated at the end of the campaign.
 
-    if RENDER:
-        create_mab_plot(
-            N_ITERATIONS,
-            N_ARMS,
-            mc_regret_mean,
-            mc_regret_std,
-            mc_estimated_means_squared_error_mean,
-            mc_estimated_means_squared_error_std,
-            max_rv_distribution_over_iterations,
-            posterior_params,
-            real_means,
-            estimated_means,
-            acqf,
-            "bernoulli_multi_armed_bandit",
-        )
+for acqf in ACQFS:
+    name = acqf.__class__.__name__
+    result = simulate(acqf)
+    MSE = np.linalg.norm(result.estimated_win_rates - WIN_RATES, "fro") / n_arms
 
-### Outcome
+    print(name)
+    print("-" * len(name))
+    print("Optimal win rate:", max(WIN_RATES))
+    print("Achieved win rate:", result.earned_rewards.mean())
+    print("Mean squared error (last iteration):", MSE)
+    print("\n")
 
-# We observe a smaller regret when using the online optimization approach (Thompson sampling), which makes sense as we
-# are trying to recommend the arm with the largest reward more often. The MSE, on the other hand, is larger than in the
-# active learning approach. As the active learning approach will sample the next action based on the maximum standard
-# deviation, we are concentrating more on learning the effect size than maximizing the reward. Despite those differences,
-# both approaches identify the best arm rather quickly. This can be seen by the $P(X_i > X_{-i})$ plot stating the probability
-# for each arm to generate the maximum reward.
+
+### Discussion
+
+# As expected, we observe a higher achieved win rate for the online optimization strategy, as its underlying acquisition function is designed to recommend the arm with the largest reward most often.
+# This comes at the cost of a higher estimation error for the remaining arms, which are sampled less frequently.
+# In contrast, the active learning strategy attempts to balance the estimation errors across arms, resulting in a significantly smaller overall error.
+# However, in order to do so, it must sample arms with low and high win rates equally often, which reduces the achieved rate and thus the total reward accumulated in the campaign.
