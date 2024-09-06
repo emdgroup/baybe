6a71c730a00515f29e0d6fd93d2dcf27de4d170c
AdrianSosic <adrian.sosic@merckgroup.com>
2024-09-06 11:46:54 +0200

    Move BetaBernoulliMultiArmedBanditSurrogate to subpackage

--- baybe/surrogates/__init__.py
+++ baybe/surrogates/__init__.py
@@ -1,6 +1,6 @@
 """BayBE surrogates."""
 
-from baybe.surrogates.bandit import BetaBernoulliMultiArmedBanditSurrogate
+from baybe.surrogates.bandits import BetaBernoulliMultiArmedBanditSurrogate
 from baybe.surrogates.custom import CustomONNXSurrogate, register_custom_architecture
 from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
 from baybe.surrogates.linear import BayesianLinearSurrogate
