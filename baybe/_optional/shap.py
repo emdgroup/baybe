0c8e94566232f096ddb35b27424e31c1cbd19ac3
Wieczorek, Alexander <Alexander.Wieczorek@empa.ch>
2024-09-29 20:26:14 +0200

    Optional import of shap package.

---
+++ baybe/_optional/shap.py
@@ -0,0 +1,17 @@
+"""Optional SHAP import."""
+
+from baybe.exceptions import OptionalImportError
+
+try:
+    import shap
+except ModuleNotFoundError as ex:
+    raise OptionalImportError(
+        "Feature importance ranking functionality is unavailable "
+        "because 'shap' is not installed. "
+        "Consider installing BayBE with 'polars' dependency, e.g. via "
+        "`pip install baybe[shap]`."
+    ) from ex
+
+__all__ = [
+    "shap",
+]
