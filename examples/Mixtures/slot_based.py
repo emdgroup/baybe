9315451f7fd49d5bca9acfad65d25bd832abb193
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-04 18:13:00 +0200

    Rework slot-based mixture

--- examples/Mixtures/slot_based.py
+++ examples/Mixtures/slot_based.py
@@ -1,14 +1,37 @@
 ## Example for Modelling a Slot-Based Mixture
 
-# Example for imposing sum constraints for discrete parameters.
-# The constraints simulate a situation where we want to mix up to three solvents.
-# However, their respective fractions need to sum up to 100.
-# Also, the solvents should never be chosen twice, which requires various other constraints.
+### Terminology
 
-# This example assumes some basic familiarity with using BayBE.
-# We thus refer to [`campaign`](./../Basics/campaign.md) for a basic example.
+# Modelling a mixture is possible on a non-traditional way with something we refer to as
+# **slot**. A slot consists of one parameter indicating the amount of a substance and
+# another parameter indicating the type of substance (as label) that is in the slot.
+# Contrary to traditional mixture modelling, the total number of parameters is not
+# defined by how many substance choices we have, but by the maximum number of slots we
+# want to allow. For instance, if we want to design a mixture with *up to five*
+# components, we would need 5 slots, i.e. 10 parameters.
 
-### Necessary imports for this example
+# A corresponding search space with three slots could look like this:
+# | Slot1_Label | Slot1_Amount | Slot2_Label | Slot2_Amount | Slot3_Label | Slot3_Amount |
+# |:------------|:-------------|:------------|:-------------|:------------|:-------------|
+# | Solvent1    | 10           | Solvent5    | 20           | Solvent4    | 30           |
+# | Solvent1    | 30           | Solvent8    | 40           | Solvent2    | 30           |
+# | Solvent3    | 20           | Solvent1    | 35           | Solvent9    | 30           |
+# | Solvent2    | 15           | Solvent3    | 10           | Solvent1    | 30           |
+
+# This slot-based representation has one decided advantage compared to traditional
+# modelling: We can utilize BayBE's label encodings for the label parameters. For
+# instance, when mixing small molecules, the
+# [`SubstanceParameter`](baybe.parameters.substance.SubstanceParameter) can be used to
+# smartly encode the slot labels, enabling the algorithm to perform a chemically-aware
+# mixture optimization.
+
+# In this example, we show how to design the search space and the various discrete
+# constraints we need to impose. We simulate a situation where we want to mix up to
+# three solvents, i.e. we will have 3 slots (6 parameters). Their respective amounts
+# need to sum up to 100. Also, a solvents should never be chosen twice, which
+# requires various other constraints.
+
+### Imports
 
 import math
 import os
@@ -23,22 +46,21 @@ from baybe.constraints import (
     DiscreteSumConstraint,
     ThresholdCondition,
 )
-from baybe.objectives import SingleTargetObjective
 from baybe.parameters import NumericalDiscreteParameter, SubstanceParameter
 from baybe.searchspace import SearchSpace
 from baybe.targets import NumericalTarget
 from baybe.utils.dataframe import add_fake_measurements
 
-### Experiment setup
+### Parameter Setup
 
-# This parameter denotes the tolerance with regard to the calculation of the sum.
+# Set some basic settings.
 
-SUM_TOLERANCE = 1.0
-
+SUM_TOLERANCE = 0.1  # The tolerance we allow for the fulfillment of sum constraints
 SMOKE_TEST = "SMOKE_TEST" in os.environ
+RESOLUTION = 5 if SMOKE_TEST else 11  # resolution of the discretization
 
-# This parameter denotes the resolution of the discretization of the parameters
-RESOLUTION = 5 if SMOKE_TEST else 12
+# Create the parameters for the slot labels. Each of our slots offers a choice between
+# 4 solvents.
 
 dict_solvents = {
     "water": "O",
@@ -46,75 +68,110 @@ dict_solvents = {
     "C2": "CC",
     "C3": "CCC",
 }
-solvent1 = SubstanceParameter(name="Solv1", data=dict_solvents, encoding="MORDRED")
-solvent2 = SubstanceParameter(name="Solv2", data=dict_solvents, encoding="MORDRED")
-solvent3 = SubstanceParameter(name="Solv3", data=dict_solvents, encoding="MORDRED")
-
-# Parameters for representing the fraction.
-
-fraction1 = NumericalDiscreteParameter(
-    name="Frac1", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+slot1_label = SubstanceParameter(
+    name="Slot1_Label", data=dict_solvents, encoding="MORDRED"
 )
-fraction2 = NumericalDiscreteParameter(
-    name="Frac2", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+slot2_label = SubstanceParameter(
+    name="Slot2_Label", data=dict_solvents, encoding="MORDRED"
 )
-fraction3 = NumericalDiscreteParameter(
-    name="Frac3", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+slot3_label = SubstanceParameter(
+    name="Slot3_Label", data=dict_solvents, encoding="MORDRED"
 )
 
-parameters = [solvent1, solvent2, solvent3, fraction1, fraction2, fraction3]
+# Create the parameters for the slot amounts.
 
-### Creating the constraint
+slot1_amount = NumericalDiscreteParameter(
+    name="Slot1_Amount", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+)
+slot2_amount = NumericalDiscreteParameter(
+    name="Slot2_Amount", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+)
+slot3_amount = NumericalDiscreteParameter(
+    name="Slot3_Amount", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
+)
 
-# Since the constraints are required for the creation of the searchspace, we create
-# them next.
-# Note that we need a `PermutationInvarianceConstraint` here.
-# The reason is that constraints are normally applied in a specific order.
-# However, the fractions should be invariant under permutations.
-# We thus require an explicit constraint for this.
+# Store all parameters.
+
+parameters = [
+    slot1_label,
+    slot2_label,
+    slot3_label,
+    slot1_amount,
+    slot2_amount,
+    slot3_amount,
+]
+
+### Constraint Setup
+
+# Like for all mixtures, let us ensure that the overall sum of slot amounts is always
+# 100.
+
+sum_constraint = DiscreteSumConstraint(
+    parameters=["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"],
+    condition=ThresholdCondition(threshold=100, operator="=", tolerance=SUM_TOLERANCE),
+)
+
+# We could have a situation where we do not care about the order of addition of
+# components to the mixture. This comes with two additional constraints.
+
+# If there is no order of addition, it does not matter whether we have two slots
+# with the same substance or just one holding the combined amounts of two slots
+# with the same slot ingredient. Thus, let us make sure that no slot contains a
+# duplicate label entry.
+
+no_duplicates_constraint = DiscreteNoLabelDuplicatesConstraint(
+    parameters=["Slot1_Label", "Slot2_Label", "Slot3_Label"]
+)
+
+# Next, we need to take care of permutation invariance. If our order of addition does
+# not matter, the result of exchanging slot 1 with slot 3 does not change the mixture,
+# i.e. the mixture slots are permutation invariant.
+
+# One complication arising for the permutation invariance in this case stems from the
+# fact that we not only have a label per slot, but also a numerical amount. Now if
+# this amount is zero, it actually does not matter what label the slot has (i.e.
+# what substance should be considered for that slot), because we are adding 0 of it to
+# the mixture anyway. In BayBE, we call this a "dependency", i.e. the slot labels
+# depend on the slot amounts and are only relevant if the amount fulfills some
+# condition (in this case "amount > 0"). The `DiscreteDependenciesConstraint` tells
+# the `DiscretePermutationInvarianceConstraint` about these dependencies so that they
+# are correctly included in the filtering process.
 
 perm_inv_constraint = DiscretePermutationInvarianceConstraint(
-    parameters=["Solv1", "Solv2", "Solv3"],
+    parameters=["Slot1_Label", "Slot2_Label", "Slot3_Label"],
     dependencies=DiscreteDependenciesConstraint(
-        parameters=["Frac1", "Frac2", "Frac3"],
+        parameters=["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"],
         conditions=[
             ThresholdCondition(threshold=0.0, operator=">"),
             ThresholdCondition(threshold=0.0, operator=">"),
             ThresholdCondition(threshold=0.0, operator=">"),
         ],
-        affected_parameters=[["Solv1"], ["Solv2"], ["Solv3"]],
+        affected_parameters=[["Slot1_Label"], ["Slot2_Label"], ["Slot3_Label"]],
     ),
 )
 
-# This is now the actual sum constraint
-
-sum_constraint = DiscreteSumConstraint(
-    parameters=["Frac1", "Frac2", "Frac3"],
-    condition=ThresholdCondition(threshold=100, operator="=", tolerance=SUM_TOLERANCE),
-)
-
-# The permutation invariance might create duplicate labels.
-# We thus include a constraint to remove them.
-
-no_duplicates_constraint = DiscreteNoLabelDuplicatesConstraint(
-    parameters=["Solv1", "Solv2", "Solv3"]
-)
+# Store all constraints.
 
 constraints = [perm_inv_constraint, sum_constraint, no_duplicates_constraint]
 
-### Creating the searchspace and the objective
+# ```{admonition} Order of Addition
+# :class: note
+# Whether you need to impose the constraints for removing duplicates and
+# permutation invariance depends on your use case. If the order of addition is relevant
+# to your mixture, there is no permutation invariance and one could argue that
+# duplicates should also be allowed if subsequent steps can add the same substance.
+# ```
+
+### Campaign Setup
 
 searchspace = SearchSpace.from_product(parameters=parameters, constraints=constraints)
-
-objective = SingleTargetObjective(target=NumericalTarget(name="Target_1", mode="MAX"))
-
-### Creating the Campaign
-
+objective = NumericalTarget(name="Target_1", mode="MAX").to_objective()
 campaign = Campaign(searchspace=searchspace, objective=objective)
 
-### Manual verification of the constraint
+### Verification of Constraints
 
-# The following loop performs some recommendations and manually verifies the given constraints.
+# Now let us take a look at some recommendations for this campaign and check whether
+# the constraints we imposed are indeed adhered to.
 
 N_ITERATIONS = 2 if SMOKE_TEST else 3
 for kIter in range(N_ITERATIONS):
@@ -122,8 +179,10 @@ for kIter in range(N_ITERATIONS):
 
     print("## ASSERTS ##")
     print(
-        "No. of searchspace entries where fractions do not sum to 100.0:      ",
-        campaign.searchspace.discrete.exp_rep[["Frac1", "Frac2", "Frac3"]]
+        "No. of searchspace entries where amounts do not sum to 100.0:      ",
+        campaign.searchspace.discrete.exp_rep[
+            ["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"]
+        ]
         .sum(axis=1)
         .apply(lambda x: x - 100.0)
         .abs()
@@ -131,18 +190,26 @@ for kIter in range(N_ITERATIONS):
         .sum(),
     )
     print(
-        "No. of searchspace entries that have duplicate solvent labels:       ",
-        campaign.searchspace.discrete.exp_rep[["Solv1", "Solv2", "Solv3"]]
+        "No. of searchspace entries that have duplicate slot labels:        ",
+        campaign.searchspace.discrete.exp_rep[
+            ["Slot1_Label", "Slot2_Label", "Slot3_Label"]
+        ]
         .nunique(axis=1)
         .ne(3)
         .sum(),
     )
     print(
-        "No. of searchspace entries with permutation-invariant combinations:  ",
-        campaign.searchspace.discrete.exp_rep[["Solv1", "Solv2", "Solv3"]]
+        "No. of searchspace entries with permutation-invariant combinations:",
+        campaign.searchspace.discrete.exp_rep[
+            ["Slot1_Label", "Slot2_Label", "Slot3_Label"]
+        ]
         .apply(frozenset, axis=1)
         .to_frame()
-        .join(campaign.searchspace.discrete.exp_rep[["Frac1", "Frac2", "Frac3"]])
+        .join(
+            campaign.searchspace.discrete.exp_rep[
+                ["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"]
+            ]
+        )
         .duplicated()
         .sum(),
     )
@@ -151,7 +218,12 @@ for kIter in range(N_ITERATIONS):
     # points than intended due to numeric rounding
     print(
         f"No. of unique 1-solvent entries (exp. {math.comb(len(dict_solvents), 1)*1})",
-        (campaign.searchspace.discrete.exp_rep[["Frac1", "Frac2", "Frac3"]] == 0.0)
+        (
+            campaign.searchspace.discrete.exp_rep[
+                ["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"]
+            ]
+            == 0.0
+        )
         .sum(axis=1)
         .eq(2)
         .sum(),
@@ -159,7 +231,12 @@ for kIter in range(N_ITERATIONS):
     print(
         f"No. of unique 2-solvent entries (exp."
         f" {math.comb(len(dict_solvents), 2)*(RESOLUTION-2)})",
-        (campaign.searchspace.discrete.exp_rep[["Frac1", "Frac2", "Frac3"]] == 0.0)
+        (
+            campaign.searchspace.discrete.exp_rep[
+                ["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"]
+            ]
+            == 0.0
+        )
         .sum(axis=1)
         .eq(1)
         .sum(),
@@ -167,7 +244,12 @@ for kIter in range(N_ITERATIONS):
     print(
         f"No. of unique 3-solvent entries (exp."
         f" {math.comb(len(dict_solvents), 3)*((RESOLUTION-3)*(RESOLUTION-2))//2})",
-        (campaign.searchspace.discrete.exp_rep[["Frac1", "Frac2", "Frac3"]] == 0.0)
+        (
+            campaign.searchspace.discrete.exp_rep[
+                ["Slot1_Amount", "Slot2_Amount", "Slot3_Amount"]
+            ]
+            == 0.0
+        )
         .sum(axis=1)
         .eq(0)
         .sum(),
