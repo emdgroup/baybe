5a5afbd67e775e40455293742494ada5817ccddf
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-08 17:06:59 +0200

    Update full lookup example

--- examples/Backtesting/full_lookup.py
+++ examples/Backtesting/full_lookup.py
@@ -96,7 +96,7 @@ objective = SingleTargetObjective(target=NumericalTarget(name="yield", mode="MAX
 # First let us create three campaigns that each use a different chemical encoding to
 # treat substances.
 
-substance_encodings = ["MORDRED", "RDKIT", "ECFP"]
+substance_encodings = ["MORDRED", "PUBCHEM", "ECFP"]
 scenarios = {
     encoding: Campaign(
         searchspace=SearchSpace.from_product(
