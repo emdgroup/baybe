9b4f550d26e5949279e435476f1c9d142595a6d9
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-09 12:14:51 +0200

    Fix enum documentation

--- baybe/parameters/enum.py
+++ baybe/parameters/enum.py
@@ -28,38 +28,103 @@ class SubstanceEncoding(ParameterEncoding):
     """Available encodings for substance parameters."""
 
     ATOMPAIR = "ATOMPAIR"
+    """AtomPairFingerprint from scikit-fingerprints."""
+
     AUTOCORR = "AUTOCORR"
+    """AutocorrFingerprint from scikit-fingerprints."""
+
     AVALON = "AVALON"
+    """AvalonFingerprint from scikit-fingerprints."""
+
     E3FP = "E3FP"
+    """E3FPFingerprint from scikit-fingerprints."""
+
     ECFP = "ECFP"
+    """ECFPFingerprint from scikit-fingerprints."""
+
     MORGAN_FP = "MORGAN_FP"
+    """Deprecated!"""
+
     ERG = "ERG"
+    """ERGFingerprint from scikit-fingerprints."""
+
     ESTATE = "ESTATE"
+    """EStateFingerprint from scikit-fingerprints."""
+
     FUNCTIONALGROUPS = "FUNCTIONALGROUPS"
+    """FunctionalGroupsFingerprint from scikit-fingerprints."""
+
     GETAWAY = "GETAWAY"
+    """GETAWAYFingerprint from scikit-fingerprints."""
+
     GHOSECRIPPEN = "GHOSECRIPPEN"
+    """GhoseCrippenFingerprint from scikit-fingerprints."""
+
     KLEKOTAROTH = "KLEKOTAROTH"
+    """KlekotaRothFingerprint from scikit-fingerprints."""
+
     LAGGNER = "LAGGNER"
+    """LaggnerFingerprint from scikit-fingerprints."""
+
     LAYERED = "LAYERED"
+    """LayeredFingerprint from scikit-fingerprints."""
+
     LINGO = "LINGO"
+    """LingoFingerprint from scikit-fingerprints."""
+
     MACCS = "MACCS"
+    """MACCSFingerprint from scikit-fingerprints."""
+
     MAP = "MAP"
+    """MAPFingerprint from scikit-fingerprints."""
+
     MHFP = "MHFP"
+    """MHFPFingerprint from scikit-fingerprints."""
+
     MORSE = "MORSE"
+    """MORSEFingerprint from scikit-fingerprints."""
+
     MQNS = "MQNS"
+    """MQNsFingerprint from scikit-fingerprints."""
+
     MORDRED = "MORDRED"
+    """MordredFingerprint from scikit-fingerprints."""
+
     PATTERN = "PATTERN"
+    """PatternFingerprint from scikit-fingerprints."""
+
     PHARMACOPHORE = "PHARMACOPHORE"
+    """PharmacophoreFingerprint from scikit-fingerprints."""
+
     PHYSIOCHEMICALPROPERTIES = "PHYSIOCHEMICALPROPERTIES"
+    """PhysiochemicalPropertiesFingerprint from scikit-fingerprints."""
+
     PUBCHEM = "PUBCHEM"
+    """PubChemFingerprint from scikit-fingerprints."""
+
     RDF = "RDF"
+    """RDFFingerprint from scikit-fingerprints."""
+
     RDKIT2DDESCRIPTORS = "RDKIT2DDESCRIPTORS"
+    """RDKit2DDescriptorsFingerprint from scikit-fingerprints."""
+
     RDKIT = "RDKIT"
+    """RDKitFingerprint from scikit-fingerprints."""
+
     SECFP = "SECFP"
+    """SECFPFingerprint from scikit-fingerprints."""
+
     TOPOLOGICALTORSION = "TOPOLOGICALTORSION"
+    """TopologicalTorsionFingerprint from scikit-fingerprints."""
+
     USRCAT = "USRCAT"
+    """USRFingerprint from scikit-fingerprints."""
+
     USR = "USR"
+    """USRCATFingerprint from scikit-fingerprints."""
+
     WHIM = "WHIM"
+    """WHIMFingerprint from scikit-fingerprints."""
 
 
 fingerprint_name_map: dict[str, str] = {
