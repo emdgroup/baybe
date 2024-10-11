3ac857f45bda26e00ddfe4574e265193e5c35c01
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-11 14:30:04 +0200

    Add utility test

---
+++ tests/utils/test_dataframe.py
@@ -0,0 +1,46 @@
+"""Tests for dataframe utilities."""
+
+import warnings
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from baybe.utils.dataframe import df_add_noise_to_degenerate_rows
+
+
+def test_degenerate_rows():
+    """Test noise-based deduplication of degenerate rows."""
+    # Create random dataframe
+    df = pd.DataFrame(np.random.randint(0, 100, size=(5, 3))).astype(float)
+
+    # Manually create some degenerate rows
+    df.loc[1] = df.loc[0]  # Make row 1 identical to row 0
+    df.loc[3] = df.loc[2]  # Make row 3 identical to row 2
+    df.iloc[:, -1] = 50.0  # Make last column constant to test the edge case
+
+    # Add noise
+    df_add_noise_to_degenerate_rows(df)
+
+    # Assert that the utility fixed the degenerate rows
+    assert not df.duplicated().any(), "Degenerate rows were not fixed by the utility."
+
+
+def test_degenerate_rows_invalid_input():
+    """Test that the utility correctly handles invalid input."""
+    # Create random dataframe
+    df = pd.DataFrame(np.random.randint(0, 100, size=(5, 3))).astype(float)
+
+    # Manually create some degenerate rows
+    df.loc[1] = df.loc[0]  # Make row 1 identical to row 0
+    df.loc[3] = df.loc[2]  # Make row 3 identical to row 2
+
+    # Insert invalid data types
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=FutureWarning)
+        df.iloc[:, -1] = "A"
+        df.iloc[1, 2] = "B"
+
+    # Add noise
+    with pytest.raises(TypeError):
+        df_add_noise_to_degenerate_rows(df)
