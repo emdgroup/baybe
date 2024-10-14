6d11d8de5215291857b41104b553187b7f968b5f
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-15 00:12:22 +0200

    Improve text

--- examples/Mixtures/slot_based.py
+++ examples/Mixtures/slot_based.py
@@ -2,7 +2,7 @@
 
 ### Terminology
 
-# Modelling a mixture is possible on a non-traditional way with something we refer to as
+# Modelling a mixture is possible in a non-traditional way with something we refer to as
 # **slot**. A slot consists of one parameter indicating the amount of a substance and
 # another parameter indicating the type of substance (as label) that is in the slot.
 # Contrary to traditional mixture modelling, the total number of parameters is not
@@ -13,10 +13,10 @@
 # A corresponding search space with three slots could look like this:
 # | Slot1_Label | Slot1_Amount | Slot2_Label | Slot2_Amount | Slot3_Label | Slot3_Amount |
 # |:------------|:-------------|:------------|:-------------|:------------|:-------------|
-# | Solvent1    | 10           | Solvent5    | 20           | Solvent4    | 30           |
+# | Solvent1    | 10           | Solvent5    | 20           | Solvent4    | 70           |
 # | Solvent1    | 30           | Solvent8    | 40           | Solvent2    | 30           |
-# | Solvent3    | 20           | Solvent1    | 35           | Solvent9    | 30           |
-# | Solvent2    | 15           | Solvent3    | 10           | Solvent1    | 30           |
+# | Solvent3    | 20           | Solvent1    | 35           | Solvent9    | 45           |
+# | Solvent2    | 15           | Solvent3    | 10           | Solvent1    | 45           |
 
 # This slot-based representation has one decided advantage compared to traditional
 # modelling: We can utilize BayBE's label encodings for the label parameters. For
@@ -30,6 +30,14 @@
 # three solvents, i.e. we will have 3 slots (6 parameters). Their respective amounts
 # need to sum up to 100. Also, a solvents should never be chosen twice, which
 # requires various other constraints.
+
+# ```{admonition} Discrete vs. Continuous Modelling
+# :class: important
+# In here, we utilize only discrete parameters, although in principle, the parameters
+# corresponding to amounts could also be modelled as continuous numbers. This however,
+# would mean some of the constraints we need act between discrete and continuous
+# parameters - which is not supported at the moment.
+# ```
 
 ### Imports
 
@@ -51,15 +59,15 @@ from baybe.searchspace import SearchSpace
 from baybe.targets import NumericalTarget
 from baybe.utils.dataframe import add_fake_measurements
 
-### Parameter Setup
-
-# Set some basic settings.
+# Basic example settings.
 
 SUM_TOLERANCE = 0.1  # The tolerance we allow for the fulfillment of sum constraints
 SMOKE_TEST = "SMOKE_TEST" in os.environ
 RESOLUTION = 5 if SMOKE_TEST else 11  # resolution of the discretization
 
-# Create the parameters for the slot labels. Each of our slots offers a choice between
+### Parameter Setup
+
+# Create parameters for the slot labels. Each of our slots offers a choice between
 # 4 solvents.
 
 dict_solvents = {
@@ -78,7 +86,7 @@ slot3_label = SubstanceParameter(
     name="Slot3_Label", data=dict_solvents, encoding="MORDRED"
 )
 
-# Create the parameters for the slot amounts.
+# Create parameters for the slot amounts.
 
 slot1_amount = NumericalDiscreteParameter(
     name="Slot1_Amount", values=list(np.linspace(0, 100, RESOLUTION)), tolerance=0.2
