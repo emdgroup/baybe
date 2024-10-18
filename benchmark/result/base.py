2701ad4c4cb89e2fb0505522ce21e9e3c79a7a02
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-10-18 06:38:29 +0200

    Add converter for metadata

--- benchmark/result/base.py
+++ benchmark/result/base.py
@@ -1,9 +1,10 @@
 """Base classes for the benchmarking results."""
 
 from abc import ABC, abstractmethod
+from typing import Any
 from uuid import UUID
 
-from attrs import define
+from attrs import define, field
 from matplotlib.figure import Figure, SubFigure
 
 from benchmark.metric import Metric
@@ -13,6 +14,15 @@ from benchmark.metric import Metric
 class Result(ABC):
     """Abstract base class for all benchmarking results."""
 
+    @staticmethod
+    def _convert_metadata_to_string(metadata: dict[Any, Any]) -> dict[str, str]:
+        """Convert the metadata to a string representation.
+
+        The function will convert the metadata to a string representation
+        to ensure that the metadata can be written to a csv file.
+        """
+        return {str(key): str(value) for key, value in metadata.items()}
+
     title: str
     """The title of the benchmarking result."""
 
@@ -20,7 +30,7 @@ class Result(ABC):
     """The unique identifier of the benchmark running which can be set
     to compare different executions of the same benchmark setting."""
 
-    metadata: dict[str, str]
+    metadata: dict[str, str] = field(converter=_convert_metadata_to_string)
     """Metadata about the benchmarking result."""
 
     @abstractmethod
