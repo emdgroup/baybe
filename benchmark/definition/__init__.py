d973b2e983a4d020701fc6f4015e175430dba6f8
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-10-22 20:28:06 +0200

    Add benchmark classes. Add a structure to implement and reflect benchmark classes and combine all relevant information especially for storing results as well as a reasonable possibility to use them and measure different kind of metrics.

---
+++ benchmark/definition/__init__.py
@@ -0,0 +1,6 @@
+"""Benchmark task definitions."""
+
+from benchmark.definition.base import Benchmark
+from benchmark.definition.basic_benchmarking import SingleExecutionBenchmark
+
+__all__ = ["Benchmark", "SingleExecutionBenchmark"]
