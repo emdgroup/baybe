89a345c5e7d766e025b5d57aad1b4013a229f1e3
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-10-24 17:17:01 +0200

    Changing the name for the benchmarking list and remove unnecessary exception handling

--- benchmark/domain/__init__.py
+++ benchmark/domain/__init__.py
@@ -1,10 +1,10 @@
 """This module contains the benchmarks for the different domains of the benchmark."""
 
-from benchmark.definition import SingleExecutionBenchmark
+from benchmark.definition import Benchmark
 from benchmark.domain.synthetic_3 import benchmark_synthetic_3
 
-SINGLE_BENCHMARKS_TO_RUN: list[SingleExecutionBenchmark] = [
+BENCHMARKS: list[Benchmark] = [
     benchmark_synthetic_3,
 ]
 
-__all__ = ["SINGLE_BENCHMARKS_TO_RUN"]
+__all__ = ["BENCHMARKS"]
