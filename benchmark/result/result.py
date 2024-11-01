f46fdfed7608a01ecc7ff7876c18b1919d4694f6
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-11-01 18:29:46 +0100

    Add measurement base and example implementation. Add a base class for upcoming measurement implementation to reflect them in the benchmarking classes already and provide a simple example for the usage by adding the simple regret.

--- benchmark/result/result.py
+++ benchmark/result/result.py
@@ -4,7 +4,7 @@ from typing import Any
 from uuid import UUID
 
 from attrs import define, field
-from attrs.validators import instance_of
+from attrs.validators import deep_mapping, instance_of
 from pandas import DataFrame
 
 from baybe.serialization.core import converter
@@ -16,11 +16,13 @@ from benchmark.result.metadata_class import ResultMetadata
 class Result(SerialMixin):
     """A single result of the benchmarking."""
 
-    benchmark_origin_id: UUID = field(validator=instance_of(UUID))
+    benchmark_origin_identifier: UUID = field(validator=instance_of(UUID))
     """The unique identifier of the benchmark running which can be set
     to compare different executions of the same benchmark setting."""
 
-    benchmark_settings: dict[str, Any] = field(validator=instance_of(dict))
+    benchmark_settings: dict[str, Any] = field(
+        validator=deep_mapping(instance_of(str), instance_of(Any), instance_of(dict))
+    )
     """Metadata about the benchmarking result."""
 
     benchmark_result: DataFrame = field()
@@ -28,14 +30,6 @@ class Result(SerialMixin):
 
     result_metadata: ResultMetadata = field(validator=instance_of(ResultMetadata))
     """The metadata of the benchmark result."""
-
-    @benchmark_settings.validator
-    def _validate_metadata(self, _: Any, value: dict[str, Any]) -> None:
-        if not isinstance(value, dict):
-            raise ValueError(f"Metadata must be a dictionary, got {type(value)}")
-        for key in value.keys():
-            if not isinstance(key, str):
-                raise ValueError(f"Metadata keys must be strings, got {type(key)}")
 
 
 converter.register_unstructure_hook(UUID, lambda x: str(x))
