6a3ced8eec3f6a415f464d3ee1f90f76ab55b642
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-11-11 15:39:18 +0100

    Remove logging message in __main__.py

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,19 +1,12 @@
 """Run the benchmarks for the given scenario."""
 
-import logging
-
 from benchmark.domain import BENCHMARKS
-from benchmark.result.result import Result
 
 
 def main():
-    """Run the performance test for the given scenario."""
+    """Run the performance test for the defined scenarios."""
     for benchmark in BENCHMARKS:
-        benchmark_result: Result = benchmark.run()
-        logging.info(
-            f"Benchmark {benchmark_result.name}"
-            + f"({benchmark_result.identifier}) finished."
-        )
+        benchmark.run()
 
 
 if __name__ == "__main__":
