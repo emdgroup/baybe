6825a3537c8ac1e5842b6fce42441ddef44e0b04
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-11-11 15:39:17 +0100

    Restructure class and validator, and remove unnecessary parameter

--- benchmark/definition/benchmarking.py
+++ benchmark/definition/benchmarking.py
@@ -5,6 +5,7 @@ from collections.abc import Callable
 from uuid import UUID, uuid4
 
 from attrs import define, field
+from attrs.validators import instance_of
 from pandas import DataFrame
 
 from benchmark.result.result import Result
@@ -14,28 +15,16 @@ from benchmark.result.result import Result
 class Benchmark:
     """A class to define a benchmark task."""
 
-    name: str
+    name: str = field(validator=instance_of(str))
     """The name of the benchmark."""
 
     benchmark_function: Callable[[], tuple[DataFrame, dict[str, str]]]
     """The function that executes the benchmark code and returns
     the results as well as metadata."""
 
-    objective_scenarios: list[str] = field(factory=list)
-    """The name of the simulated scenarios referring to
-    :func:`baybe.simulation.core.simulate_experiment`: to
-    evaluate the benchmarking results."""
-
-    identifier: UUID = field(factory=uuid4)
+    identifier: UUID = field(factory=uuid4, validator=instance_of(UUID))
     """The unique identifier of the benchmark running which can be set
     to compare different executions of the same benchmark setting."""
-
-    _metadata: dict[str, str] = field(init=False)
-    """Metadata about the benchmark. Will be set after the benchmark is executed."""
-
-    _benchmark_result: Result = field(init=False)
-    """The result of the benchmarking which is set after execution.
-    Set after the benchmark is executed."""
 
     def run(self) -> Result:
         """Execute the benchmark.
@@ -44,10 +33,10 @@ class Benchmark:
         metrics if any were set and measures execution time.
         """
         start_ns = time.perf_counter_ns()
-        result, self._metadata = self.benchmark_function()
+        result, metadata = self.benchmark_function()
         stop_ns = time.perf_counter_ns()
         time_delta = stop_ns - start_ns
-        self._benchmark_result = Result(
-            self.name, self.identifier, self._metadata, result, time_delta
+        benchmark_result = Result(
+            self.name, self.identifier, metadata, result, time_delta
         )
-        return self._benchmark_result
+        return benchmark_result
