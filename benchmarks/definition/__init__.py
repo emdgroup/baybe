b031d929c175ec3975f96c28fdb842932f6cf613
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-11-13 13:20:19 +0100

    Add Benchmark and BenchmarkSettings to module exports

--- benchmarks/definition/__init__.py
+++ benchmarks/definition/__init__.py
@@ -1,7 +1,13 @@
 """Benchmark task definitions."""
 
-from benchmarks.definition.config import ConvergenceExperimentSettings
+from benchmarks.definition.config import (
+    Benchmark,
+    BenchmarkSettings,
+    ConvergenceExperimentSettings,
+)
 
 __all__ = [
     "ConvergenceExperimentSettings",
+    "Benchmark",
+    "BenchmarkSettings",
 ]
