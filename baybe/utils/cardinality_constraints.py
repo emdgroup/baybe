3e275e4454556d6ae42d21cd77e0035e5613a5cc
Di Jin <di.jin@merckgroup.com>
2024-12-16 14:03:21 +0100

    Refine logic in counting the near-zero elements

--- baybe/utils/cardinality_constraints.py
+++ baybe/utils/cardinality_constraints.py
@@ -18,22 +18,29 @@ def count_near_zeros(
         points: The recommendations of the parameter objects.
 
     Raises:
-        ValueError: If the dimensionality of parameters does not match that of points.
+        ValueError: If parameters does not cover all parameters present in points.
 
     Returns:
         The counts of near-zero values in the recommendations.
 
 
     """
-    if len(parameters) != points.shape[1]:
+    p_names = [p.name for p in parameters]
+    if not set(points.columns).issubset(set(p_names)):
         raise ValueError(
-            "Dimensionality mismatch: number of parameters = {len("
-            "parameters)}, parameters in recommendations "
-            "= {points.shape[1]}."
+            "Parameters must cover all parameters present in points: "
+            "parameter names in parameters are: {p_name} and parameter "
+            "names from points are: {points.columns}."
         )
 
-    # Boolean values indicating whether candidate is near-zero: True for near-zero.
-    p_thresholds = np.array([p.near_zero_threshold for p in parameters])
+    # Only keep parameters that are present in points; The order of parameters
+    # aligns with that in points.
+    parameters_filtered_sorted = (
+        p for p_name in points.columns for p in parameters if p.name == p_name
+    )
+
+    # Boolean values indicating whether the candidate is near-zero: True for near-zero.
+    p_thresholds = np.array([p.near_zero_threshold for p in parameters_filtered_sorted])
     p_thresholds_mask = np.broadcast_to(p_thresholds, points.shape)
     near_zero_flags = (points > -p_thresholds_mask) & (points < p_thresholds_mask)
     return np.sum(near_zero_flags, axis=1)
@@ -60,15 +67,11 @@ def is_min_cardinality_fulfilled(
         if c.min_cardinality == 0:
             continue
 
-        # TODO: Is the parameters in constraints sorted or not? Can we assume the
-        #  order of parameters in constraints align with that in the subspace?
-
         # Counts the near-zero elements
         batch_related_to_c = batch[c.parameters]
-        parameters_related_to_c = tuple(
-            p for p in subspace_continuous.parameters if p.name in c.parameters
+        n_near_zeros = count_near_zeros(
+            subspace_continuous.parameters, batch_related_to_c
         )
-        n_near_zeros = count_near_zeros(parameters_related_to_c, batch_related_to_c)
 
         # When the minimum cardinality is violated
         if np.any(len(c.parameters) - n_near_zeros < c.min_cardinality):
