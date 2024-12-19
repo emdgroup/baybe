fe3a36d3446c80bdfa08a1fc96fb3f6191238649
AdrianSosic <adrian.sosic@merckgroup.com>
2024-12-19 13:20:42 +0100

    Use explicit value assignment for dataframe update
    Co-authored-by: Martin Fitzner <17951239+Scienfitz@users.noreply.github.com>

--- baybe/simulation/lookup.py
+++ baybe/simulation/lookup.py
@@ -82,7 +82,7 @@ def _look_up_targets_from_callable(
 ) -> None:
     """Look up target values by querying a callable."""
     df_targets = lookup(queries)
-    queries[df_targets.columns] = df_targets
+    queries[df_targets.columns] = df_targets.values
 
 
 def _look_up_targets_from_dataframe(
