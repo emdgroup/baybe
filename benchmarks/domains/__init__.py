faebf5acdd5da759bf07a6449949401ee4826465
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-01-17 14:10:21 +0100

    Add Michalewicz benchmark

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -2,9 +2,11 @@
 
 from benchmarks.definition.config import Benchmark
 from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+from benchmarks.domains.synthetic_michalewicz import synthetic_michalewicz
 
 BENCHMARKS: list[Benchmark] = [
     synthetic_2C1D_1C_benchmark,
+    synthetic_michalewicz,
 ]
 
 __all__ = ["BENCHMARKS"]
