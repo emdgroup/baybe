d6d502b43c6e239883cb370128f9de39a14f591a
AdrianSosic <adrian.sosic@merckgroup.com>
2025-02-17 10:15:47 +0100

    Add pareto_objectives strategy and serialization test

--- tests/hypothesis_strategies/objectives.py
+++ tests/hypothesis_strategies/objectives.py
@@ -4,11 +4,18 @@ import hypothesis.strategies as st
 
 from baybe.objectives.desirability import DesirabilityObjective
 from baybe.objectives.enum import Scalarizer
+from baybe.objectives.pareto import ParetoObjective
 from baybe.objectives.single import SingleTargetObjective
 
 from ..hypothesis_strategies.basic import finite_floats
 from ..hypothesis_strategies.targets import numerical_targets
 from ..hypothesis_strategies.utils import intervals as st_intervals
+
+_intervals = st_intervals(exclude_fully_unbounded=True, exclude_half_bounded=True)
+
+_targets = st.lists(
+    numerical_targets(_intervals), min_size=2, unique_by=lambda t: t.name
+)
 
 
 def single_target_objectives():
@@ -19,10 +26,7 @@ def single_target_objectives():
 @st.composite
 def desirability_objectives(draw: st.DrawFn):
     """Generate :class:`baybe.objectives.desirability.DesirabilityObjective`."""
-    intervals = st_intervals(exclude_fully_unbounded=True, exclude_half_bounded=True)
-    targets = draw(
-        st.lists(numerical_targets(intervals), min_size=2, unique_by=lambda t: t.name)
-    )
+    targets = draw(_targets)
     weights = draw(
         st.lists(
             finite_floats(min_value=0.0, exclude_min=True),
@@ -32,3 +36,9 @@ def desirability_objectives(draw: st.DrawFn):
     )
     scalarizer = draw(st.sampled_from(Scalarizer))
     return DesirabilityObjective(targets, weights, scalarizer)
+
+
+@st.composite
+def pareto_objectives(draw: st.DrawFn):
+    """Generate :class:`baybe.objectives.pareto.ParetoObjective`."""
+    return ParetoObjective(draw(_targets))
