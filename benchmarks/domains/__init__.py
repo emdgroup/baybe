4b29227992f5a5bcea68cd9b7788e2e8870bd75e
Karin Hrovatin <47607471+Hrovatin@users.noreply.github.com>
2025-02-21 13:41:40 +0100

    Add a few botorch kernel preset benchmarks and adapt scripts for a test benchmark run

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,10 +1,15 @@
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
-from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+
+# from benchmarks.domains.kernel_presets.easom_tl_noise import \
+# easom_tl_noise_benchmark
+from benchmarks.domains.kernel_presets.hartmann_tl_inverted_noise import (
+    hartmann_tl_inverted_noise_benchmark,
+)
 
 BENCHMARKS: list[Benchmark] = [
-    synthetic_2C1D_1C_benchmark,
+    hartmann_tl_inverted_noise_benchmark,
 ]
 
 __all__ = ["BENCHMARKS"]
