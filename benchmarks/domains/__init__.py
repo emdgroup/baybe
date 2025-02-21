8e56260979a6836f719f3141c8127ade42a9ef6b
Karin Hrovatin <47607471+Hrovatin@users.noreply.github.com>
2025-02-21 15:36:37 +0100

    Define benchmarks to run

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,15 +1,26 @@
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
-
-# from benchmarks.domains.kernel_presets.easom_tl_noise import \
-# easom_tl_noise_benchmark
+from benchmarks.domains.kernel_presets.arylhalides_tl_substance import (
+    arylhalides_tl_substance_benchmark,
+)
+from benchmarks.domains.kernel_presets.direct_arylation_tl_temp import (
+    direct_arylation_tl_temp_benchmark,
+)
+from benchmarks.domains.kernel_presets.easom_tl_noise import easom_tl_noise_benchmark
 from benchmarks.domains.kernel_presets.hartmann_tl_inverted_noise import (
     hartmann_tl_inverted_noise_benchmark,
+)
+from benchmarks.domains.kernel_presets.michalewicz_tl_noise import (
+    michalewicz_tl_noise_benchmark,
 )
 
 BENCHMARKS: list[Benchmark] = [
     hartmann_tl_inverted_noise_benchmark,
+    easom_tl_noise_benchmark,
+    michalewicz_tl_noise_benchmark,
+    arylhalides_tl_substance_benchmark,
+    direct_arylation_tl_temp_benchmark,
 ]
 
 __all__ = ["BENCHMARKS"]
