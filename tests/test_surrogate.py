0e0266651c6f012594a8d4433aebccea1c34789f
AdrianSosic <adrian.sosic@merckgroup.com>
2025-02-24 17:28:40 +0100

    Replace BroadcastingSurrogate with CompositeSurrogate.from_template

--- tests/test_surrogate.py
+++ tests/test_surrogate.py
@@ -8,7 +8,7 @@ import pytest
 from baybe.objectives.pareto import ParetoObjective
 from baybe.parameters.numerical import NumericalDiscreteParameter
 from baybe.recommenders.pure.bayesian.botorch import BotorchRecommender
-from baybe.surrogates.composite import BroadcastingSurrogate, CompositeSurrogate
+from baybe.surrogates.composite import CompositeSurrogate
 from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
 from baybe.surrogates.random_forest import RandomForestSurrogate
 from baybe.targets.numerical import NumericalTarget
@@ -32,22 +32,22 @@ def test_caching(patched, searchspace, objective, fake_measurements):
 
 
 @pytest.mark.parametrize(
-    "meta_surrogate_cls", [BroadcastingSurrogate, CompositeSurrogate]
+    "surrogate",
+    [
+        CompositeSurrogate(
+            {"t1": GaussianProcessSurrogate(), "t2": RandomForestSurrogate()},
+        ),
+        CompositeSurrogate.from_template(GaussianProcessSurrogate()),
+    ],
+    ids=["via_init", "via_template"],
 )
-def test_composite_surrogates(meta_surrogate_cls):
+def test_composite_surrogates(surrogate):
     """Composition yields a valid surrogate."""
     t1 = NumericalTarget("t1", "MAX")
     t2 = NumericalTarget("t2", "MIN")
     searchspace = NumericalDiscreteParameter("p", [0, 1]).to_searchspace()
     objective = ParetoObjective([t1, t2])
     measurements = pd.DataFrame({"p": [0], "t1": [0], "t2": [0]})
-
-    if issubclass(meta_surrogate_cls, BroadcastingSurrogate):
-        surrogate = BroadcastingSurrogate(GaussianProcessSurrogate())
-    else:
-        surrogate = CompositeSurrogate(
-            {"t1": GaussianProcessSurrogate(), "t2": RandomForestSurrogate()}
-        )
     BotorchRecommender(surrogate_model=surrogate).recommend(
         2, searchspace, objective, measurements
     )
