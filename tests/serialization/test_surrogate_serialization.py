ee9e53c098419fc315e66459070a2ec346b29ed3
AdrianSosic <adrian.sosic@merckgroup.com>
2025-02-24 10:11:56 +0100

    Add composite surrogate serialization test

--- tests/serialization/test_surrogate_serialization.py
+++ tests/serialization/test_surrogate_serialization.py
@@ -4,7 +4,10 @@ import pytest
 
 from baybe._optional.info import ONNX_INSTALLED
 from baybe.surrogates.base import Surrogate
+from baybe.surrogates.composite import CompositeSurrogate
 from baybe.surrogates.custom import CustomONNXSurrogate
+from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
+from baybe.surrogates.random_forest import RandomForestSurrogate
 from baybe.utils.basic import get_subclasses
 
 
@@ -20,4 +23,20 @@ def test_surrogate_serialization(request, surrogate_cls):
 
     string = surrogate.to_json()
     surrogate2 = Surrogate.from_json(string)
+    assert surrogate == surrogate2, (surrogate, surrogate2)
+
+
+@pytest.mark.parametrize(
+    "surrogate",
+    [
+        CompositeSurrogate(
+            {"A": RandomForestSurrogate(), "B": GaussianProcessSurrogate()}
+        ),
+    ],
+    ids=["via_init"],
+)
+def test_composite_surrogate_serialization(surrogate):
+    """A serialization roundtrip yields an equivalent object."""
+    string = surrogate.to_json()
+    surrogate2 = CompositeSurrogate.from_json(string)
     assert surrogate == surrogate2, (surrogate, surrogate2)
