c956dd4702666b3b20140c5c2a7b8cb165614abe
Karin Hrovatin <47607471+Hrovatin@users.noreply.github.com>
2025-02-27 11:20:55 +0100

    Add benchmarks to the list of benchmarks to be run

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,10 +1,28 @@
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
-from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+from benchmarks.domains.botorch_multitaskgp.arylhalides_tl_substance import (
+    arylhalides_tl_substance_benchmark,
+)
+from benchmarks.domains.botorch_multitaskgp.direct_arylation_tl_temp import (
+    direct_arylation_tl_temp_benchmark,
+)
+from benchmarks.domains.botorch_multitaskgp.easom_tl_noise import (
+    easom_tl_noise_benchmark,
+)
+from benchmarks.domains.botorch_multitaskgp.hartmann_tl_inverted_noise import (
+    hartmann_tl_inverted_noise_benchmark,
+)
+from benchmarks.domains.botorch_multitaskgp.michalewicz_tl_noise import (
+    michalewicz_tl_noise_benchmark,
+)
 
 BENCHMARKS: list[Benchmark] = [
-    synthetic_2C1D_1C_benchmark,
+    arylhalides_tl_substance_benchmark,
+    direct_arylation_tl_temp_benchmark,
+    hartmann_tl_inverted_noise_benchmark,
+    easom_tl_noise_benchmark,
+    michalewicz_tl_noise_benchmark,
 ]
 
 __all__ = ["BENCHMARKS"]
