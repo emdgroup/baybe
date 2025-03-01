13361f0ae4505449ffb68c373b8c2df384016aa2
Rita Lyu <90297295+ritalyu17@users.noreply.github.com>
2025-02-28 20:50:28 -0500

    Update __init__.py

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
@@ -49,3 +50,18 @@ BENCHMARKS: list[Benchmark] = [
 
 __all__ = ["BENCHMARKS"]
 >>>>>>> 0a265eec (Rename config module to base)
+=======
+"""Benchmark domains."""
+
+from benchmarks.definition.base import Benchmark
+from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+from benchmarks.domains.Hardness import hardness_benchmark, hardness_transfer_learning_benchmark
+
+BENCHMARKS: list[Benchmark] = [
+    synthetic_2C1D_1C_benchmark,
+    hardness_benchmark,
+    hardness_transfer_learning_benchmark,
+]
+
+__all__ = ["BENCHMARKS"]
+>>>>>>> 0a8aed51 (Update __init__.py)
