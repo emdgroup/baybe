55b7c73061dc7e24b61489643bcd9ed502ac720f
Rita Lyu <90297295+ritalyu17@users.noreply.github.com>
2025-02-28 20:41:56 -0500

    Rename config module to base

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
@@ -30,3 +31,21 @@ BENCHMARKS: list[Benchmark] = [
 
 __all__ = ["BENCHMARKS"]
 >>>>>>> 14473d7b ([pre-commit.ci] auto fixes from pre-commit.com hooks)
+=======
+"""Benchmark domains."""
+
+from benchmarks.definition.base import Benchmark
+from benchmarks.domains.Hardness import (
+    hardness_benchmark,
+    hardness_transfer_learning_benchmark,
+)
+from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+
+BENCHMARKS: list[Benchmark] = [
+    synthetic_2C1D_1C_benchmark,
+    hardness_benchmark,
+    hardness_transfer_learning_benchmark,
+]
+
+__all__ = ["BENCHMARKS"]
+>>>>>>> 0a265eec (Rename config module to base)
