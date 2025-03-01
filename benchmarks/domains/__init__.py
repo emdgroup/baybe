7c176ae88232d79127018ce51a358f505702a2ce
Rita Lyu <90297295+ritalyu17@users.noreply.github.com>
2025-02-28 20:37:04 -0500

    Add files via upload

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -2,10 +2,12 @@
 
 from benchmarks.definition.base import Benchmark
 from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
+from benchmarks.domains.Hardness import hardness_benchmark, hardness_transfer_learning_benchmark
 
 BENCHMARKS: list[Benchmark] = [
     synthetic_2C1D_1C_benchmark,
+    hardness_benchmark,
+    hardness_transfer_learning_benchmark, 
 ]
-
 
 __all__ = ["BENCHMARKS"]
