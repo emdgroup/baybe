ed7262b0b4f7333c3a9b7cb84a0ec321b4c854fe
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-03-12 15:54:03 +0100

    Rework of aryl_halides benchmark

---
+++ benchmarks/domains/transfer_learning/aryl_halides/__init__.py
@@ -0,0 +1,7 @@
+"""Aryl halides transfer learning benchmarks."""
+
+from benchmarks.domains.transfer_learning.aryl_halides.ChlorTrifluour_IodMeth import (
+    arylhalides_ChloroTrifluour_IodoMetho,
+)
+
+__all__ = ["arylhalides_ChloroTrifluour_IodoMetho"]
