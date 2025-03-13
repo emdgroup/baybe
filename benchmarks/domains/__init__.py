a010ca932664a6c7da33de76d42e41eba9cce949
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-03-13 08:50:10 +0100

    Add second aryl halides TL benchmark

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -13,10 +13,14 @@ from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
 from benchmarks.domains.transfer_learning.aryl_halides.ChlorTrifluour_IodMeth import (
     arylhalides_ChlorTrifluour_IodMeth_benchmark,
 )
+from benchmarks.domains.transfer_learning.aryl_halides.IodoPyr_ChloroPyr import (
+    arylhalides_IodoPyr_ChloroPyr_benchmark,
+)
 
 BENCHMARKS: list[Benchmark] = [
     synthetic_2C1D_1C_benchmark,
     arylhalides_ChlorTrifluour_IodMeth_benchmark,
+    arylhalides_IodoPyr_ChloroPyr_benchmark,
     direct_arylation_tl_temp_benchmark,
     hartmann_tl_inverted_noise_benchmark,
     easom_tl_noise_benchmark,
---
+++ benchmarks/domains/transfer_learning/aryl_halides/IodoPyr_ChloroPyr.py
@@ -0,0 +1,42 @@
+"""Aryl-Halide benchmark for transfer learning.
+
+As source parameter, this benchmark uses 2-iodopyridine.
+As target parameter, this benchmark uses 3-chloropyridine.
+"""
+
+from __future__ import annotations
+
+import pandas as pd
+
+from benchmarks.definition import (
+    ConvergenceBenchmark,
+    ConvergenceBenchmarkSettings,
+)
+from benchmarks.domains.transfer_learning.aryl_halides.base import (
+    abstract_arylhalides_tl_substance_benchmark,
+)
+
+
+def arylhalides_IodoPyr_ChloroPyr(
+    settings: ConvergenceBenchmarkSettings,
+) -> pd.DataFrame:
+    """Actual benchmark function."""
+    return abstract_arylhalides_tl_substance_benchmark(
+        settings=settings,
+        source_tasks=["2-iodopyridine"],
+        target_tasks=["3-chloropyridine"],
+        percentages=[0.01, 0.02, 0.05, 0.1, 0.2],
+    )
+
+
+benchmark_config = ConvergenceBenchmarkSettings(
+    batch_size=2,
+    n_doe_iterations=12,
+    n_mc_iterations=40,
+)
+
+arylhalides_IodoPyr_ChloroPyr_benchmark = ConvergenceBenchmark(
+    function=arylhalides_IodoPyr_ChloroPyr,
+    optimal_target_values={"yield": 68.76495224},
+    settings=benchmark_config,
+)
