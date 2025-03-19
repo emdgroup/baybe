bcf7edce3d6dd5c5c6d78398213ee53cd6bd4934
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-03-19 16:47:09 +0100

    Unify naming of aryl_halide

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -3,10 +3,10 @@
 from benchmarks.definition.base import Benchmark
 from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
 from benchmarks.domains.transfer_learning.aryl_halides.ChlorTrifluour_IodMeth import (
-    arylhalides_ChlorTrifluour_IodMeth_benchmark,
+    aryl_halide_ChlorTrifluour_IodMeth_benchmark,
 )
 from benchmarks.domains.transfer_learning.aryl_halides.IodoPyr_ChloroPyr import (
-    arylhalides_IodoPyr_ChloroPyr_benchmark,
+    aryl_halide_IodoPyr_ChloroPyr_benchmark,
 )
 from benchmarks.domains.transfer_learning.direct_arylation.temperature import (
     direct_arylation_tl_temperature_benchmark,
@@ -29,8 +29,8 @@ from benchmarks.domains.transfer_learning.michalewicz.michalewicz_tl_continuous 
 
 BENCHMARKS: list[Benchmark] = [
     synthetic_2C1D_1C_benchmark,
-    arylhalides_ChlorTrifluour_IodMeth_benchmark,
-    arylhalides_IodoPyr_ChloroPyr_benchmark,
+    aryl_halide_ChlorTrifluour_IodMeth_benchmark,
+    aryl_halide_IodoPyr_ChloroPyr_benchmark,
     direct_arylation_tl_temperature_benchmark,
     easom_tl_71_noise5_benchmark,
     easom_tl_47_negate_noise5_benchmark,
