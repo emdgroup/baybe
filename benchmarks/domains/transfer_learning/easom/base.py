81096d9ecb16599d9827bf1f788c3d2c3d327121
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-03-19 17:10:09 +0100

    Include no source points case

--- benchmarks/domains/transfer_learning/easom/base.py
+++ benchmarks/domains/transfer_learning/easom/base.py
@@ -163,7 +163,7 @@ def abstract_easom_tl_noise(
 
     results = []
 
-    for p in percentages:
+    for p in [0, *percentages]:
         results.append(
             simulate_scenarios(
                 {
@@ -173,21 +173,13 @@ def abstract_easom_tl_noise(
                 lookup,
                 initial_data=[
                     initial_data.sample(frac=p) for _ in range(settings.n_mc_iterations)
-                ],
+                ]
+                if p > 0
+                else None,
                 batch_size=settings.batch_size,
                 n_doe_iterations=settings.n_doe_iterations,
+                n_mc_iterations=settings.n_mc_iterations if p == 0 else None,
                 impute_mode="error",
             )
         )
-    # No training data and non-TL campaign
-    results.append(
-        simulate_scenarios(
-            {"0": tl_campaign, "non-TL": nontl_campaign},
-            lookup,
-            batch_size=settings.batch_size,
-            n_doe_iterations=settings.n_doe_iterations,
-            n_mc_iterations=settings.n_mc_iterations,
-            impute_mode="error",
-        )
-    )
     return pd.concat(results)
