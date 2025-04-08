64277874f64c16854facb0714959d196a04fd855
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-04-08 13:49:36 +0200

    Add comment on lookup filtering

--- benchmarks/domains/transfer_learning/aryl_halides/base.py
+++ benchmarks/domains/transfer_learning/aryl_halides/base.py
@@ -78,7 +78,13 @@ def make_objective() -> SingleTargetObjective:
 
 
 def make_lookup(data: pd.DataFrame, target_tasks: Sequence[str]) -> pd.DataFrame:
-    """Create the lookup for the benchmark."""
+    """Create the lookup for the benchmark.
+
+    Note that we filter the data to only include the target tasks.
+    Without the filtering, there would be multiple entries for the same parameter
+    configuration. Since this might yield issues for the non-transfer learning
+    campaigns, we filter the data to only include the target tasks.
+    """
     return data[data["aryl_halide"].isin(target_tasks)]
 
 
