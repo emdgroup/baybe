8452f22153a90ace72b42c8f74154bf5be413ff4
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-04-08 13:25:42 +0200

    Fix no-data case for aryl_halides

--- benchmarks/domains/transfer_learning/aryl_halides/ChlorTrifluour_IodMeth.py
+++ benchmarks/domains/transfer_learning/aryl_halides/ChlorTrifluour_IodMeth.py
@@ -32,7 +32,7 @@ def aryl_halide_ChlorTrifluour_IodoMeth(
         settings=settings,
         source_tasks=["1-chloro-4-(trifluoromethyl)benzene"],
         target_tasks=["1-iodo-4-methoxybenzene"],
-        percentages=[0.01, 0.05, 0.1, 0.2],
+        percentages=[0.01, 0.1, 0.2],
     )
 
 
