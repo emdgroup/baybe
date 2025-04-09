b5d974131708a038585054bfa86a4da226862dc2
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-04-09 16:20:45 +0200

    Remove expensive easom benchmark

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -17,9 +17,6 @@ from benchmarks.domains.transfer_learning.direct_arylation.temperature_tl import
 from benchmarks.domains.transfer_learning.easom.easom_tl_47_negate_noise5 import (
     easom_tl_47_negate_noise5_benchmark,
 )
-from benchmarks.domains.transfer_learning.easom.easom_tl_71_noise5 import (
-    easom_tl_71_noise5_benchmark,
-)
 from benchmarks.domains.transfer_learning.hartmann.hartmann_tl_3_15_15 import (
     hartmann_tl_3_15_15_benchmark,
 )
@@ -36,7 +33,6 @@ BENCHMARKS: list[Benchmark] = [
     aryl_halide_IP_CP_tl_benchmark,
     aryl_halide_CT_I_BM_tl_benchmark,
     direct_arylation_tl_temperature_benchmark,
-    easom_tl_71_noise5_benchmark,
     easom_tl_47_negate_noise5_benchmark,
     hartmann_tl_3_30_negate_15_benchmark,
     hartmann_tl_3_15_15_benchmark,
