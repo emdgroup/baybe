310384d7a7e6f11f7c55dfe294b1475d74a2dbd4
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-04-09 23:03:50 +0200

    Add Hartmann benchmarks

--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -1,6 +1,11 @@
 """Benchmark domains."""
 
 from benchmarks.definition.base import Benchmark
+from benchmarks.domains.hartmann.hartmann_3d import hartmann_3d_benchmark
+from benchmarks.domains.hartmann.hartmann_3d_discretized import (
+    hartmann_3d_discretized_benchmark,
+)
+from benchmarks.domains.hartmann.hartmann_6d import hartmann_6d_benchmark
 from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
 from benchmarks.domains.transfer_learning.aryl_halides.CT_I_BM_tl import (
     aryl_halide_CT_I_BM_tl_benchmark,
@@ -25,7 +30,10 @@ from benchmarks.domains.transfer_learning.michalewicz.michalewicz_tl_continuous 
 )
 
 BENCHMARKS: list[Benchmark] = [
+    hartmann_3d_discretized_benchmark,
     synthetic_2C1D_1C_benchmark,
+    hartmann_3d_benchmark,
+    hartmann_6d_benchmark,
     aryl_halide_CT_IM_tl_benchmark,
     aryl_halide_IP_CP_tl_benchmark,
     aryl_halide_CT_I_BM_tl_benchmark,
