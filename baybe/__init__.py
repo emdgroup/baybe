369a515172bfbaaff9bcc0388acf74b92fb19397
AdrianSosic <adrian.sosic@merckgroup.com>
2025-04-10 09:49:40 +0200

    Switch steps in version inference logic
    To ensure up-to-date version info for editable installs

--- baybe/__init__.py
+++ baybe/__init__.py
@@ -12,22 +12,12 @@ def infer_version() -> str:  # pragma: no cover
     """Determine the package version for the different ways the code can be invoked."""
     # ----------------------------------------------------------------------------------
     # Attempt 1:
-    # If the package has been installed, read the version from the metadata created
-    # during the installation process. In this case, the version string has been
-    # determined via setuptools_scm from the git history. (If the install had been
-    # attempted without the git folder in place, setuptools_scm would have complained,
-    # causing the installation process to fail.)
-    from importlib.metadata import version
-
-    try:
-        return version(__name__)
-    except ModuleNotFoundError:
-        pass
-
-    # ----------------------------------------------------------------------------------
-    # Attempt 2:
-    # If the package is not installed, try to replicate the version on the fly, using
-    # the same logic by invoking setuptools_scm (provided the latter is installed).
+    # Try to dynamically infer the version on the fly from the git history using
+    # setuptools_scm, provided the latter is installed. This relies on the same logic as
+    # for writing the version metadata during the installation process (see Attempt 2
+    # below) but has the advantage that the version info is always up-to-date, even when
+    # using an "editable installation" using the "-e" flag, where the metadata file may
+    # become outdated when the git HEAD changes.
     try:
         from pathlib import Path
 
@@ -42,9 +32,22 @@ def infer_version() -> str:  # pragma: no cover
         pass
 
     # ----------------------------------------------------------------------------------
+    # Attempt 2:
+    # If the package is installed, retrieve the version from the metadata generated
+    # via setuptools_scm during the installation process. (If the install had been
+    # attempted without the git folder in place, setuptools_scm would have complained,
+    # causing the installation process to fail.)
+    from importlib.metadata import version
+
+    try:
+        return version(__name__)
+    except ModuleNotFoundError:
+        pass
+
+    # ----------------------------------------------------------------------------------
     # Fallback:
-    # If the package has not been installed and also no git history is available,
-    # there is no way to figure out the correct version.
+    # If neither the package is installed nor the git history is available, return
+    # "unknown" since the version cannot be determined.
     return "unknown"
 
 
