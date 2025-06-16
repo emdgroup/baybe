Correct final merit normalisation to stay consistent with the reference code

baybe/objectives/chimera.py
@@ -239,11 +239,13 @@ class ChimeraObjective(Objective):
             merits = merits * neg_mask + pos_mask * current_obj
 
         # Normalize CHIMERA merits
-        merits_range = merits.max() - merits.min()
-        if merits_range > 0:
-            merits = (merits - merits.min()) / (merits.max() - merits.min())
-        else:
-            merits = np.zeros_like(merits)  # Handle uniform values
+        # merits_range = merits.max() - merits.min()
+        # if merits_range > 0:
+        #     merits = (merits - merits.min()) / (merits.max() - merits.min())
+        # else:
+        #     merits = np.zeros_like(merits)  # Handle uniform values
+        if np.amax(merits) > 0.0:
+            merits = (merits - np.amin(merits)) / (np.amax(merits) - np.amin(merits))
         return merits
 
     @override