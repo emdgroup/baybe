Fix posterior_stats computation
While the mechanism was not wrong for single-output surrogates (which
is the only case currently encountered because multi-output cases are
sliced into single-output cases via composite surrogates), the logic
would break when the surrogate itself provides multiple dimensions via
its posterior. In this case:
* One can run into silent bugs because the dataframe slicing at the end
  would have shadowed the multi-output nature. In fact, this problem was
  noticed when working on the pre-transform extension of the
  desirability objective.
* There would be an unnecessary for-loop for the different outputs.

baybe/surrogates/base.py
@@ -352,33 +352,32 @@ class Surrogate(ABC, SurrogateProtocol, SerialMixin):
         targets = [t.name for t in self._objective.targets]
         result = pd.DataFrame(index=candidates.index)
         with torch.no_grad():
-            for k, target_name in enumerate(targets):
-                for stat in stats:
-                    try:
-                        if isinstance(stat, float):  # Calculate quantile statistic
-                            stat_name = f"Q_{stat}"
-                            vals = posterior.quantile(torch.tensor(stat))
-                        else:  # Calculate non-quantile statistic
-                            stat_name = stat
-                            vals = getattr(
-                                posterior,
-                                stat if stat not in ["std", "var"] else "variance",
-                            )
-                    except (AttributeError, NotImplementedError) as e:
-                        # We could arrive here because an invalid statistics string has
-                        # been requested or because a quantile point has been requested,
-                        # but the posterior type does not implement quantiles.
-                        raise TypeError(
-                            f"The utilized posterior of type "
-                            f"'{posterior.__class__.__name__}' does not support the "
-                            f"statistic associated with the requested input '{stat}'."
-                        ) from e
+            for stat in stats:
+                try:
+                    if isinstance(stat, float):  # Calculate quantile statistic
+                        stat_name = f"Q_{stat}"
+                        vals = posterior.quantile(torch.tensor(stat))
+                    else:  # Calculate non-quantile statistic
+                        stat_name = stat
+                        vals = getattr(
+                            posterior,
+                            stat if stat not in ["std", "var"] else "variance",
+                        )
+                except (AttributeError, NotImplementedError) as e:
+                    # We could arrive here because an invalid statistics string has
+                    # been requested or because a quantile point has been requested,
+                    # but the posterior type does not implement quantiles.
+                    raise TypeError(
+                        f"The utilized posterior of type "
+                        f"'{posterior.__class__.__name__}' does not support the "
+                        f"statistic associated with the requested input '{stat}'."
+                    ) from e
 
-                    if stat == "std":
-                        vals = torch.sqrt(vals)
+                if stat == "std":
+                    vals = torch.sqrt(vals)
 
-                    numpyvals = vals.cpu().numpy().reshape((len(result), len(targets)))
-                    result[f"{target_name}_{stat_name}"] = numpyvals[:, k]
+                numpyvals = vals.cpu().numpy().reshape((len(result), len(targets)))
+                result[[f"{name}_{stat_name}" for name in targets]] = numpyvals
 
         return result
 