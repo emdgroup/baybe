bd6ea05e481838536db1ca052c64eccbf4475016
AdrianSosic <adrian.sosic@merckgroup.com>
2025-07-02 16:52:18 +0200

    Move transformation tests to correct module

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -5,8 +5,9 @@ import pandas as pd
 import pytest
 import torch
 from pandas.testing import assert_series_equal
+from pytest import param
 
-from baybe.targets.numerical import NumericalTarget as ModernTarget
+from baybe.targets.numerical import NumericalTarget
 from baybe.transformations import (
     AbsoluteTransformation,
     AffineTransformation,
@@ -22,6 +23,7 @@ from baybe.transformations import (
     TwoSidedLinearTransformation,
 )
 from baybe.utils.dataframe import to_tensor
+from baybe.utils.interval import Interval
 
 
 def sample_input() -> pd.Series:
@@ -93,10 +95,10 @@ def test_identity_transformation_chaining():
 
 def test_generic_transformation(series):
     """Torch callables can be used to specify custom transformations."""
-    t1 = ModernTarget("t", AbsoluteTransformation())
-    t2 = ModernTarget("t", CustomTransformation(torch.abs))
-    t3 = ModernTarget("t", torch.abs)
-    t4 = ModernTarget(
+    t1 = NumericalTarget("t", AbsoluteTransformation())
+    t2 = NumericalTarget("t", CustomTransformation(torch.abs))
+    t3 = NumericalTarget("t", torch.abs)
+    t4 = NumericalTarget(
         "t", torch.abs(IdentityTransformation())
     )  # explicitly trigger __torch_function__
 
@@ -108,14 +110,14 @@ def test_generic_transformation(series):
 
 def test_transformation_addition(series):
     """Adding transformations results in chaining/shifting."""
-    t1 = ModernTarget(
+    t1 = NumericalTarget(
         "t",
         ChainedTransformation(
             [AbsoluteTransformation(), AffineTransformation(shift=1)]
         ),
     )
-    t2 = ModernTarget("t", AbsoluteTransformation() + 1)
-    t3 = ModernTarget("t", AbsoluteTransformation() + AffineTransformation(shift=1))
+    t2 = NumericalTarget("t", AbsoluteTransformation() + 1)
+    t3 = NumericalTarget("t", AbsoluteTransformation() + AffineTransformation(shift=1))
 
     transformed = t1.transform(series)
     assert_series_equal(transformed, t2.transform(series))
@@ -124,14 +126,14 @@ def test_transformation_addition(series):
 
 def test_transformation_multiplication(series):
     """Multiplying transformations results in scaling."""
-    t1 = ModernTarget(
+    t1 = NumericalTarget(
         "t",
         ChainedTransformation(
             [AbsoluteTransformation(), AffineTransformation(factor=2)]
         ),
     )
-    t2 = ModernTarget("t", AbsoluteTransformation() * 2)
-    t3 = ModernTarget("t", AbsoluteTransformation() + AffineTransformation(factor=2))
+    t2 = NumericalTarget("t", AbsoluteTransformation() * 2)
+    t3 = NumericalTarget("t", AbsoluteTransformation() + AffineTransformation(factor=2))
 
     transformed = t1.transform(series)
     assert_series_equal(transformed, t2.transform(series))
@@ -140,10 +142,10 @@ def test_transformation_multiplication(series):
 
 def test_torch_overloading(series):
     """Transformations can be passed to torch callables for chaining."""
-    t1 = ModernTarget(
+    t1 = NumericalTarget(
         "t", AffineTransformation(factor=2) + CustomTransformation(torch.abs)
     )
-    t2 = ModernTarget("t", torch.abs(AffineTransformation(factor=2)))
+    t2 = NumericalTarget("t", torch.abs(AffineTransformation(factor=2)))
     assert_series_equal(t1.transform(series), t2.transform(series))
 
 
@@ -177,6 +179,63 @@ aff_0 = aff(to_tensor(0))
 aff_1 = aff(to_tensor(1))
 bell_off_1 = bell(to_tensor(bell.center + 1))
 bell_off_01 = bell(to_tensor(bell.center + 0.1))
+
+
+@pytest.mark.parametrize(
+    ("transformation", "bounds", "expected"),
+    [
+        param(id, (None, None), (None, None), id="id_open"),
+        param(id, (0, None), (0, None), id="id_half_open"),
+        param(id, (0, 1), (0, 1), id="id_finite"),
+        param(clamp, (0, 1), (2, 2), id="clamp_left_outside"),
+        param(clamp, (0, 3), (2, 3), id="clamp_left_overlapping"),
+        param(clamp, (3, 4), (3, 4), id="clamp_overlapping"),
+        param(clamp, (4, 6), (4, 5), id="clamp_right_overlapping"),
+        param(clamp, (6, 7), (5, 5), id="clamp_right_outside"),
+        param(aff, (None, None), (None, None), id="affine_open"),
+        param(aff, (0, None), (aff_0, None), id="affine_half_open"),
+        param(aff, (0, 1), (aff_0, aff_1), id="affine_finite"),
+        param(ts_v, (1, 3), (0, 5), id="ts_v_with_center"),
+        param(ts_v, (3, 4), (5, 10), id="ts_v_no_center"),
+        param(ts_l, (1, 3), (-2, 5), id="ts_l_with_center"),
+        param(ts_l, (3, 4), (5, 10), id="ts_l_no_center"),
+        param(ts_n, (1, 3), (-5, 0), id="ts_n_with_center"),
+        param(ts_n, (3, 4), (-10, -5), id="ts_n_no_center"),
+        param(bell, (None, None), (0, 1), id="bell_open"),
+        param(bell, (c, None), (0, 1), id="bell_half_open"),
+        param(bell, (c + 1, None), (0, bell_off_1), id="bell_half_open_reduced"),
+        param(bell, (c + 0.1, c + 1), (bell_off_1, bell_off_01), id="bell_no_center"),
+        param(bell, (c - 0.1, c + 1), (bell_off_1, 1), id="bell_with_center"),
+        param(abs, (None, None), (0, None), id="abs_open"),
+        param(abs, (0, None), (0, None), id="abs_half_open"),
+        param(abs, (0, 1), (0, 1), id="abs_0_1"),
+        param(abs, (0.1, 1), (0.1, 1), id="abs_0.1_1"),
+        param(abs, (-0.1, 1), (0, 1), id="abs_-0.1_1"),
+        param(tri, (None, None), (0, 1), id="tri_open"),
+        param(tri, (0, 1), (0, 0), id="tri_left_outside"),
+        param(tri, (0, 3), (0, 0.5), id="tri_left_overlapping"),
+        param(tri, (3, 4), (0.5, 1), id="tri_overlapping"),
+        param(tri, (5, 11), (0, 0.75), id="tri_right_overlapping"),
+        param(tri, (11, 12), (0, 0), id="tri_right_outside"),
+        param(log, (0, None), (None, None), id="log_open"),
+        param(log, (1, np.exp(1)), (0, 1), id="log_unit"),
+        param(exp, (None, None), (0, None), id="exp_open"),
+        param(exp, (0, 1), (1, np.exp(1)), id="exp_unit"),
+        param(pow_even, (1, None), (1, None), id="pow_even_open_right"),
+        param(pow_even, (None, -1), (1, None), id="pow_even_open_left"),
+        param(pow_even, (-2, 3), (0, 9), id="pow_even_with_center"),
+        param(pow_even, (1, 3), (1, 9), id="pow_even_without_center"),
+        param(pow_odd, (1, None), (1, None), id="pow_odd_open_right"),
+        param(pow_odd, (None, -1), (None, -1), id="pow_odd_open_left"),
+        param(pow_odd, (-2, 3), (-8, 27), id="pow_odd_with_center"),
+        param(pow_odd, (1, 3), (1, 27), id="pow_odd_without_center"),
+        param(chain, (-0.1, 3), (5, 7), id="chain"),
+    ],
+)
+def test_image_computation(transformation, bounds, expected):
+    """The image of a transformation is computed correctly."""
+    bounds = (None, None) if bounds is None else bounds
+    assert transformation.get_image(bounds) == Interval.create(expected)
 
 
 def test_affine_transformation_operation_order(series):
