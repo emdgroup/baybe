c35e0801df73282381cd8466c9b0c14ef31e3ea1
AdrianSosic <adrian.sosic@merckgroup.com>
2025-07-04 09:03:36 +0200

    Fix metadata serialization
    In the previous implementation, a Metadata object had two different dict
    representations, depending on which conversion route was taken.
    
    The problem has been captured in tests and is now now fixed through the
    applied conversion hooks.
    
    A minimal reproducing example demonstrating the issue:
    
    ```python
    from baybe.utils.metadata import Metadata, to_metadata
    
    dct = {"description": "test", "unit": "m", "key": "value"}
    metadata = to_metadata(dct)
    
    print("Dict:\n", dct, "\n")
    print("Via to_metadata:\n", metadata, "\n")
    print("Via from_dict:\n", Metadata.from_dict(dct), "\n")
    print("Via to_dict:\n", metadata.to_dict(), "\n")
    ```

--- baybe/utils/metadata.py
+++ baybe/utils/metadata.py
@@ -4,11 +4,13 @@ from __future__ import annotations
 
 from typing import Any
 
+import cattrs
 from attrs import define, field, fields
 from attrs.validators import deep_mapping, instance_of
 from attrs.validators import optional as optional_v
 
-from baybe.serialization.mixin import SerialMixin
+from baybe.serialization import SerialMixin, converter
+from baybe.utils.basic import classproperty
 
 
 @define(frozen=True)
@@ -34,6 +36,20 @@ class Metadata(SerialMixin):
     )
     """Additional user-defined metadata."""
 
+    @misc.validator
+    def _validate_misc(self, _, value: dict[str, Any]) -> None:
+        if inv := set(value).intersection(self._explicit_fields):
+            raise ValueError(
+                f"Miscellaneous metadata cannot contain the following fields: {inv}. "
+                f"Use the corresponding attributes instead."
+            )
+
+    @classproperty
+    def _explicit_fields(self) -> set[str]:
+        """The explicit metadata fields."""  # noqa: D401
+        flds = fields(Metadata)
+        return {fld.name for fld in flds if fld.name != flds.misc.name}
+
 
 def to_metadata(value: dict[str, Any] | Metadata, /) -> Metadata:
     """Convert a dictionary to :class:`Metadata` (with :class:`Metadata` passthrough).
@@ -57,10 +73,22 @@ def to_metadata(value: dict[str, Any] | Metadata, /) -> Metadata:
         )
 
     # Separate known fields from unknown ones
-    flds = fields(Metadata)
-    value = value.copy()
-    known_fields = {
-        fld: value.pop(fld, None) for fld in (flds.description.name, flds.unit.name)
-    }
+    return converter.structure(value, Metadata)
 
-    return Metadata(**known_fields, misc=value)
+
+@converter.register_structure_hook
+def _separate_metadata_fields(dct: dict[str, Any], _: type[Metadata]) -> Metadata:
+    """Separate known fields from miscellaneous metadata."""
+    dct = dct.copy()
+    explicit = {fld: dct.pop(fld, None) for fld in Metadata._explicit_fields}
+    return Metadata(**explicit, misc=dct)
+
+
+@converter.register_unstructure_hook
+def _flatten_misc_metadata(metadata: Metadata) -> dict[str, Any]:
+    """Flatten the metadata for serialization."""
+    fn = cattrs.gen.make_dict_unstructure_fn(Metadata, converter)
+    dct = fn(metadata)
+    fld = fields(Metadata).misc.name
+    dct = dct | dct.pop(fld)
+    return dct
