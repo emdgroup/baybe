56c7b45bccabbba0aadea1b36f9eeef08b92ed76
AdrianSosic <adrian.sosic@merckgroup.com>
2025-07-04 08:55:32 +0200

    Add serialization test for field separation

--- tests/serialization/test_metadata_serialization.py
+++ tests/serialization/test_metadata_serialization.py
@@ -2,7 +2,7 @@
 
 from hypothesis import given
 
-from baybe.utils.metadata import Metadata
+from baybe.utils.metadata import Metadata, to_metadata
 
 from ..hypothesis_strategies.metadata import metadata
 
@@ -13,3 +13,12 @@ def test_metadata_roundtrip(meta: Metadata):
     string = meta.to_json()
     meta2 = Metadata.from_json(string)
     assert meta == meta2, (meta, meta2)
+
+
+def test_field_separation():
+    """Field separation works regardless of the conversion route."""
+    dct = {"description": "test", "unit": "m", "key": "value"}
+    metadata = Metadata("test", "m", {"key": "value"})
+    via_converter = to_metadata(dct)
+    via_from_dict = Metadata.from_dict(dct)
+    assert metadata == via_converter == via_from_dict
