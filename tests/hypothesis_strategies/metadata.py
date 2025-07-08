1306e471943e6bae661021b55d7b807f8d91ff40
AdrianSosic <adrian.sosic@merckgroup.com>
2025-07-08 15:31:27 +0200

    Split hypothesis strategy

--- tests/hypothesis_strategies/metadata.py
+++ tests/hypothesis_strategies/metadata.py
@@ -3,14 +3,16 @@
 import hypothesis.strategies as st
 from hypothesis import assume
 
+from baybe.parameters.base import ParameterMetadata
 from baybe.utils.metadata import Metadata
+
+_descriptions = st.one_of(st.none(), st.text(min_size=1))
+"""A strategy generating metadata descriptions."""
 
 
 @st.composite
-def metadata(draw: st.DrawFn):
-    """Generate :class:`baybe.utils.metadata.Metadata`."""
-    description = draw(st.one_of(st.none(), st.text(min_size=1)))
-    unit = draw(st.one_of(st.none(), st.text(min_size=1)))
+def _miscs(draw: st.DrawFn, cls: type[Metadata]):
+    """Generates miscellaneous metadata for various metadata classes."""
     misc = draw(
         st.dictionaries(
             st.text(min_size=1),
@@ -18,5 +20,22 @@ def metadata(draw: st.DrawFn):
             max_size=5,
         )
     )
-    assume(not Metadata._explicit_fields.intersection(misc))
-    return Metadata(description=description, unit=unit, misc=misc)
+    assume(not cls._explicit_fields.intersection(misc))
+    return misc
+
+
+@st.composite
+def metadata(draw: st.DrawFn):
+    """Generate :class:`baybe.utils.metadata.Metadata`."""
+    description = draw(_descriptions)
+    misc = draw(_miscs(Metadata))
+    return Metadata(description=description, misc=misc)
+
+
+@st.composite
+def parameter_metadata(draw: st.DrawFn):
+    """Generate :class:`baybe.parameters.base.ParameterMetadata`."""
+    description = draw(_descriptions)
+    unit = draw(st.one_of(st.none(), st.text(min_size=1)))
+    misc = draw(_miscs(ParameterMetadata))
+    return ParameterMetadata(description=description, unit=unit, misc=misc)
