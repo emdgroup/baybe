88be042e1b69542b4d531fe222f59cdbbf325bad
Martin Fitzner <martin.fitzner@merckgroup.com>
2025-07-16 11:22:38 +0200

    Make test regex invariant

--- tests/validation/test_campaign_validation.py
+++ tests/validation/test_campaign_validation.py
@@ -18,11 +18,13 @@ def test_overlapping_target_parameter_names():
     )
     parameters = (
         NumericalDiscreteParameter("n1", [1, 2, 3]),
-        NumericalDiscreteParameter("dies", [4, 5, 6]),
+        NumericalDiscreteParameter("bla", [4, 5, 6]),
         NumericalDiscreteParameter("n2", [7, 8, 9]),
     )
     searchspace = SearchSpace.from_product(parameters)
     objective = ParetoObjective(targets)
 
-    with pytest.raises(ValueError, match="appear in both collections: {'n2', 'n1'}."):
+    with pytest.raises(
+        ValueError, match="appear in both collections: {(?:'n1', 'n2'|'n2', 'n1')}"
+    ):
         Campaign(searchspace=searchspace, objective=objective)
