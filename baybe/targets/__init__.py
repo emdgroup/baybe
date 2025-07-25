157d25112374b8ebf0a77c785e38119a8e8ca5b8
AdrianSosic <adrian.sosic@merckgroup.com>
2025-07-25 15:08:03 +0200

    Add metadata class to parameter and target package namespaces

--- baybe/targets/__init__.py
+++ baybe/targets/__init__.py
@@ -3,9 +3,11 @@
 from baybe.targets.binary import BinaryTarget
 from baybe.targets.enum import TargetMode, TargetTransformation
 from baybe.targets.numerical import NumericalTarget
+from baybe.utils.metadata import MeasurableMetadata
 
 __all__ = [
     "BinaryTarget",
+    "MeasurableMetadata",
     "NumericalTarget",
     "TargetMode",
     "TargetTransformation",
