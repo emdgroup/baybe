bc3e8581a8a3d674cdc9af4ab0a525941093eb95
Karin Hrovatin <47607471+Hrovatin@users.noreply.github.com>
2025-08-08 15:24:37 +0200

    Add  CLI args to improve benchmarks usability when run locally

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -17,30 +17,46 @@ from benchmarks.result import Result
 RUNS_IN_CI = "CI" in os.environ
 
 
-def save_benchmark_data(benchmark: Benchmark, result: Result) -> None:
+def save_benchmark_data(
+    benchmark: Benchmark,
+    result: Result,
+    name: str | None = None,
+    outdir: str | None = None,
+) -> None:
     """Save the benchmark data to the object storage."""
-    path_constructor = PathConstructor.from_result(result)
+    path_constructor = PathConstructor.from_result(result, name=name, outdir=outdir)
+    if outdir:
+        os.makedirs(outdir, exist_ok=True)
     persist_dict = benchmark.to_dict() | result.to_dict()
 
     object_storage = S3ObjectStorage() if RUNS_IN_CI else LocalFileObjectStorage()
     object_storage.write_json(persist_dict, path_constructor)
 
 
-def run_all_benchmarks() -> None:
+def run_all_benchmarks(
+    smoketest: str | None = None,
+    name: str | None = None,
+    outdir: str | None = None,
+) -> None:
     """Run all benchmarks."""
     for benchmark in BENCHMARKS:
-        result = benchmark()
-        save_benchmark_data(benchmark, result)
+        result = benchmark(smoketest=smoketest)
+        save_benchmark_data(benchmark, result, name=name, outdir=outdir)
 
 
-def run_benchmarks(benchmark_names: Collection[str]) -> None:
+def run_benchmarks(
+    benchmark_names: Collection[str],
+    smoketest: str | None = None,
+    name: str | None = None,
+    outdir: str | None = None,
+) -> None:
     """Run a subset based on the benchmark name."""
     for benchmark in BENCHMARKS:
         if benchmark.name not in benchmark_names:
             continue
 
-        result = benchmark()
-        save_benchmark_data(benchmark, result)
+        result = benchmark(smoketest=smoketest)
+        save_benchmark_data(benchmark, result, name=name, outdir=outdir)
 
 
 def main() -> None:
@@ -49,13 +65,25 @@ def main() -> None:
     parser.add_argument(
         "--benchmark-list", nargs="+", help="List of benchmarks to run", default=None
     )
+    parser.add_argument(
+        "--smoketest", type=str, help="Smoketest setting to use.", default=None
+    )
+    parser.add_argument(
+        "--name", help="Additional name to add to saved file.", default=None
+    )
+    parser.add_argument("--outdir", help="Save files into directory.", default=None)
     args = parser.parse_args()
     if not args.benchmark_list:
-        run_all_benchmarks()
+        run_all_benchmarks(smoketest=args.smoketest, name=args.name)
         return
 
     benchmark_execute_set = set(args.benchmark_list)
-    run_benchmarks(benchmark_execute_set)
+    run_benchmarks(
+        benchmark_execute_set,
+        smoketest=args.smoketest,
+        name=args.name,
+        outdir=args.outdir,
+    )
 
 
 if __name__ == "__main__":
