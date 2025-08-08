14da36d80c684b5bde8d485dbdc828a0b84262a7
kalama-ai <kathrin.skubch@merckgroup.com>
2025-08-08 15:31:08 +0200

    Set smaller source data set sizes.

--- benchmarks/domains/regression/aryl_halides/sou_CT_I_tar_BM_tl_regr.py
+++ benchmarks/domains/regression/aryl_halides/sou_CT_I_tar_BM_tl_regr.py
@@ -114,9 +114,9 @@ def aryl_halide_CT_I_BM_tl_regr(
 # Define the benchmark settings
 benchmark_config = TransferLearningRegressionSettings(
     random_seed=42,
-    num_mc_iterations=5,
+    num_mc_iterations=30,
     max_train_points=10,
-    source_fractions=[0.1, 0.3, 0.5, 0.7, 0.9],
+    source_fractions=[0.01, 0.05, 0.1],
     noise_std=0.0,  # Not used for real data
     metrics=["RMSE", "R2", "MAE", "LPD", "NLPD"],
 )
