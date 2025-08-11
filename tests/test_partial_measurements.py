a836a931aece8f52601e9a8b0b1ae6e1721554f6
AdrianSosic <adrian.sosic@merckgroup.com>
2025-08-11 18:44:04 +0200

    Refactor partial measurements tests

--- tests/test_partial_measurements.py
+++ tests/test_partial_measurements.py
@@ -1,8 +1,10 @@
 """Tests for partial measurements."""
 
+import re
 from contextlib import nullcontext
 
 import numpy as np
+import pandas as pd
 import pytest
 from pytest import param
 
@@ -12,20 +14,43 @@ from baybe.objectives import DesirabilityObjective, ParetoObjective
 from baybe.targets import NumericalTarget
 from baybe.utils.dataframe import create_fake_input
 
+desirability_targets = [
+    NumericalTarget.normalize_ramp("t1", cutoffs=(0, 1)),
+    NumericalTarget.normalize_ramp("t2", cutoffs=(0, 1), descending=True),
+]
+
 
 @pytest.mark.parametrize(
     "objective",
     [
         param(NumericalTarget("t1").to_objective(), id="single_target"),
         param(
-            DesirabilityObjective(
-                [
-                    NumericalTarget.normalize_ramp("t1", cutoffs=(0, 1)),
-                    NumericalTarget.normalize_ramp(
-                        "t2", cutoffs=(0, 1), descending=True
-                    ),
-                ]
-            ),
+            DesirabilityObjective(desirability_targets, as_pre_transformation=True),
+            id="desirability",
+        ),
+    ],
+)
+@pytest.mark.parametrize("n_grid_points", [5], ids=["g5"])
+def test_invalid_partial_measurements(campaign):
+    """Objectives that require complete measurements raise an error when encountering
+    incomplete measurements.
+    """  # noqa: D205
+    target_names = [t.name for t in campaign.targets]
+    with pytest.raises(
+        IncompleteMeasurementsError,
+        match=f"missing values for the following targets: "
+        f"{re.escape(str(target_names))}",
+    ):
+        campaign.add_measurements(
+            pd.DataFrame({t.name: [float("nan")] for t in campaign.targets})
+        )
+
+
+@pytest.mark.parametrize(
+    "objective",
+    [
+        param(
+            DesirabilityObjective(desirability_targets),
             id="desirability",
         ),
         param(
@@ -39,26 +64,20 @@ from baybe.utils.dataframe import create_fake_input
 @pytest.mark.parametrize("n_iterations", [2], ids=["i2"])
 @pytest.mark.parametrize("batch_size", [5], ids=["b5"])
 @pytest.mark.parametrize("n_grid_points", [5], ids=["g5"])
-def test_partial_measurements(ongoing_campaign, objective):
-    c = ongoing_campaign
-    m = c.measurements
-    ts = c.targets
+def test_partial_measurements(ongoing_campaign):
+    """Objectives that can handle partial measurements do not complain when
+    encountering incomplete measurements.
+    """  # noqa: D205
+    m = ongoing_campaign.measurements
+    ts = ongoing_campaign.targets
 
     # Mark some measurements as unmeasured / failed
     m.iloc[0, m.columns.get_loc(ts[0].name)] = np.nan
     m.iloc[-1, m.columns.get_loc(ts[-1].name)] = np.nan
-    c.update_measurements(m)
-
-    # Failure is expected when single surrogates are used, which is exactly the case
-    # for non-Pareto objectives. For ParetoObjectives, the filtering is expected to
-    # remove affected rows, resulting in no error.
-    context = nullcontext()
-    if not isinstance(objective, ParetoObjective):
-        context = pytest.raises(ValueError, match="Bad input in the rows")
+    ongoing_campaign.update_measurements(m)
 
     # Trigger refit
-    with context:
-        c.recommend(2)
+    ongoing_campaign.recommend(2)
 
 
 @pytest.mark.parametrize(
