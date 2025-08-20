48dec028b970dbb25056094050ce601a008542c4
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2025-08-20 13:00:04 +0200

    Add tests for interpoint constraints

--- tests/constraints/test_constraints_continuous.py
+++ tests/constraints/test_constraints_continuous.py
@@ -7,6 +7,8 @@ from pytest import param
 from baybe.constraints import ContinuousLinearConstraint
 from tests.conftest import run_iterations
 
+TOLERANCE = 0.01
+
 
 @pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
 @pytest.mark.parametrize("constraint_names", [["ContiConstraint_1"]])
@@ -66,6 +68,101 @@ def test_inequality3(campaign, n_iterations, batch_size):
     print(res)
 
     assert (1.0 * res["Conti_finite1"] + 3.0 * res["Conti_finite2"]).le(0.301).all()
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize("constraint_names", [["InterConstraint_1"]])
+def test_interpoint_equality_single_parameter(campaign, n_iterations, batch_size):
+    """Test single parameter interpoint equality constraint."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    res_grouped = res.groupby("BatchNr")
+    interpoint_result = res_grouped["Conti_finite1"].sum()
+    np.allclose(interpoint_result, 0.3, atol=TOLERANCE)
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize("constraint_names", [["InterConstraint_2"]])
+def test_interpoint_inequality_single_parameter(campaign, n_iterations, batch_size):
+    """Test single parameter interpoint inequality constraint."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    res_grouped = res.groupby("BatchNr")
+    interpoint_result = 2 * res_grouped["Conti_finite1"].sum()
+    assert interpoint_result.ge(0.3 - TOLERANCE).all()
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize("constraint_names", [["InterConstraint_3"]])
+def test_interpoint_equality_multiple_parameters(campaign, n_iterations, batch_size):
+    """Test interpoint equality constraint involving multiple parameters."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    res_grouped = res.groupby("BatchNr")
+    interpoint_result = (
+        res_grouped["Conti_finite1"].sum() + res_grouped["Conti_finite2"].sum()
+    )
+    assert np.allclose(interpoint_result, 0.3, atol=TOLERANCE)
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize("constraint_names", [["InterConstraint_4"]])
+def test_geq_interpoint_inequality_multiple_parameters(
+    campaign, n_iterations, batch_size
+):
+    """Test geq-interpoint inequality constraint involving multiple parameters."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    res_grouped = res.groupby("BatchNr")
+    interpoint_result = (
+        2 * res_grouped["Conti_finite1"].sum() - res_grouped["Conti_finite2"].sum()
+    )
+    print(f"{interpoint_result=}")
+    assert interpoint_result.ge(0.3 - TOLERANCE).all()
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize("constraint_names", [["InterConstraint_5"]])
+def test_leq_interpoint_inequality_multiple_parameters(
+    campaign, n_iterations, batch_size
+):
+    """Test leq-interpoint inequality constraint involving multiple parameters."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    res_grouped = res.groupby("BatchNr")
+    interpoint_result = (
+        2 * res_grouped["Conti_finite1"].sum() - res_grouped["Conti_finite2"].sum()
+    )
+    assert interpoint_result.le(0.3 + TOLERANCE).all()
+
+
+@pytest.mark.parametrize("parameter_names", [["Conti_finite1", "Conti_finite2"]])
+@pytest.mark.parametrize(
+    "constraint_names", [["ContiConstraint_4", "InterConstraint_2"]]
+)
+def test_interpoint_normal_mix(campaign, n_iterations, batch_size):
+    """Test mixing interpoint and normal inequality constraints."""
+    run_iterations(campaign, n_iterations, batch_size, add_noise=False)
+    res = campaign.measurements
+    print(res)
+
+    interpoint_result = 2 * res.groupby("BatchNr")["Conti_finite1"].sum()
+    assert interpoint_result.ge(0.3 - TOLERANCE).all()
+    assert (
+        (1.0 * res["Conti_finite1"] + 3.0 * res["Conti_finite2"])
+        .ge(0.3 - TOLERANCE)
+        .all()
+    )
 
 
 @pytest.mark.slow
