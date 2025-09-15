223473205b010bef2c436b7cc338947441e3f4dc
kalama-ai <kathrin.skubch@merckgroup.com>
2025-09-15 16:03:55 +0200

    Implment RunModes for regression benchmarks

--- benchmarks/definition/regression/settings.py
+++ benchmarks/definition/regression/settings.py
@@ -1,37 +1,87 @@
 """Regression benchmark configuration."""
 
-from attrs import define, field
-from attrs.validators import and_, deep_iterable, ge, instance_of, le
+from attrs import define, field, fields
 
-from baybe.utils.validation import finite_float
-from benchmarks.definition.base import Benchmark, BenchmarkSettings
+from benchmarks.definition.base import (
+    Benchmark,
+    BenchmarkSettings,
+    RunMode,
+    make_runmode_attr_validator,
+)
 
 
 @define(frozen=True, kw_only=True)
 class RegressionBenchmarkSettings(BenchmarkSettings):
     """Base settings for regression benchmarks."""
 
-    n_mc_iterations: int = field(validator=instance_of(int))
-    """Number of Monte Carlo iterations."""
+    n_mc_iterations_settings: dict[RunMode, int] = field(
+        validator=make_runmode_attr_validator(int)
+    )
+    """The settings for number of Monte Carlo iterations."""
 
-    max_n_train_points: int = field(validator=instance_of(int))
-    """Maximum number of training points to consider."""
+    max_n_train_points_settings: dict[RunMode, int] = field(
+        validator=make_runmode_attr_validator(int)
+    )
+    """The settings for maximum number of training points to consider."""
 
-    noise_std: float = field(converter=float, validator=and_(finite_float, ge(0.0)))
-    """Standard deviation of noise to add to the data."""
+    noise_std_settings: dict[RunMode, float] = field(
+        validator=make_runmode_attr_validator(float)
+    )
+    """The settings for standard deviation of noise to add to the data."""
+
+    @property
+    def n_mc_iterations(self) -> int:
+        """The number of Monte Carlo iterations for the current runmode."""
+        if self.runmode not in self.n_mc_iterations_settings:
+            raise ValueError(
+                "Current runmode not found in "
+                f"{fields(RegressionBenchmarkSettings).n_mc_iterations_settings.name}"
+                f": {self.runmode.value}"
+            )
+        return self.n_mc_iterations_settings[self.runmode]
+
+    @property
+    def max_n_train_points(self) -> int:
+        """The maximum number of training points for the current runmode."""
+        if self.runmode not in self.max_n_train_points_settings:
+            raise ValueError(
+                "Current runmode not found in "
+                f"{fields(RegressionBenchmarkSettings).max_n_train_points_settings.name}"
+                f": {self.runmode.value}"
+            )
+        return self.max_n_train_points_settings[self.runmode]
+
+    @property
+    def noise_std(self) -> float:
+        """The standard deviation of noise for the current runmode."""
+        if self.runmode not in self.noise_std_settings:
+            raise ValueError(
+                "Current runmode not found in "
+                f"{fields(RegressionBenchmarkSettings).noise_std_settings.name}"
+                f": {self.runmode.value}"
+            )
+        return self.noise_std_settings[self.runmode]
 
 
 @define(frozen=True, kw_only=True)
 class TransferLearningRegressionBenchmarkSettings(RegressionBenchmarkSettings):
     """Settings for transfer learning regression benchmark."""
 
-    source_fractions: tuple[float, ...] = field(
-        validator=deep_iterable(
-            member_validator=and_(ge(0.0), le(1.0)),
-            iterable_validator=instance_of(tuple),
-        )
+    source_fractions_settings: dict[RunMode, tuple[float, ...]] = field(
+        validator=make_runmode_attr_validator(tuple)
     )
-    """Fractions of source data to use."""
+    """The settings for fractions of source data to use."""
+
+    @property
+    def source_fractions(self) -> tuple[float, ...]:
+        """The fractions of source data to use for the current runmode."""
+        if self.runmode not in self.source_fractions_settings:
+            raise ValueError(
+                "Current runmode not found in "
+                f"{fields(TransferLearningRegressionBenchmarkSettings).source_fractions_settings.name}"
+                f": {self.runmode.value}"
+            )
+        return self.source_fractions_settings[self.runmode]
 
 
 @define(frozen=True)
--- benchmarks/domains/regression/direct_arylation_temperature_tl_regr.py
+++ benchmarks/domains/regression/direct_arylation_temperature_tl_regr.py
@@ -8,6 +8,7 @@ from benchmarks.definition import (
     TransferLearningRegressionBenchmark,
     TransferLearningRegressionBenchmarkSettings,
 )
+from benchmarks.definition.base import RunMode
 from benchmarks.definition.regression.core import run_tl_regression_benchmark
 from benchmarks.domains.transfer_learning.direct_arylation.temperature_tl import (
     load_data,
@@ -45,10 +46,22 @@ def direct_arylation_temperature_tl_regr(
 
 # Define the benchmark settings
 benchmark_config = TransferLearningRegressionBenchmarkSettings(
-    n_mc_iterations=30,
-    max_n_train_points=10,
-    source_fractions=(0.01, 0.05, 0.1, 0.2),
-    noise_std=0.0,
+    n_mc_iterations_settings={
+        RunMode.DEFAULT: 30,
+        RunMode.SMOKETEST: 2,
+    },
+    max_n_train_points_settings={
+        RunMode.DEFAULT: 10,
+        RunMode.SMOKETEST: 5,
+    },
+    source_fractions_settings={
+        RunMode.DEFAULT: (0.01, 0.05, 0.1, 0.2),
+        RunMode.SMOKETEST: (0.1,),
+    },
+    noise_std_settings={
+        RunMode.DEFAULT: 0.0,
+        RunMode.SMOKETEST: 0.0,
+    },
 )
 
 # Create the benchmark
