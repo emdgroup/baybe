21dc6bcdc71af13a83ce429df47c9287ef97f4ef
Martin Fitzner <martin.fitzner@merckgroup.com>
2025-09-19 17:30:55 +0200

    Compress tests

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,16 +1,15 @@
 """Tests for the simulation module."""
 
 import os
+from collections.abc import Callable, Sequence
+from functools import partial
 
+import numpy as np
 import pandas as pd
 import pytest
+from pytest import param
 
-from baybe.campaign import Campaign
-from baybe.objectives import SingleTargetObjective
-from baybe.parameters.numerical import NumericalDiscreteParameter
-from baybe.searchspace.core import SearchSpace
-from baybe.simulation import simulate_experiment, simulate_scenarios
-from baybe.targets import NumericalTarget
+from baybe.simulation import simulate_scenarios
 
 pytestmark = pytest.mark.skipif(
     os.environ.get("BAYBE_TEST_ENV") != "FULLTEST",
@@ -18,103 +17,65 @@ pytestmark = pytest.mark.skipif(
 )
 
 
-@pytest.fixture(scope="function")
-def simulation_campaign() -> Campaign:
-    """Fixture for a simple campaign used in simulation tests."""
-    params = [
-        NumericalDiscreteParameter("a", [0, 1, 2]),
-        NumericalDiscreteParameter("b", [3, 4, 5]),
-    ]
-    searchspace = SearchSpace.from_product(parameters=params)
-    objective = SingleTargetObjective(NumericalTarget("c"))
-    return Campaign(searchspace=searchspace, objective=objective)
+def _match_aggregator(x: Sequence, /, match_value):
+    idx = np.abs(np.asarray(x) - match_value).argmin()
+    return x[idx]
 
 
-@pytest.fixture(scope="function")
-def simulation_lookup() -> pd.DataFrame:
-    """Fixture for a simple lookup DataFrame used in simulation tests."""
-    return pd.DataFrame(
-        {
-            "a": [0, 0, 0, 1, 1, 1, 2, 2, 2],
-            "b": [3, 4, 5, 3, 4, 5, 3, 4, 5],
-            "c": [10, 20, 30, 40, 50, 60, 70, 80, 90],
-        }
-    )
+_aggregators: dict[str, Callable] = {
+    "Target_max": max,
+    "Target_max_bounded": max,
+    "Target_min": min,
+    "Target_min_bounded": min,
+    "Target_match_bell": partial(_match_aggregator, match_value=50.0),
+}
 
 
-@pytest.fixture(scope="function")
-def simulation_random_seed() -> int:
-    """Fixture for a random seed used in simulation tests."""
-    return 1337
-
-
-def test_simulate_experiment_dataframe_structure(
-    simulation_campaign, simulation_lookup, simulation_random_seed
-):
-    """Test simulate_experiment result structure and correctness."""
-    n_doe_iterations = 3
-
-    result = simulate_experiment(
-        simulation_campaign,
-        simulation_lookup,
-        n_doe_iterations=n_doe_iterations,
-        batch_size=2,
-        random_seed=simulation_random_seed,
-    )
-
-    expected_cols = [
-        "Iteration",
-        "Num_Experiments",
-        "c_Measurements",
-        "c_IterBest",
-        "c_CumBest",
-    ]
-
-    assert isinstance(result, pd.DataFrame)
-    assert list(result.columns) == expected_cols
-    assert len(result) == n_doe_iterations
-    assert all(result["c_IterBest"] == result["c_Measurements"].apply(max))
-
-    all_measurements = [
-        m for measurements in result["c_Measurements"] for m in measurements
-    ]
-    cum_lens = result["c_Measurements"].apply(len).cumsum()
-    expected_cum_best = [max(all_measurements[:cum_len]) for cum_len in cum_lens]
-
-    assert list(result["c_CumBest"]) == expected_cum_best
-
-
-def test_simulate_scenarios_structure(
-    simulation_campaign, simulation_lookup, simulation_random_seed
-):
+@pytest.mark.parametrize(
+    "target_names",
+    [
+        param(["Target_max"], id="single_max"),
+        param(["Target_min"], id="single_min"),
+        param(["Target_match_bell"], id="single_match"),
+        param(
+            ["Target_max_bounded", "Target_min_bounded", "Target_match_bell"],
+            id="multi",
+        ),
+    ],
+)
+def test_simulate_scenarios_structure(campaign, batch_size):
     """Test simulate_scenarios output structure and correctness."""
-    scenarios = {"Test Campaign": simulation_campaign}
+    doe_iterations = 2
+    n_mc_iterations = 2
+    simulation_random_seed = 1337
+    scenarios = {"test": campaign}
 
-    doe_iterations = 3
-    n_mc_iterations = 3
     result = simulate_scenarios(
         scenarios,
-        simulation_lookup,
+        None,  # use random data for lookup
         n_doe_iterations=doe_iterations,
-        batch_size=2,
+        batch_size=batch_size,
         random_seed=simulation_random_seed,
         n_mc_iterations=n_mc_iterations,
+        parallel_runs=False,
     )
 
     assert isinstance(result, pd.DataFrame)
-    print(result.columns)
     assert len(result) == doe_iterations * n_mc_iterations * len(scenarios)
     expected_cols = [
         "Scenario",
         "Monte_Carlo_Run",
         "Iteration",
         "Num_Experiments",
-        "c_Measurements",
-        "c_IterBest",
-        "c_CumBest",
         "Random_Seed",
     ]
-    assert list(result.columns) == expected_cols
+    for t in campaign.targets:
+        expected_cols += [
+            f"{t.name}_Measurements",
+            f"{t.name}_IterBest",
+            f"{t.name}_CumBest",
+        ]
+    assert set(result.columns) == set(expected_cols), (result.columns, expected_cols)
     assert set(result["Scenario"].unique()) == set(scenarios.keys())
     assert set(result["Monte_Carlo_Run"].unique()) == set(range(n_mc_iterations))
     assert set(result["Iteration"].unique()) == set(range(doe_iterations))
@@ -125,12 +86,26 @@ def test_simulate_scenarios_structure(
     assert set(result["Random_Seed"].unique()) == set(expected_seed_values)
     for mc_run in result["Monte_Carlo_Run"].unique():
         mc_data = result[result["Monte_Carlo_Run"] == mc_run]
-        assert all(mc_data["c_IterBest"] == mc_data["c_Measurements"].apply(max))
 
-        all_measurements = [
-            m for measurements in mc_data["c_Measurements"] for m in measurements
-        ]
-        cum_lens = mc_data["c_Measurements"].apply(len).cumsum()
-        expected_cum_best = [max(all_measurements[:cum_len]) for cum_len in cum_lens]
+        for t in campaign.targets:
+            print("Name: ", t.name)
+            aggregator = _aggregators[t.name]
+            assert np.isclose(
+                mc_data[f"{t.name}_IterBest"].values,
+                mc_data[f"{t.name}_Measurements"].apply(aggregator).values,
+            ).all()
 
-        assert list(mc_data["c_CumBest"]) == expected_cum_best
+            all_measurements = [
+                m
+                for measurements in mc_data[f"{t.name}_Measurements"]
+                for m in measurements
+            ]
+            cum_lens = mc_data[f"{t.name}_Measurements"].apply(len).cumsum().values
+            expected_cum_best = [
+                aggregator(all_measurements[:cum_len]) for cum_len in cum_lens
+            ]
+            print("A ", cum_lens)
+            print("B ", all_measurements)
+            print("C ", expected_cum_best)
+
+            assert list(mc_data[f"{t.name}_CumBest"]) == expected_cum_best
