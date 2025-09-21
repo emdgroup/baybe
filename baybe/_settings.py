bae3b76fb1b8ceff6c817458c9df2dd94924e1b2
AdrianSosic <adrian.sosic@merckgroup.com>
2025-09-21 23:09:24 +0200

    Enable context manager approach

--- baybe/_settings.py
+++ baybe/_settings.py
@@ -2,10 +2,16 @@
 
 from __future__ import annotations
 
+from copy import deepcopy
+from typing import TYPE_CHECKING
+
 from attrs import Attribute, define, field, fields
 from attrs.validators import instance_of
 
 from baybe.utils.basic import classproperty
+
+if TYPE_CHECKING:
+    from types import TracebackType
 
 # The temporary assignment to `None` is needed because the object is already referenced
 # in the `Settings` class body
@@ -17,6 +23,9 @@ settings: Settings = None  # type: ignore[assignment]
 class Settings:
     """BayBE settings."""
 
+    _previous_settings: Settings | None = field(default=None, init=False)
+    """The previously applied settings (used for context management)."""
+
     dataframe_validation: bool = field(default=True, validator=instance_of(bool))
     """Controls if dataframe content is validated against the recommendation context."""
 
@@ -26,8 +35,23 @@ class Settings:
             # --> Nothing to do
             return
 
+        # Store the global settings at the time of instantiation for later rollback
+        self._previous_settings = deepcopy(settings)
+
         # Each new instance immediately applies its settings globally
         self.overwrite(settings)
+
+    def __enter__(self) -> Settings:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        assert self._previous_settings is not None
+        self._previous_settings.overwrite(settings)
 
     @classproperty
     def attributes(cls) -> tuple[Attribute, ...]:
