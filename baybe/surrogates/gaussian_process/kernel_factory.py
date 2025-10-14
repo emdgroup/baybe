b4c4deea9ab6c65a3236b467648538a6a1bf55e3
Julie Fang <julie.fang@tum.de>
2025-10-14 10:40:39 -0400

    add wrapper

--- baybe/surrogates/gaussian_process/kernel_factory.py
+++ baybe/surrogates/gaussian_process/kernel_factory.py
@@ -41,6 +41,32 @@ class PlainKernelFactory(KernelFactory, SerialMixin):
         return self.kernel
 
 
+# Internal kernel
+@define(frozen=True)
+class ProjectionAdapterFactory(KernelFactory, SerialMixin):
+    """A wrapper object for calling Projection Kernel."""
+
+    default_factory: KernelFactory
+    proj_dim: int
+    learn_projection: bool = False
+
+    def __call__(
+        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
+    ) -> Kernel:
+        """Wrap a Projection Kernel for lazy eval in GP."""
+        from baybe.kernels.base import ProjectionKernel
+
+        base_kernel = self.default_factory(searchspace, train_x, train_y)
+
+        proj_kernel = ProjectionKernel(
+            base_kernel=base_kernel,
+            proj_dim=self.proj_dim,
+            learn_projection=self.learn_projection,
+        )
+
+        return proj_kernel
+
+
 def to_kernel_factory(x: Kernel | KernelFactory, /) -> KernelFactory:
     """Wrap a kernel into a plain kernel factory (with factory passthrough)."""
     return x.to_factory() if isinstance(x, Kernel) else x
