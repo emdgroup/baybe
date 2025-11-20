32229da5e2bd7e9642f6f18ea08e7b60c152b236
AdrianSosic <adrian.sosic@gmail.com>
2025-11-20 09:04:22 +0100

    Clean up draft code
    * Add missing type hint
    * Fix order of dimensions
    * Create matrix as new tensor
    * Normalize matrix correctly

--- baybe/kernels/_gpytorch.py
+++ baybe/kernels/_gpytorch.py
@@ -30,16 +30,15 @@ class ProjectionKernel(Kernel):
         self.learn_projection = learn_projection
 
         if projection_matrix is not None:
-            self._set_projection_matrix(
-                torch.as_tensor(projection_matrix, dtype=DTypeFloatTorch)
-            )
+            matrix = torch.tensor(projection_matrix, dtype=DTypeFloatTorch)
+            self._set_projection_matrix(matrix)
 
     @staticmethod
-    def _make_projection_matrix(n_input_dims: int, n_projections) -> Tensor:
+    def _make_projection_matrix(n_input_dims: int, n_projections: int) -> Tensor:
         """Generate a random Gaussian projection matrix."""
-        return torch.randn(n_projections, n_input_dims, dtype=DTypeFloatTorch).div(
-            n_projections**0.5
-        )
+        matrix = torch.randn(n_input_dims, n_projections, dtype=DTypeFloatTorch)
+        matrix = matrix / torch.norm(matrix, dim=0, keepdim=True)
+        return matrix
 
     def _set_projection_matrix(self, matrix: Tensor, /) -> None:
         """Set the projection matrix as a parameter or buffer."""
@@ -50,7 +49,7 @@ class ProjectionKernel(Kernel):
         """Apply the base kernel to the projected input tensors."""
         if not hasattr(self, "projection_matrix"):
             self._set_projection_matrix(
-                self._make_projection_matrix(self.n_projections, x1.size(-1))
+                self._make_projection_matrix(x1.size(-1), self.n_projections)
             )
 
         x1_proj = x1 @ self.projection_matrix
