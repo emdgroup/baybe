6d9bf6a0eb5e126059a98e2c65af083873af4db5
AdrianSosic <adrian.sosic@gmail.com>
2025-11-24 14:46:17 +0100

    Simplify gpytorch projection kernel class to bare minimum

--- baybe/kernels/_gpytorch.py
+++ baybe/kernels/_gpytorch.py
@@ -18,40 +18,23 @@ class ProjectionKernel(Kernel):
     def __init__(
         self,
         base_kernel: Kernel,
+        projection_matrix: _ConvertibleToTensor,
         *,
-        n_projections: int,
-        projection_matrix: _ConvertibleToTensor | None = None,
         learn_projection: bool = False,
     ):
         super().__init__()
 
         self.base_kernel = base_kernel
-        self.n_projections = n_projections
         self.learn_projection = learn_projection
 
-        if projection_matrix is not None:
-            matrix = torch.tensor(projection_matrix, dtype=DTypeFloatTorch)
-            self._set_projection_matrix(matrix)
-
-    @staticmethod
-    def _make_projection_matrix(n_input_dims: int, n_projections: int) -> Tensor:
-        """Generate a random Gaussian projection matrix."""
-        matrix = torch.randn(n_input_dims, n_projections, dtype=DTypeFloatTorch)
-        matrix = matrix / torch.norm(matrix, dim=0, keepdim=True)
-        return matrix
-
-    def _set_projection_matrix(self, matrix: Tensor, /) -> None:
-        """Set the projection matrix as a parameter or buffer."""
-        type_ = torch.nn.Parameter if self.learn_projection else torch.nn.Buffer
-        self.projection_matrix = type_(matrix)
+        matrix = torch.tensor(projection_matrix, dtype=DTypeFloatTorch)
+        if self.learn_projection:
+            self.register_parameter("projection_matrix", torch.nn.Parameter(matrix))
+        else:
+            self.register_buffer("projection_matrix", matrix)
 
     def forward(self, x1: Tensor, x2: Tensor, **kwargs):
         """Apply the base kernel to the projected input tensors."""
-        if not hasattr(self, "projection_matrix"):
-            self._set_projection_matrix(
-                self._make_projection_matrix(x1.size(-1), self.n_projections)
-            )
-
         x1_proj = x1 @ self.projection_matrix
         x2_proj = x2 @ self.projection_matrix
         return self.base_kernel(x1_proj, x2_proj, **kwargs)
