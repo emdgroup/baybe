6fac34b9464099a361ab9fa36ac6d96aed25a99a
Jordan Penn <jordan.penn5841@gmail.com>
2025-12-09 18:43:45 +0000

    Add DiscreteNumericalFidelityParameter and CategoricalFidelityParameter

--- baybe/parameters/validation.py
+++ baybe/parameters/validation.py
@@ -1,5 +1,6 @@
 """Validation functionality for parameters."""
 
+from collections.abc import Collection
 from typing import Any
 
 from attrs import Attribute
@@ -12,3 +13,15 @@ def validate_decorrelation(obj: Any, attribute: Attribute, value: Any) -> None:
     if isinstance(value, float):
         gt(0.0)(obj, attribute, value)
         lt(1.0)(obj, attribute, value)
+
+
+def validate_contains_one(  # noqa: DOC101, DOC103
+    obj: Any, _: Any, value: Collection[float]
+):
+    """Validate that ``value`` contains at least one entry equal to ``1.0``.
+
+    Raises:
+        ValueError: If ``value`` does not include ``1.0``
+    """
+    if 1.0 not in value:
+        raise ValueError(f"{obj.name} must contain 1.0")
