757edbb398f58d075c3571eb0b38f332b5ba48c1
Myra Zmarsly <myra.zmarsly@merckgroup.com>
2025-12-10 16:01:43 +0100

    Remove unsused import of utils.dataframe after rebasing

--- baybe/objectives/base.py
+++ baybe/objectives/base.py
@@ -14,11 +14,7 @@ from baybe.serialization.mixin import SerialMixin
 from baybe.targets.base import Target
 from baybe.targets.numerical import NumericalTarget
 from baybe.utils.basic import is_all_instance
-from baybe.utils.dataframe import (
-    get_transform_objects,
-    to_tensor,
-    transform_target_columns,
-)
+from baybe.utils.dataframe import get_transform_objects, to_tensor
 from baybe.utils.dataframe import (
     handle_missing_values as df_handle_missing_values,
 )
