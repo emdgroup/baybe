c8d3b3264b0b035b46fe92aeeb4f408d43e2e87d
Jordan Penn <jordan.penn5841@gmail.com>
2026-01-19 10:56:23 +0000

    Fixes: validation, conversion, typing, docstrings

--- baybe/parameters/fidelity.py
+++ baybe/parameters/fidelity.py
@@ -4,10 +4,9 @@ from functools import cached_property
 from typing import Any, ClassVar, cast
 
 import cattrs
-import numpy as np
 import pandas as pd
-from attrs import define, field, fields
-from attrs.validators import and_, deep_iterable, ge, le, min_len
+from attrs import Converter, define, field, fields
+from attrs.validators import and_, deep_iterable, ge, instance_of, le, min_len
 from typing_extensions import override
 
 from baybe.parameters.base import DiscreteParameter
@@ -17,7 +16,18 @@ from baybe.parameters.validation import (
     validate_is_finite,
     validate_unique_values,
 )
+from baybe.utils.conversion import expand_scalar_progression
 from baybe.utils.numerical import DTypeFloatNumpy
+
+
+def _convert_zetas(value, self):
+    seq_len = len(self._values)
+    expanded_value = (
+        expand_scalar_progression(value, seq_len)
+        if isinstance(value, (int, float))
+        else value
+    )
+    return tuple(expanded_value)
 
 
 @define(frozen=True, slots=False)
@@ -29,7 +39,7 @@ class CategoricalFidelityParameter(DiscreteParameter):
 
     _values: tuple[str, ...] = field(
         alias="values",
-        converter=lambda x: cattrs.structure(x, tuple[str, ...]),  # type: ignore
+        converter=lambda x: cattrs.structure(x, tuple[str, ...]),
         validator=[
             min_len(2),
             validate_unique_values,  # type: ignore
@@ -41,37 +51,38 @@ class CategoricalFidelityParameter(DiscreteParameter):
         alias="costs",
         converter=lambda x: cattrs.structure(x, tuple[float, ...]),
         validator=[
-            min_len(2),
             validate_is_finite,
             deep_iterable(member_validator=ge(0.0)),
         ],
     )
     """The costs associated with querying the parameter at each value."""
 
-    highest_fidelity: str = field(
-        converter=str,
-        default=None,
-    )
-    """The name of the highest fidelity value."""
-
     # TODO: Handle other kinds of assumption about the relationship between fidelities.
     # _zeta currently takes the role of the discrepancy parameter in MF-GP-UCB
     # (Kandasamy et al, 2017) but other parameters may be needed for more general
     # multi-fidelity approaches which use a CategoricalFidelityParameter.
-    _zeta: tuple[float, ...] | float = field(
+    # TODO: Add an argument for adaptive learning of fidelity discrepancy.
+    # To be added in an upcoming pull request.
+    _zeta: tuple[float, ...] = field(
         alias="zeta",
-        converter=lambda x: (
-            cattrs.structure(x, float)
-            if isinstance(x, (float, int))
-            else cattrs.structure(x, tuple[float, ...])
+        converter=Converter(
+            _convert_zetas,
+            takes_self=True,
+        ),  # type: ignore
+        validator=(
+            validate_is_finite,
+            deep_iterable(member_validator=ge(0.0)),
         ),
-        default=None,
     )
     """The maximum discrepancy from target (high) fidelity at any design choice.
 
-    Either a tuple of positive values, one for each fidelity, or a scalar specifying
-    that the first fidelity input into 'values' has discrepancy 0 (it is the high
-    fidelity), the next have discrepancy 'zeta', 2*'zeta' and so on."""
+    Either a tuple of positive values, , one for each fidelity, equal to 0 at the
+    highest fidelity, or a scalar specifying that the first fidelity input into
+    'values' has discrepancy 0 (the highest fidelity), the next have discrepancy
+    'zeta', 2*'zeta' and so on."""
+
+    highest_fidelity: str | None = field(validator=instance_of(str), default=None)
+    """The name of the highest fidelity value. Determined by 'zeta' if not given."""
 
     @_costs.validator
     def _validate_cost_length(  # noqa: DOC101, DOC103
@@ -84,8 +95,8 @@ class CategoricalFidelityParameter(DiscreteParameter):
         """
         if len(value) != len(self._values):
             raise ValueError(
-                f"Length of '{fields(type(self))._costs.name}'"
-                f"and '{fields(type(self))._values.name}'"
+                f"Length of '{fields(type(self))._costs.alias}'"
+                f"and '{fields(type(self))._values.alias}'"
                 f"different in '{self.name}'."
             )
 
@@ -96,47 +107,14 @@ class CategoricalFidelityParameter(DiscreteParameter):
         """Validate instance attribute ``zeta``.
 
         Raises:
-            ValueError: If ``zeta`` is ``tuple[float, ...]`` and
-                ``len(zeta) != len(self._values)``.
-            ValueError: If ``zeta`` contains any negative or infinite values.
+            ValueError: If ``zeta`` and ``values`` are different lengths.
         """
-        if value is None:
-            # Jordan MHS TODO: add adaptive_zeta argument
-            # Jordan MHS TODO: handle other kinds of discrepancy param
-            # Jordan MHS TODO: make zeta optional if above params defined
+        if len(value) != len(self._values):
             raise ValueError(
-                f"Argument '{fields(type(self))._zeta.name}'undefined in '{self.name}'."
+                f"Tuples '{fields(type(self))._zeta.alias}'"
+                f"and '{fields(type(self))._values.alias}' are"
+                f"different lengths in '{self.name}'."
             )
-
-        elif isinstance(value, float):
-            if not np.isfinite(value):
-                raise ValueError(
-                    f"Arguemnt '{fields(type(self))._zeta.name}'"
-                    f"is infinite in '{self.name}'."
-                )
-            elif value < 0:
-                raise ValueError(
-                    f"Parameter '{fields(type(self))._zeta.name}'"
-                    f"is negative in '{self.name}'."
-                )
-            else:
-                return
-
-        else:
-            assert isinstance(value, tuple)
-
-            if len(value) != len(self._values):
-                raise ValueError(
-                    f"Tuples '{fields(type(self))._zeta.name}'"
-                    f"and '{fields(type(self))._values.name}' are"
-                    f"different lengths in '{self.name}'."
-                )
-
-            if any(np.isinf(value)):
-                raise ValueError(
-                    f"Tuple '{fields(type(self))._zeta.name}' contains"
-                    f"infinite values in '{self.name}'."
-                )
 
     @highest_fidelity.validator
     def _validate_highest_fidelity(  # noqa: DOC101, DOC103
@@ -144,8 +122,8 @@ class CategoricalFidelityParameter(DiscreteParameter):
     ):
         if target_value not in self._values:
             raise ValueError(
-                f"'{fields(type(self)).highest_fidelity.name}' {target_value}"
-                f"is not in '{fields(type(self))._values.name}' in '{self.name}'."
+                f"'{fields(type(self)).highest_fidelity.alias}' {target_value} is "
+                f"not in '{fields(type(self))._values.alias}' in '{self.name}'."
             )
 
         target_idx = self._values.index(target_value)
@@ -153,18 +131,18 @@ class CategoricalFidelityParameter(DiscreteParameter):
         if isinstance(self._zeta, tuple):
             if self._zeta[target_idx] != 0:
                 raise ValueError(
-                    f"'{fields(type(self)).highest_fidelity.name}' cannot have a"
-                    f"'{fields(type(self))._zeta.name}' value of '0' in the"
+                    f"'{fields(type(self)).highest_fidelity.alias}' must have "
+                    f"'{fields(type(self))._zeta.alias}' value of '0' in the "
                     f"fidelity parameter '{self.name}'."
                 )
 
         elif isinstance(self._zeta, float):
             if target_idx != 0:
                 raise ValueError(
-                    f"When specifying scalar '{fields(type(self))._zeta.name}',"
-                    f"'{fields(type(self)).highest_fidelity.name}' must be the first"
-                    f"name in '{fields(type(self))._values.name}' so it has"
-                    f"'{fields(type(self))._zeta.name} = 0' in '{self.name}'."
+                    f"When specifying scalar '{fields(type(self))._zeta.alias}', "
+                    f"'{fields(type(self)).highest_fidelity.alias}' must be "
+                    f"the first name in '{fields(type(self))._values.alias}' so it "
+                    f"has '{fields(type(self))._zeta.alias} = 0' in '{self.name}'."
                 )
 
     @override
@@ -226,7 +204,7 @@ class NumericalDiscreteFidelityParameter(DiscreteParameter):
 
     _values: tuple[float, ...] = field(
         alias="values",
-        converter=lambda x: cattrs.structure(x, tuple[float, ...]),  # type: ignore
+        converter=lambda x: cattrs.structure(x, tuple[float, ...]),
         validator=[
             min_len(2),
             validate_unique_values,  # type: ignore
@@ -259,8 +237,8 @@ class NumericalDiscreteFidelityParameter(DiscreteParameter):
         """
         if len(value) != len(self._values):
             raise ValueError(
-                f"Length of '{fields(type(self))._costs.name}'"
-                f"and '{fields(type(self))._values.name}' different in '{self.name}'."
+                f"Length of '{fields(type(self))._costs.alias}'"
+                f"and '{fields(type(self))._values.alias}' different in '{self.name}'."
             )
 
     @override
