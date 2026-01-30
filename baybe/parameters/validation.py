5856e7fc20edf7c4e17e3b148f3e2f03f2595ff7
AdrianSosic <adrian.sosic@gmail.com>
2026-01-30 14:12:21 +0100

    Refactor validators

--- baybe/parameters/validation.py
+++ baybe/parameters/validation.py
@@ -1,52 +1,70 @@
 """Validation functionality for parameters."""
 
-from collections.abc import Collection, Sequence
+from collections.abc import Collection
 from typing import Any
 
 import numpy as np
+from attrs import Attribute
 from attrs.validators import gt, instance_of, lt
 
-
-def validate_unique_values(  # noqa: DOC101, DOC103
-    obj: Any, _: Any, value: list
-) -> None:
-    """Validate that there are no duplicates in ``value``.
-
-    Raises:
-        ValueError: If there are duplicates in ``value``.
-    """
-    if len(set(value)) != len(value):
-        raise ValueError(
-            f"Cannot assign the following values containing duplicates to "
-            f"parameter {obj.name}: {value}."
-        )
+from baybe.parameters.base import Parameter
 
 
-def validate_decorrelation(obj: Any, attribute: Any, value: float) -> None:
-    """Validate the decorrelation."""
+def validate_decorrelation(obj: Parameter, attribute: Attribute, value: float) -> None:
+    """Validate that the input represents a valid decorrelation setting."""
     instance_of((bool, float))(obj, attribute, value)
     if isinstance(value, float):
         gt(0.0)(obj, attribute, value)
         lt(1.0)(obj, attribute, value)
 
 
-def validate_is_finite(  # noqa: DOC101, DOC103
-    obj: Any, _: Any, value: Sequence[float]
+def validate_unique_values(  # noqa: DOC101, DOC103
+    obj: Parameter, attribute: Attribute, values: Collection[Any]
 ) -> None:
-    """Validate that ``value`` contains no infinity/nan.
+    """Validate that the input contains unique elements.
 
     Raises:
-        ValueError: If ``value`` contains infinity/nan.
+        ValueError: If the input contains duplicates.
     """
-    if not all(np.isfinite(value)):
+    if len(set(values)) != len(values):
         raise ValueError(
-            f"Cannot assign the following values containing infinity/nan to "
-            f"parameter {obj.name}: {value}."
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must contain "
+            f"unique elements. Given: {values}."
+        )
+
+
+def validate_is_finite(  # noqa: DOC101, DOC103
+    obj: Parameter, attribute: Attribute, values: Collection[float]
+) -> None:
+    """Validate that the input contains no infinity/nan.
+
+    Raises:
+        ValueError: If the input contains infinity/nan.
+    """
+    if not all(np.isfinite(values)):
+        raise ValueError(
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must not "
+            f"contain infinity/nan elements. Given: {values}."
+        )
+
+
+def validate_contains_exactly_one_zero(  # noqa: DOC101, DOC103
+    obj: Parameter, attribute: Attribute, values: Collection[float]
+) -> None:
+    """Validate that the input contains exactly one element equal to ``0.0``.
+
+    Raises:
+        ValueError: If the input does not contain ``0.0`` exactly once.
+    """
+    if (count := list(values).count(0.0)) != 1:
+        raise ValueError(
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must contain "
+            f"the element 0.0 exactly once. Found {count} such elements: {values}."
         )
 
 
 def validate_contains_one(  # noqa: DOC101, DOC103
-    obj: Any, _: Any, values: Collection[float], use_alias: bool = True
+    obj: Parameter, attribute: Attribute, values: Collection[float]
 ):
     """Validate that ``value`` contains at least one entry equal to ``1.0``.
 
@@ -54,7 +72,7 @@ def validate_contains_one(  # noqa: DOC101, DOC103
         ValueError: If ``value`` does not include ``1.0``
     """
     if 1.0 not in values:
-        if use_alias:
-            raise ValueError(f"{obj.alias} must contain 1.0")
-        else:
-            raise ValueError(f"{obj.name} must contain 1.0")
+        raise ValueError(
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must "
+            f"contain the element 1.0. Given: {values}."
+        )
