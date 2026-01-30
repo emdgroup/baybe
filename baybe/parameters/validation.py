51f5dc656229d3b677a961ea7e680e8f91903791
AdrianSosic <adrian.sosic@gmail.com>
2026-01-30 14:12:21 +0100

    Refactor validators

--- baybe/parameters/validation.py
+++ baybe/parameters/validation.py
@@ -1,22 +1,38 @@
 """Validation functionality for parameters."""
 
 from collections.abc import Collection
-from typing import Any
 
 from attrs import Attribute
 from attrs.validators import gt, instance_of, lt
 
+from baybe.parameters.base import Parameter
 
-def validate_decorrelation(obj: Any, attribute: Attribute, value: Any) -> None:
-    """Validate the decorrelation."""
+
+def validate_decorrelation(obj: Parameter, attribute: Attribute, value: float) -> None:
+    """Validate that the input represents a valid decorrelation setting."""
     instance_of((bool, float))(obj, attribute, value)
     if isinstance(value, float):
         gt(0.0)(obj, attribute, value)
         lt(1.0)(obj, attribute, value)
 
 
+def validate_contains_exactly_one_zero(  # noqa: DOC101, DOC103
+    obj: Parameter, attribute: Attribute, values: Collection[float]
+) -> None:
+    """Validate that the input contains exactly one element equal to ``0.0``.
+
+    Raises:
+        ValueError: If the input does not contain ``0.0`` exactly once.
+    """
+    if (count := list(values).count(0.0)) != 1:
+        raise ValueError(
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must contain "
+            f"the element 0.0 exactly once. Found {count} such elements: {values}."
+        )
+
+
 def validate_contains_one(  # noqa: DOC101, DOC103
-    obj: Any, _: Any, values: Collection[float], use_alias: bool = True
+    obj: Parameter, attribute: Attribute, values: Collection[float]
 ):
     """Validate that ``value`` contains at least one entry equal to ``1.0``.
 
@@ -24,7 +40,7 @@ def validate_contains_one(  # noqa: DOC101, DOC103
         ValueError: If ``value`` does not include ``1.0``
     """
     if 1.0 not in values:
-        if use_alias:
-            raise ValueError(f"{obj.alias} must contain 1.0")
-        else:
-            raise ValueError(f"{obj.name} must contain 1.0")
+        raise ValueError(
+            f"The '{attribute.alias}' attribute of parameter '{obj.name}' must "
+            f"contain the element 1.0. Given: {values}."
+        )
