5e8c7de61a6bdf66c6c84288203258bdb911bf4b
AdrianSosic <adrian.sosic@gmail.com>
2026-02-10 17:54:22 +0100

    Extract current GP defaults in into separate EDBO module

--- baybe/surrogates/gaussian_process/presets/default.py
+++ baybe/surrogates/gaussian_process/presets/default.py
@@ -5,15 +5,15 @@ from __future__ import annotations
 import gc
 from typing import TYPE_CHECKING
 
-import numpy as np
 from attrs import define
 from typing_extensions import override
 
-from baybe.kernels.basic import MaternKernel
-from baybe.kernels.composite import ScaleKernel
-from baybe.parameters import TaskParameter
 from baybe.priors.basic import GammaPrior
 from baybe.surrogates.gaussian_process.kernel_factory import KernelFactory
+from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
+    SmoothedEDBOKernelFactory,
+    _smoothed_edbo_noise_factory,
+)
 
 if TYPE_CHECKING:
     from torch import Tensor
@@ -21,76 +21,24 @@ if TYPE_CHECKING:
     from baybe.kernels.base import Kernel
     from baybe.searchspace.core import SearchSpace
 
-# Boundaries for low and high dimension limits
-_DIM_LIMITS = (8, 75)
-
 
 @define
 class DefaultKernelFactory(KernelFactory):
-    """A factory providing the default kernel for Gaussian process surrogates.
-
-    This is taking the low and high dimensional limits of
-    :class:`baybe.surrogates.gaussian_process.presets.edbo.EDBOKernelFactory`
-    and interpolates the prior moments linearly between them.
-    """
+    """A factory providing the default kernel for Gaussian process surrogates."""
 
     @override
     def __call__(
         self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
     ) -> Kernel:
-        effective_dims = train_x.shape[-1] - len(
-            [p for p in searchspace.parameters if isinstance(p, TaskParameter)]
-        )
-
-        # Interpolate prior moments linearly between low D and high D regime
-        # The high D regime itself is the average of the EDBO OHE and Mordred regime
-        # Values outside the dimension limits will get the border value assigned
-        lengthscale_prior = GammaPrior(
-            np.interp(effective_dims, _DIM_LIMITS, [1.2, 2.5]),
-            np.interp(effective_dims, _DIM_LIMITS, [1.1, 0.55]),
-        )
-        lengthscale_initial_value = np.interp(effective_dims, _DIM_LIMITS, [0.2, 6.0])
-        outputscale_prior = GammaPrior(
-            np.interp(effective_dims, _DIM_LIMITS, [5.0, 3.5]),
-            np.interp(effective_dims, _DIM_LIMITS, [0.5, 0.15]),
-        )
-        outputscale_initial_value = np.interp(effective_dims, _DIM_LIMITS, [8.0, 15.0])
-
-        return ScaleKernel(
-            MaternKernel(
-                nu=2.5,
-                lengthscale_prior=lengthscale_prior,
-                lengthscale_initial_value=lengthscale_initial_value,
-            ),
-            outputscale_prior=outputscale_prior,
-            outputscale_initial_value=outputscale_initial_value,
-        )
+        return SmoothedEDBOKernelFactory()(searchspace, train_x, train_y)
 
 
 def _default_noise_factory(
     searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
 ) -> tuple[GammaPrior, float]:
-    """Create the default noise settings for the Gaussian process surrogate.
-
-    This is taking the low and high dimensional limits of
-    :func:`baybe.surrogates.gaussian_process.presets.edbo._edbo_noise_factory`
-    and interpolates the prior moments linearly between them.
-    """
+    """Create the default noise settings for the Gaussian process surrogate."""
     # TODO: Replace this function with a proper likelihood factory
-
-    # Interpolate prior moments linearly between low D and high D regime
-    # The high D regime itself is the average of the EDBO OHE and Mordred regime
-    # Values outside the dimension limits will get the border value assigned
-    effective_dims = train_x.shape[-1] - len(
-        [p for p in searchspace.parameters if isinstance(p, TaskParameter)]
-    )
-    return (
-        GammaPrior(
-            np.interp(effective_dims, _DIM_LIMITS, [1.05, 1.5]),
-            np.interp(effective_dims, _DIM_LIMITS, [0.5, 0.1]),
-        ),
-        np.interp(effective_dims, _DIM_LIMITS, [0.1, 5.0]).item(),
-    )
+    return _smoothed_edbo_noise_factory(searchspace, train_x, train_y)
 
 
 # Collect leftover original slotted classes processed by `attrs.define`
