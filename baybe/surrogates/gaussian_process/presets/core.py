6b487160d66c64e9717573a5312953edf7122d07
AdrianSosic <adrian.sosic@gmail.com>
2026-02-10 11:16:48 +0100

    Add EDBO presets

--- baybe/surrogates/gaussian_process/presets/core.py
+++ baybe/surrogates/gaussian_process/presets/core.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, assert_never
 
 if TYPE_CHECKING:
     from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
@@ -13,16 +13,29 @@ class GaussianProcessPreset(Enum):
     """Available Gaussian process surrogate presets."""
 
     BAYBE = "BAYBE"
-    """Recreates the default settings of the Gaussian process surrogate class."""
+    """The default BayBE settings of the Gaussian process surrogate class."""
+
+    EDBO = "EDBO"
+    """The EDBO settings."""
+
+    EDBO_SMOOTHED = "EDBO_SMOOTHED"
+    """A smoothed version of the EDBO settings."""
 
 
 def make_gp_from_preset(preset: GaussianProcessPreset) -> GaussianProcessSurrogate:
     """Create a :class:`GaussianProcessSurrogate` from a :class:`GaussianProcessPreset."""  # noqa: E501
     from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
+    from baybe.surrogates.gaussian_process.presets.edbo import EDBOKernelFactory
+    from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
+        SmoothedEDBOKernelFactory,
+    )
 
+    # TODO: Pass mean and likelihood settings once possible
     if preset is GaussianProcessPreset.BAYBE:
         return GaussianProcessSurrogate()
+    if preset is GaussianProcessPreset.EDBO:
+        return GaussianProcessSurrogate(kernel_or_factory=EDBOKernelFactory())
+    if preset is GaussianProcessPreset.EDBO_SMOOTHED:
+        return GaussianProcessSurrogate(kernel_or_factory=SmoothedEDBOKernelFactory())
 
-    raise ValueError(
-        f"Unknown '{GaussianProcessPreset.__name__}' with name '{preset.name}'."
-    )
+    assert_never(preset)
