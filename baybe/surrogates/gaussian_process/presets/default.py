7a509d4abc901af3e2ee47d08921ff2e09095e18
AdrianSosic <adrian.sosic@gmail.com>
2026-02-11 14:56:18 +0100

    Deduplicate code in default preset

--- baybe/surrogates/gaussian_process/presets/default.py
+++ baybe/surrogates/gaussian_process/presets/default.py
@@ -2,63 +2,19 @@
 
 from __future__ import annotations
 
-import gc
-from typing import TYPE_CHECKING
-
-from attrs import define
-from typing_extensions import override
-
-from baybe.surrogates.gaussian_process.components.kernel import KernelFactory
-from baybe.surrogates.gaussian_process.components.likelihood import LikelihoodFactory
-from baybe.surrogates.gaussian_process.components.mean import MeanFactory
 from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
     SmoothedEDBOKernelFactory,
     SmoothedEDBOLikelihoodFactory,
 )
+from baybe.surrogates.gaussian_process.presets.utils import LazyConstantMeanFactory
 
-if TYPE_CHECKING:
-    from gpytorch.likelihoods import Likelihood as GPyTorchLikelihood
-    from gpytorch.means import Mean as GPyTorchMean
-    from torch import Tensor
-
-    from baybe.kernels.base import Kernel
-    from baybe.searchspace.core import SearchSpace
+DefaultKernelFactory = SmoothedEDBOKernelFactory
+"""The factory providing the default kernel for Gaussian process surrogates."""
 
 
-@define
-class DefaultKernelFactory(KernelFactory):
-    """A factory providing the default kernel for Gaussian process surrogates."""
-
-    @override
-    def __call__(
-        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
-    ) -> Kernel:
-        return SmoothedEDBOKernelFactory()(searchspace, train_x, train_y)
+DefaultMeanFactory = LazyConstantMeanFactory
+"""The factory providing the default mean function for Gaussian process surrogates."""
 
 
-@define
-class DefaultMeanFactory(MeanFactory):
-    """A factory providing the default mean function for Gaussian process surrogates."""
-
-    @override
-    def __call__(
-        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
-    ) -> GPyTorchMean:
-        from gpytorch.means import ConstantMean
-
-        return ConstantMean()
-
-
-@define
-class DefaultLikelihoodFactory(LikelihoodFactory):
-    """A factory providing the default likelihood for Gaussian process surrogates."""
-
-    @override
-    def __call__(
-        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
-    ) -> GPyTorchLikelihood:
-        return SmoothedEDBOLikelihoodFactory()(searchspace, train_x, train_y)
-
-
-# Collect leftover original slotted classes processed by `attrs.define`
-gc.collect()
+DefaultLikelihoodFactory = SmoothedEDBOLikelihoodFactory
+"""The factory providing the default likelihood for Gaussian process surrogates."""
