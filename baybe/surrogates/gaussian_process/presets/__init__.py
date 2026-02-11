9df998fd075528988739ffacd9ce90c1fce2f724
AdrianSosic <adrian.sosic@gmail.com>
2026-02-11 14:51:08 +0100

    Add missing entries to __init__.py

--- baybe/surrogates/gaussian_process/presets/__init__.py
+++ baybe/surrogates/gaussian_process/presets/__init__.py
@@ -1,19 +1,46 @@
 """Gaussian process surrogate presets."""
 
+# Core
 from baybe.surrogates.gaussian_process.presets.core import (
     GaussianProcessPreset,
     make_gp_from_preset,
 )
-from baybe.surrogates.gaussian_process.presets.default import DefaultKernelFactory
-from baybe.surrogates.gaussian_process.presets.edbo import EDBOKernelFactory
+
+# Default preset
+from baybe.surrogates.gaussian_process.presets.default import (
+    DefaultKernelFactory,
+    DefaultLikelihoodFactory,
+    DefaultMeanFactory,
+)
+
+# EDBO preset
+from baybe.surrogates.gaussian_process.presets.edbo import (
+    EDBOKernelFactory,
+    EDBOLikelihoodFactory,
+    EDBOMeanFactory,
+)
+
+# Smoothed EDBO preset
 from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
     SmoothedEDBOKernelFactory,
+    SmoothedEDBOLikelihoodFactory,
+    SmoothedEDBOMeanFactory,
 )
 
 __all__ = [
-    "DefaultKernelFactory",
-    "EDBOKernelFactory",
+    # Core
     "GaussianProcessPreset",
-    "SmoothedEDBOKernelFactory",
     "make_gp_from_preset",
+    # Default preset
+    "DefaultKernelFactory",
+    "DefaultLikelihoodFactory",
+    "DefaultMeanFactory",
+    # EDBO preset
+    "EDBOKernelFactory",
+    "EDBOLikelihoodFactory",
+    "EDBOMeanFactory",
+    # Smoothed EDBO preset
+    "SmoothedEDBOKernelFactory",
+    "SmoothedEDBOLikelihoodFactory",
+    "SmoothedEDBOMeanFactory",
 ]
