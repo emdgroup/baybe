1df0e5805a800dacd9f9290fd00e735f6b23abaf
AdrianSosic <adrian.sosic@gmail.com>
2026-02-12 09:07:41 +0100

    Absorb index kernel construction into ICMKernelFactory

--- baybe/surrogates/gaussian_process/components/kernel.py
+++ baybe/surrogates/gaussian_process/components/kernel.py
@@ -4,7 +4,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from attrs import define, field
+from typing_extensions import override
+
 from baybe.kernels.base import Kernel
+from baybe.kernels.composite import ProductKernel
+from baybe.searchspace.core import SearchSpace
 from baybe.surrogates.gaussian_process.components.generic import (
     ComponentFactory,
     PlainComponentFactory,
@@ -12,6 +17,7 @@ from baybe.surrogates.gaussian_process.components.generic import (
 
 if TYPE_CHECKING:
     from gpytorch.kernels import Kernel as GPyTorchKernel
+    from torch import Tensor
 
     KernelFactory = ComponentFactory[Kernel | GPyTorchKernel]
     PlainKernelFactory = PlainComponentFactory[Kernel | GPyTorchKernel]
@@ -19,3 +25,41 @@ else:
     # At runtime, we use only the BayBE type for serialization compatibility
     KernelFactory = ComponentFactory[Kernel]
     PlainKernelFactory = PlainComponentFactory[Kernel]
+
+
+@define
+class ICMKernelFactory(KernelFactory):
+    """A kernel factory that constructs an ICM kernel for transfer learning.
+
+    ICM: Intrinsic model of coregionalization
+    """
+
+    base_kernel_factory: KernelFactory = field(alias="base_kernel_or_factory")
+    """The factory for the base kernel operating on numerical input features."""
+
+    task_kernel_factory: KernelFactory = field(alias="task_kernel_or_factory")
+    """The factory for the task kernel operating on the task indices."""
+
+    @base_kernel_factory.default
+    def _default_base_kernel_factory(self) -> KernelFactory:
+        from baybe.surrogates.gaussian_process.presets.baybe import (
+            DefaultNumericalKernelFactory,
+        )
+
+        return DefaultNumericalKernelFactory()
+
+    @task_kernel_factory.default
+    def _default_task_kernel_factory(self) -> KernelFactory:
+        from baybe.surrogates.gaussian_process.presets.baybe import (
+            DefaultTaskKernelFactory,
+        )
+
+        return DefaultTaskKernelFactory()
+
+    @override
+    def __call__(
+        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
+    ) -> Kernel:
+        base_kernel = self.base_kernel_factory(searchspace, train_x, train_y)
+        task_kernel = self.task_kernel_factory(searchspace, train_x, train_y)
+        return ProductKernel([base_kernel, task_kernel])
--- baybe/surrogates/gaussian_process/presets/baybe.py
+++ baybe/surrogates/gaussian_process/presets/baybe.py
@@ -2,14 +2,53 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from attrs import define
+from typing_extensions import override
+
+from baybe.kernels.base import Kernel
+from baybe.kernels.basic import IndexKernel
+from baybe.searchspace.core import SearchSpace
+from baybe.surrogates.gaussian_process.components.kernel import KernelFactory
 from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
     SmoothedEDBOKernelFactory,
     SmoothedEDBOLikelihoodFactory,
 )
 from baybe.surrogates.gaussian_process.presets.factories import LazyConstantMeanFactory
 
-DefaultKernelFactory = SmoothedEDBOKernelFactory
-"""The factory providing the default kernel for Gaussian process surrogates."""
+if TYPE_CHECKING:
+    from torch import Tensor
+
+
+@define
+class DefaultKernelFactory(KernelFactory):
+    """The default kernel factory for Gaussian process surrogates."""
+
+    @override
+    def __call__(
+        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
+    ) -> Kernel:
+        from baybe.surrogates.gaussian_process.components.kernel import ICMKernelFactory
+
+        is_multitask = searchspace.n_tasks > 0
+        factory = ICMKernelFactory if is_multitask else DefaultNumericalKernelFactory
+        return factory()(searchspace, train_x, train_y)
+
+
+DefaultNumericalKernelFactory = SmoothedEDBOKernelFactory
+"""The factory providing the default numerical kernel for Gaussian process surrogates."""  # noqa: E501
+
+
+@define
+class DefaultTaskKernelFactory(KernelFactory):
+    """The factory providing the default task kernel for Gaussian process surrogates."""
+
+    @override
+    def __call__(
+        self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
+    ) -> Kernel:
+        return IndexKernel(num_tasks=searchspace.n_tasks, rank=searchspace.n_tasks)
 
 
 DefaultMeanFactory = LazyConstantMeanFactory
