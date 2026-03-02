ed9cc553abcf51bc80f46afe8e5662a5b1664047
AdrianSosic <adrian.sosic@gmail.com>
2026-03-02 15:53:36 +0100

    Extend BoTorch factories to multitask case

--- baybe/surrogates/gaussian_process/presets/botorch.py
+++ baybe/surrogates/gaussian_process/presets/botorch.py
@@ -12,7 +12,11 @@ from typing_extensions import override
 from baybe.kernels.base import Kernel
 from baybe.searchspace.core import SearchSpace
 from baybe.surrogates.gaussian_process.components import LikelihoodFactoryProtocol
+from baybe.surrogates.gaussian_process.components._gpytorch import (
+    make_botorch_multitask_likelihood,
+)
 from baybe.surrogates.gaussian_process.components.kernel import (
+    ICMKernelFactory,
     KernelFactoryProtocol,
 )
 from baybe.surrogates.gaussian_process.components.mean import MeanFactoryProtocol
@@ -31,18 +35,31 @@ class BotorchKernelFactory(KernelFactoryProtocol):
     def __call__(
         self, searchspace: SearchSpace, train_x: Tensor, train_y: Tensor
     ) -> Kernel | GPyTorchKernel:
+        from botorch.models.kernels.positive_index import PositiveIndexKernel
         from botorch.models.utils.gpytorch_modules import (
             get_covar_module_with_dim_scaled_prior,
         )
 
-        base = get_covar_module_with_dim_scaled_prior(
-            ard_num_dims=len(searchspace.comp_rep_columns)
-        )
-
         if searchspace.n_tasks == 1:
-            return base
+            return get_covar_module_with_dim_scaled_prior(
+                ard_num_dims=len(searchspace.comp_rep_columns), active_dims=None
+            )
 
-        raise NotImplementedError()
+        assert searchspace.task_idx is not None
+        base_idcs = [
+            idx
+            for idx in range(len(searchspace.comp_rep_columns))
+            if idx != searchspace.task_idx
+        ]
+        base = get_covar_module_with_dim_scaled_prior(
+            ard_num_dims=len(base_idcs), active_dims=base_idcs
+        )
+        index_kernel = PositiveIndexKernel(
+            num_tasks=searchspace.n_tasks,
+            rank=searchspace.n_tasks,
+            active_dims=[searchspace.task_idx],
+        )
+        return ICMKernelFactory(base, index_kernel)(searchspace, train_x, train_y)
 
 
 class BotorchMeanFactory(MeanFactoryProtocol):
@@ -54,10 +71,17 @@ class BotorchMeanFactory(MeanFactoryProtocol):
     ) -> GPyTorchMean:
         from gpytorch.means import ConstantMean
 
+        from baybe.surrogates.gaussian_process.components._gpytorch import (
+            HadamardConstantMean,
+        )
+
         if searchspace.n_tasks == 1:
             return ConstantMean()
 
-        raise NotImplementedError()
+        assert searchspace.task_idx is not None
+        return HadamardConstantMean(
+            ConstantMean(), searchspace.n_tasks, searchspace.task_idx
+        )
 
 
 class BotorchLikelihoodFactory(LikelihoodFactoryProtocol):
@@ -74,7 +98,10 @@ class BotorchLikelihoodFactory(LikelihoodFactoryProtocol):
         if searchspace.n_tasks == 1:
             return get_gaussian_likelihood_with_lognormal_prior()
 
-        raise NotImplementedError()
+        assert searchspace.task_idx is not None
+        return make_botorch_multitask_likelihood(
+            num_tasks=searchspace.n_tasks, task_feature=searchspace.task_idx
+        )
 
 
 # Collect leftover original slotted classes processed by `attrs.define`
