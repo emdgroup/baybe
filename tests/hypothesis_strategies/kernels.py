ffffafee332437457be1d7b21266941e9f6eeb8f
AdrianSosic <adrian.sosic@gmail.com>
2026-03-04 14:12:01 +0100

    Add parameter selection to kernel hypothesis strategies

--- tests/hypothesis_strategies/kernels.py
+++ tests/hypothesis_strategies/kernels.py
@@ -1,5 +1,6 @@
 """Hypothesis strategies for kernels."""
 
+from collections.abc import Sequence
 from enum import Enum
 
 import hypothesis.strategies as st
@@ -18,6 +19,7 @@ from baybe.kernels.basic import (
 )
 from baybe.kernels.composite import AdditiveKernel, ProductKernel, ScaleKernel
 from tests.hypothesis_strategies.basic import positive_finite_floats
+from tests.hypothesis_strategies.parameters import parameter_names
 from tests.hypothesis_strategies.priors import priors
 
 
@@ -29,99 +31,143 @@ class KernelType(Enum):
     PRODUCT = "PRODUCT"
 
 
-linear_kernels = st.builds(
-    LinearKernel,
-    variance_prior=st.one_of(st.none(), priors()),
-    variance_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates linear kernels."""
+def active_parameter_names(names: Sequence[str] | None = None):
+    """A strategy generating optional parameter names for kernels to operate on."""
+    if names is None:
+        return st.one_of(
+            st.none(), st.lists(parameter_names, min_size=1, max_size=5, unique=True)
+        )
+    return st.just(names)
 
-matern_kernels = st.builds(
-    MaternKernel,
-    nu=st.sampled_from((0.5, 1.5, 2.5)),
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates Matern kernels."""
 
-periodic_kernels = st.builds(
-    PeriodicKernel,
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-    period_length_prior=st.one_of(st.none(), priors()),
-    period_length_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates periodic kernels."""
+def linear_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates linear kernels."""
+    return st.builds(
+        LinearKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        variance_prior=st.one_of(st.none(), priors()),
+        variance_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
 
-piecewise_polynomial_kernels = st.builds(
-    PiecewisePolynomialKernel,
-    q=st.integers(min_value=0, max_value=3),
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates piecewise polynomial kernels."""
 
-polynomial_kernels = st.builds(
-    PolynomialKernel,
-    power=st.integers(min_value=0),
-    offset_prior=st.one_of(st.none(), priors()),
-    offset_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates polynomial kernels."""
+def matern_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates Matern kernels."""
+    return st.builds(
+        MaternKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        nu=st.sampled_from((0.5, 1.5, 2.5)),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
 
-rbf_kernels = st.builds(
-    RBFKernel,
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates radial basis function (RBF) kernels."""
 
-rff_kernels = st.builds(
-    RFFKernel,
-    num_samples=st.integers(min_value=1),
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates random Fourier features (RFF) kernels."""
+def periodic_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates periodic kernels."""
+    return st.builds(
+        PeriodicKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+        period_length_prior=st.one_of(st.none(), priors()),
+        period_length_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
 
-rq_kernels = st.builds(
-    RQKernel,
-    lengthscale_prior=st.one_of(st.none(), priors()),
-    lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
-)
-"""A strategy that generates rational quadratic (RQ) kernels."""
+
+def piecewise_polynomial_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates piecewise polynomial kernels."""
+    return st.builds(
+        PiecewisePolynomialKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        q=st.integers(min_value=0, max_value=3),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
+
+
+def polynomial_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates polynomial kernels."""
+    return st.builds(
+        PolynomialKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        power=st.integers(min_value=0),
+        offset_prior=st.one_of(st.none(), priors()),
+        offset_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
+
+
+def rbf_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates radial basis function (RBF) kernels."""
+    return st.builds(
+        RBFKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
+
+
+def rff_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates random Fourier features (RFF) kernels."""
+    return st.builds(
+        RFFKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        num_samples=st.integers(min_value=1),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
+
+
+def rq_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates rational quadratic (RQ) kernels."""
+    return st.builds(
+        RQKernel,
+        parameter_names=active_parameter_names(parameter_names),
+        lengthscale_prior=st.one_of(st.none(), priors()),
+        lengthscale_initial_value=st.one_of(st.none(), positive_finite_floats()),
+    )
 
 
 @st.composite
-def index_kernels(draw: st.DrawFn):
+def index_kernels(
+    draw: st.DrawFn,
+    parameter_names: Sequence[str] | None = None,
+):
     """A strategy that generates index kernels."""
     num_tasks = draw(st.integers(min_value=2, max_value=5))
     rank = draw(st.integers(min_value=1, max_value=num_tasks))
+    names = draw(active_parameter_names(parameter_names))
     if draw(st.booleans()):
-        return PositiveIndexKernel(num_tasks=num_tasks, rank=rank)
-    return IndexKernel(num_tasks=num_tasks, rank=rank)
+        return PositiveIndexKernel(
+            parameter_names=names,
+            num_tasks=num_tasks,
+            rank=rank,
+        )
+    return IndexKernel(parameter_names=names, num_tasks=num_tasks, rank=rank)
 
 
-base_kernels = st.one_of(
-    [
-        matern_kernels,  # on top because it is the default for many use cases
-        linear_kernels,
-        rbf_kernels,
-        rq_kernels,
-        rff_kernels,
-        index_kernels(),
-        piecewise_polynomial_kernels,
-        polynomial_kernels,
-        periodic_kernels,
-    ]
-)
-"""A strategy that generates base kernels to be used within more complex kernels."""
+def base_kernels(parameter_names: Sequence[str] | None = None):
+    """A strategy that generates base kernels to be used within more complex kernels."""
+    return st.one_of(
+        [
+            matern_kernels(parameter_names),  # on top because it is the default
+            linear_kernels(parameter_names),
+            rbf_kernels(parameter_names),
+            rq_kernels(parameter_names),
+            rff_kernels(parameter_names),
+            index_kernels(parameter_names=parameter_names),
+            piecewise_polynomial_kernels(parameter_names),
+            polynomial_kernels(parameter_names),
+            periodic_kernels(parameter_names),
+        ]
+    )
 
 
 @st.composite
-def single_kernels(draw: st.DrawFn):
+def single_kernels(
+    draw: st.DrawFn,
+    parameter_names: Sequence[str] | None = None,
+):
     """Generate single kernels (i.e., without kernel arithmetic, except scaling)."""
-    base_kernel = draw(base_kernels)
+    base_kernel = draw(base_kernels(parameter_names))
     add_scale = draw(st.booleans())
     if add_scale:
         return ScaleKernel(
@@ -136,14 +182,19 @@ def single_kernels(draw: st.DrawFn):
 
 
 @st.composite
-def kernels(draw: st.DrawFn):
+def kernels(
+    draw: st.DrawFn,
+    parameter_names: Sequence[str] | None = None,
+):
     """Generate :class:`baybe.kernels.base.Kernel`."""
     kernel_type = draw(st.sampled_from(KernelType))
 
     if kernel_type is KernelType.SINGLE:
-        return draw(single_kernels())
+        return draw(single_kernels(parameter_names=parameter_names))
 
-    base_kernels = draw(st.lists(single_kernels(), min_size=2))
+    base_kernels = draw(
+        st.lists(single_kernels(parameter_names=parameter_names), min_size=2)
+    )
     if kernel_type is KernelType.ADDITIVE:
         return AdditiveKernel(base_kernels)
     if kernel_type is KernelType.PRODUCT:
