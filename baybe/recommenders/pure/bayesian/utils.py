6bc1c81387b618e5fa235486fbd46245ede94532
Jordan Penn <jordan.penn5841@gmail.com>
2026-03-05 15:16:00 +0000

    Add recommender logic.

---
+++ baybe/recommenders/pure/bayesian/utils.py
@@ -0,0 +1,30 @@
+"""Utils for bayesian recommenders."""
+
+from typing import TYPE_CHECKING
+
+from attrs import evolve
+
+from baybe.parameters import CategoricalFidelityParameter
+
+if TYPE_CHECKING:
+    from baybe.searchspace import SearchSpace
+
+
+def restricted_fidelity_searchspace(searchspace: SearchSpace, /) -> SearchSpace:
+    """Evolve a multi-fidelity searchspace so the fidelity is fixed to the highest."""
+    discrete_parameters_fixed_fidelities = tuple(
+        evolve(p, values=(p.to_index((1.0,)).item(),))
+        if isinstance(p, CategoricalFidelityParameter)
+        else p
+        for p in searchspace.discrete.parameters
+    )
+
+    discrete_subspace_fixed_fidelities = evolve(
+        searchspace.discrete, parameters=discrete_parameters_fixed_fidelities
+    )
+
+    fixed_fidelity_searchspace = evolve(
+        searchspace, discrete=discrete_subspace_fixed_fidelities
+    )
+
+    return fixed_fidelity_searchspace
