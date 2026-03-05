d6a85205b8054dfdde073ed0bd8734a4f89350f3
Jordan Penn <jordan.penn5841@gmail.com>
2026-03-05 13:57:44 +0000

    Add multi fidelity acqfs to acquistion init files

--- baybe/acquisition/custom_acqfs/__init__.py
+++ baybe/acquisition/custom_acqfs/__init__.py
@@ -4,6 +4,13 @@ from baybe.acquisition.custom_botorch_acqfs.two_stage import (
     MultiFidelityUpperConfidenceBound,
 )
 
+MFUCB = MultiFidelityUpperConfidenceBound
+
 __all__ = [
+    ######################### Acquisition functions
+    # Upper Confidence Bound
     "MultiFidelityUpperConfidenceBound",
+    ######################### Abbreviations
+    # Upper Confidence Bound
+    "MFUCB",
 ]
