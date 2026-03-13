7df5fc93a5132a3365546993516cd106156c9a1d
Jordan Penn <jordan.penn5841@gmail.com>
2026-03-13 09:32:26 +0000

    Typing fixes: working around broad Botorch typing

--- baybe/acquisition/custom_acqfs/two_stage.py
+++ baybe/acquisition/custom_acqfs/two_stage.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
+from itertools import pairwise as iter_pairwise
 from itertools import product as iter_product
+from typing import cast
 
 import torch
 from attrs import define, field
@@ -11,9 +13,12 @@ from botorch.acquisition.analytic import AnalyticAcquisitionFunction
 from botorch.acquisition.objective import PosteriorTransform
 from botorch.models.model import Model
 from botorch.utils.transforms import (
+    average_over_ensemble_models,
     t_batch_mode_transform,
 )
+from gpytorch.likelihoods import GaussianLikelihood
 from torch import Tensor
+from typing_extensions import override
 
 from baybe.parameters.validation import validate_contains_exactly_one
 from baybe.utils.validation import finite_float, validate_dict_shape
@@ -36,6 +41,12 @@ class MultiFidelityUpperConfidenceBound(AnalyticAcquisitionFunction):
     Only supports the case of `q=1` (i.e. greedy, non-batch
     selection of design points). The model must be single-outcome.
     """
+
+    # Jordan MHS NOTE: typing awkward here since register_buffer does not declare attr.
+    fidelity_columns: Tensor
+    fidelities_comb: Tensor
+    zetas_comb: Tensor
+    costs_comb: Tensor
 
     model: Model = field(validator=instance_of(Model))
     """A fitted single-outcome GP model.
@@ -75,7 +86,7 @@ class MultiFidelityUpperConfidenceBound(AnalyticAcquisitionFunction):
     fidelity parameters are summed.
     """
 
-    zetas: dict[int, tuple[float, ...]] | None = field(
+    zetas: dict[int, tuple[float, ...]] = field(
         validator=deep_mapping(
             key_validator=instance_of(int),
             value_validator=deep_iterable(
@@ -138,7 +149,11 @@ class MultiFidelityUpperConfidenceBound(AnalyticAcquisitionFunction):
             torch.tensor(list(iter_product(*self.costs.values())), dtype=torch.double),
         )
 
-    @t_batch_mode_transform(expected_q=1)
+    # Jordan MHS NOTE: mypy typing errors for these decorators with on
+    # subclasses of AcquistionFunction appear in Botorch as well as here.
+    @override
+    @t_batch_mode_transform(expected_q=1)  # type: ignore
+    @average_over_ensemble_models  # type: ignore
     def forward(self, X: Tensor) -> Tensor:
         r"""First optimization stage: choose optimal design design to query.
 
@@ -166,7 +181,9 @@ class MultiFidelityUpperConfidenceBound(AnalyticAcquisitionFunction):
         means, sigmas = self._mean_and_sigma(X_eval)
 
         means = means.view(batch_size, n_comb, q, 1)
-        sigmas = sigmas.view(batch_size, n_comb, q, 1)
+        # Jordan MHS NOTE: typing workaround to ignore possibility for botorch
+        # AnalyticAcquisitionFunction _mean_and_sigma to have compute_sigma=False.
+        sigmas = sigmas.view(batch_size, n_comb, q, 1)  # type: ignore
 
         sign = 1 if self.maximize else -1
         indiv_ucbs = sign * means + (self.beta**0.5) * sigmas + zetas_extended
@@ -189,45 +206,44 @@ class MultiFidelityUpperConfidenceBound(AnalyticAcquisitionFunction):
 
     def optimize_stage_two(self, X: Tensor) -> Tensor:
         r"""Second optimisation stage: choose optimal fidelity to query."""
-        # Jordan MHS possible TODO: consider heteroskedastic noise between fidelities.
-        aleatoric_uncertainty = torch.sqrt(self.model.likelihood.noise)
+        # Jordan MHS NOTE: casting here because botorch model likelihood is too
+        # broadly typed. Check best practice in case likelihood does not have noise.
+        likelihood = cast(GaussianLikelihood, self.model.likelihood)
+
+        # Possible TODO: consider heteroskedastic noise between fidelities.
+        aleatoric_uncertainty = torch.sqrt(likelihood.noise)
 
         found_suitable_lower_fid = False
-        optimal_X_cost = None
-
-        prev_fid = None
-        prev_cost = None
-        prev_zeta = None
 
         total_costs_comb = self.costs_comb.sum(dim=-1)
         increasing_cost_order = torch.argsort(total_costs_comb)
 
-        for i in increasing_cost_order:
-            curr_fid = self.fidelities_comb[i].clone()
-            curr_cost = self.costs_comb.sum(dim=-1)[i]
-            curr_zeta = self.zetas_comb.sum(dim=-1)[i]
+        for prev_i, curr_i in iter_pairwise(increasing_cost_order):
+            prev_fid = self.fidelities_comb[prev_i].clone()
+            prev_cost = self.costs_comb.sum(dim=-1)[prev_i]
+            curr_cost = self.costs_comb.sum(dim=-1)[curr_i]
+            prev_zeta = self.zetas_comb.sum(dim=-1)[prev_i]
 
-            if prev_cost is not None:
-                X_prev_fid = X.clone()
-                X_prev_fid[:, self.fidelity_columns] = prev_fid
+            X_prev_fid = X.clone()
+            X_prev_fid[:, self.fidelity_columns] = prev_fid
 
-                _, curr_posterior_uncertainty = self._mean_and_sigma(X_prev_fid)
+            _, prev_posterior_uncertainty = self._mean_and_sigma(X_prev_fid)
 
-                if (self.beta**0.5) * curr_posterior_uncertainty >= (
-                    aleatoric_uncertainty + prev_zeta
-                ) * torch.sqrt(prev_cost / curr_cost):
-                    found_suitable_lower_fid = True
-                    optimal_X = X_prev_fid
-                    optimal_X_cost = prev_cost
-                    break
+            # Jordan MHS NOTE: workaround poor typing in Botorch.
+            # _mean_and_sigma always returns two values unless the argument
+            # compute_sigma is set to False.
+            assert prev_posterior_uncertainty is not None, "This shouldn't be accesible"
 
-            prev_fid = curr_fid.clone()
-            prev_cost = curr_cost.clone()
-            prev_zeta = curr_zeta.clone()
+            if (self.beta**0.5) * prev_posterior_uncertainty >= (
+                aleatoric_uncertainty + prev_zeta
+            ) * torch.sqrt(prev_cost / curr_cost):
+                found_suitable_lower_fid = True
+                optimal_X = X_prev_fid
+                break
 
         if not found_suitable_lower_fid:
             optimal_X = X.clone()
-            optimal_X[:, self.fidelity_columns] = curr_fid
-            optimal_X_cost = curr_cost
+            last_fid = self.fidelities_comb[curr_i].clone()
+            optimal_X[:, self.fidelity_columns] = last_fid
 
-        return optimal_X, optimal_X_cost
+        return optimal_X
