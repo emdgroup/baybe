d699ed92dd9284db062c2579e49ec72204239e31
Jordan Penn <jordan.penn5841@gmail.com>
2026-03-13 11:13:12 +0000

    Fixed custom acqf imports

--- baybe/acquisition/custom_acqfs/__init__.py
+++ baybe/acquisition/custom_acqfs/__init__.py
@@ -4,13 +4,8 @@ from baybe.acquisition.custom_acqfs.two_stage import (
     MultiFidelityUpperConfidenceBound,
 )
 
-MFUCB = MultiFidelityUpperConfidenceBound
-
 __all__ = [
     ######################### Acquisition functions
     # Upper Confidence Bound
     "MultiFidelityUpperConfidenceBound",
-    ######################### Abbreviations
-    # Upper Confidence Bound
-    "MFUCB",
 ]
