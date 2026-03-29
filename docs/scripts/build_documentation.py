2a93ea0291ec0a9732b5f25a8ea08edd984da1a1
Karin Hrovatin <47607471+Hrovatin@users.noreply.github.com>
2026-03-29 12:41:13 +0200

    Bugfix - accidentally introduced code

--- docs/scripts/build_documentation.py
+++ docs/scripts/build_documentation.py
@@ -81,7 +81,7 @@ def build_documentation(
     examples_exist = examples_directory.is_dir()
 
     rerun_examples = run_examples or full_rebuild
-    # perform_linkcheck = verify_links or full_rebuild
+    perform_linkcheck = verify_links or full_rebuild
 
     if rerun_examples:
         build_examples(
@@ -98,8 +98,7 @@ def build_documentation(
             remove_dir=examples_exist,
         )
 
-    # if perform_linkcheck:
-    if False:
+    if perform_linkcheck:
         check_links()
 
     # Directory where the documentation is build.
