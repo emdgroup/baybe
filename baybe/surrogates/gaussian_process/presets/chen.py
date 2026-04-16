4e81b4633dc9d024c0aa2aea6145d017fe9d4fb8
AdrianSosic <adrian.sosic@gmail.com>
2026-04-16 10:33:06 +0200

    Apply ruff formatting

--- baybe/surrogates/gaussian_process/presets/chen.py
+++ baybe/surrogates/gaussian_process/presets/chen.py
@@ -1,13 +1,13 @@
-"""Adaptive Prior proposed in the paper: 
+"""Adaptive Prior proposed in the paper:
 Guanming Chen, Maximilian Fleck, Thijs Stuyver. Leveraging Hidden-Space Representations Effectively in Bayesian Optimization for Experiment Design through Dimension-Aware Hyperpriors. ChemRxiv. 09 February 2026.
-DOI: https://doi.org/10.26434/chemrxiv.10001986/v2"""
+DOI: https://doi.org/10.26434/chemrxiv.10001986/v2
+"""  # noqa
 
 from __future__ import annotations
 
 import gc
 from typing import TYPE_CHECKING
 
-import numpy as np
 from attrs import define
 from typing_extensions import override
 
@@ -25,12 +25,13 @@ if TYPE_CHECKING:
 
 import math
 
+
 @define
 class CHENKernelFactory(KernelFactory):
-    """ Surrogate model with an adaptive hyperprior proposed in the paper: 
-        Guanming Chen, Maximilian Fleck, Thijs Stuyver. Leveraging Hidden-Space Representations Effectively in Bayesian Optimization for Experiment Design through Dimension-Aware Hyperpriors. ChemRxiv. 09 February 2026.
-        DOI: https://doi.org/10.26434/chemrxiv.10001986/v2
-    """
+    """Surrogate model with an adaptive hyperprior proposed in the paper:
+    Guanming Chen, Maximilian Fleck, Thijs Stuyver. Leveraging Hidden-Space Representations Effectively in Bayesian Optimization for Experiment Design through Dimension-Aware Hyperpriors. ChemRxiv. 09 February 2026.
+    DOI: https://doi.org/10.26434/chemrxiv.10001986/v2
+    """  # noqa
 
     @override
     def __call__(
@@ -42,10 +43,10 @@ class CHENKernelFactory(KernelFactory):
 
         x = math.sqrt(effective_dims)
         l_mean = 0.4 * x + 4.0
-        
-        lengthscale_prior = GammaPrior(2.0*l_mean, 2.0)
+
+        lengthscale_prior = GammaPrior(2.0 * l_mean, 2.0)
         lengthscale_initial_value = l_mean
-        outputscale_prior = GammaPrior(1.0*l_mean, 1.0) 
+        outputscale_prior = GammaPrior(1.0 * l_mean, 1.0)
         outputscale_initial_value = l_mean
 
         return ScaleKernel(
@@ -57,6 +58,7 @@ class CHENKernelFactory(KernelFactory):
             outputscale_prior=outputscale_prior,
             outputscale_initial_value=outputscale_initial_value,
         )
+
 
 # Collect leftover original slotted classes processed by `attrs.define`
 gc.collect()
