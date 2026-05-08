159b27968ee16310e27047ffa55d44753ca16aa5
Martin Fitzner <martin.fitzner@merckgroup.com>
2026-05-08 18:22:15 +0200

    Fix docstring

--- baybe/surrogates/gaussian_process/presets/core.py
+++ baybe/surrogates/gaussian_process/presets/core.py
@@ -17,7 +17,7 @@ class GaussianProcessPreset(Enum):
 
 
 def make_gp_from_preset(preset: GaussianProcessPreset) -> GaussianProcessSurrogate:
-    """Create a :class:`GaussianProcessSurrogate` from a :class:`GaussianProcessPreset."""  # noqa: E501
+    """Create a :class:`GaussianProcessSurrogate` from a :class:`GaussianProcessPreset`."""  # noqa: E501
     from baybe.surrogates.gaussian_process.core import GaussianProcessSurrogate
 
     if preset is GaussianProcessPreset.BAYBE:
