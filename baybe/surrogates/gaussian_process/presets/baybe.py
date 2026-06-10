a2c00f33334dc1a1a8650d8d02923a18000d07b6
AdrianSosic <adrian.sosic@gmail.com>
2026-06-10 16:00:44 +0200

    Implement new BayBE preset logic

--- baybe/surrogates/gaussian_process/presets/baybe.py
+++ baybe/surrogates/gaussian_process/presets/baybe.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
-from typing import ClassVar
+import math
+from itertools import chain
+from typing import TYPE_CHECKING, ClassVar
 
 import pandas as pd
 from attrs import define, field
@@ -23,21 +25,75 @@ from baybe.surrogates.gaussian_process.components.fit_criterion import (
     FitCriterion,
     FitCriterionFactoryProtocol,
 )
-from baybe.surrogates.gaussian_process.components.kernel import _PureKernelFactory
-from baybe.surrogates.gaussian_process.components.mean import LazyConstantMeanFactory
-from baybe.surrogates.gaussian_process.presets.edbo_smoothed import (
-    SmoothedEDBOKernelFactory,
-    SmoothedEDBOLikelihoodFactory,
-    _SmoothedEDBONumericalKernelFactory,
+from baybe.surrogates.gaussian_process.components.kernel import (
+    _enable_transfer_learning,
+    _PureKernelFactory,
 )
+from baybe.surrogates.gaussian_process.components.likelihood import (
+    LikelihoodFactoryProtocol,
+)
+from baybe.surrogates.gaussian_process.components.mean import LazyConstantMeanFactory
+
+if TYPE_CHECKING:
+    from gpytorch.kernels import Kernel as GPyTorchKernel
+    from gpytorch.likelihoods import Likelihood as GPyTorchLikelihood
 
 
-class _BayBENumericalKernelFactory(_SmoothedEDBONumericalKernelFactory):
-    """The default numerical kernel factory for GP surrogates."""
+@define
+class _BayBENumericalKernelFactory(_PureKernelFactory):
+    """The default numerical kernel factory for GP surrogates.
 
+    Inspired by the dimension-scaled priors in :cite:p:`Hvarfner2024` but with slight
+    adjustements:
 
-class BayBEKernelFactory(SmoothedEDBOKernelFactory):  # type: ignore[valid-type, misc]
-    """The default kernel factory for GP surrogates."""
+    * Uses Matern instead of RBF kernel
+    * Uses a Gamma prior instead of a LogNormal prior for faster convergence (less heavy
+      tails). The parameters of the Gamma are set such that:
+        - The concentration matches that of the conventional (i.e., Hvarfner
+          predecessor) Gamma distribution used by BoTorch.
+        - The mode matches that of the LogNormal and thus also scales with sqrt(d).
+    """
+
+    _uses_parameter_names: ClassVar[bool] = True
+    # See base class.
+
+    @override
+    def _make(
+        self, searchspace: SearchSpace, objective: Objective, measurements: pd.DataFrame
+    ) -> Kernel | GPyTorchKernel:
+
+        from gpytorch.constraints import GreaterThan
+        from gpytorch.kernels import MaternKernel
+        from gpytorch.priors import GammaPrior
+
+        parameter_names = self.get_parameter_names(searchspace)
+
+        # For regular parameters, resolve parameter names to active dimension indices
+        active_dims = list(
+            chain.from_iterable(
+                searchspace.get_comp_rep_parameter_indices(name)
+                for name in parameter_names
+                if searchspace.get_parameters_by_name([name])[0]._kind
+                is _ParameterKind.REGULAR
+            )
+        )
+        ard_num_dims = len(active_dims)
+
+        concentration = 3.0
+        rate = (
+            (concentration - 1) / math.exp(math.sqrt(2) - 3) / math.sqrt(ard_num_dims)
+        )
+        lengthscale_prior = GammaPrior(concentration, rate)
+        base_kernel = MaternKernel(
+            ard_num_dims=ard_num_dims,
+            lengthscale_prior=lengthscale_prior,
+            lengthscale_constraint=GreaterThan(
+                2.5e-2, transform=None, initial_value=lengthscale_prior.mode
+            ),
+            active_dims=active_dims,
+        )
+
+        return base_kernel
 
 
 @define
@@ -67,12 +123,50 @@ class _BayBETaskKernelFactory(_PureKernelFactory):
         )
 
 
+@define
+class BayBELikelihoodFactory(LikelihoodFactoryProtocol):
+    """The default likelihood factory for GP surrogates.
+
+    Inspired by the likelihood proposed in :cite:p:`Hvarfner2024` but uses a Gamma prior
+    instead of a LogNormal prior for faster convergence (less heavy tails). The
+    parameters of the Gamma are set such that:
+
+    * The mode matches that of the LogNormal and thus also scales with sqrt(d).
+    * The curvature at the mode matches that of the LogNormal, resulting in similar
+      convergence behavior in the vicinity of the mode.
+    """
+
+    @override
+    def __call__(
+        self, searchspace: SearchSpace, objective: Objective, measurements: pd.DataFrame
+    ) -> GPyTorchLikelihood:
+
+        from botorch.models.utils.gpytorch_modules import MIN_INFERRED_NOISE_LEVEL
+        from gpytorch.constraints import GreaterThan
+        from gpytorch.likelihoods import GaussianLikelihood
+        from gpytorch.priors import GammaPrior
+
+        concentration = 2.0
+        rate = (concentration - 1) / math.exp(-4.0 - 1.0**2)
+        noise_prior = GammaPrior(concentration, rate)
+        return GaussianLikelihood(
+            noise_prior=noise_prior,
+            noise_constraint=GreaterThan(
+                MIN_INFERRED_NOISE_LEVEL,
+                transform=None,
+                initial_value=noise_prior.mode,
+            ),
+        )
+
+
+BayBEKernelFactory = _enable_transfer_learning(
+    _BayBENumericalKernelFactory, "BayBEKernelFactory"
+)
+"""The default kernel factory for GP surrogates."""
+
+
 class BayBEMeanFactory(LazyConstantMeanFactory):
     """The default mean factory for GP surrogates."""
-
-
-class BayBELikelihoodFactory(SmoothedEDBOLikelihoodFactory):
-    """The default likelihood factory for GP surrogates."""
 
 
 @define
