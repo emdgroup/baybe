178d71f8d2fcea3b2dc236f8fb3bd8f592b590c3
Martin Fitzner <martin.fitzner@merckgroup.com>
2026-07-03 11:11:05 +0200

    Simplify test_polars_label_duplicates Polars expression

--- tests/constraints/test_constraints_polars.py
+++ tests/constraints/test_constraints_polars.py
@@ -144,8 +144,7 @@ def test_polars_label_duplicates(parameters, constraints):
 
     ldf = ldf.with_columns(
         pl.concat_list(pl.col(["Solvent_1", "Solvent_2", "Solvent_3"]))
-        .list.eval(pl.element().n_unique())
-        .explode()
+        .list.n_unique()
         .alias("n_unique")
     )
     df = ldf.filter(pl.col("n_unique") != len(parameters)).collect()
