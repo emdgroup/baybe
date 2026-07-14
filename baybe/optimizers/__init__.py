071ff0155e5c4c5f312ac51c2aaaac68b94f6659
StefanPSchmid <schmiste@ethz.ch>
2026-07-14 10:28:41 +0200

    Add optimizer __init__

--- baybe/optimizers/__init__.py
+++ baybe/optimizers/__init__.py
@@ -1,9 +1,15 @@
 """Optimizers."""
 
 from baybe.optimizers.base import OptimizerProtocol
+from baybe.optimizers.composite import (
+    Alternating,
+    CompositionStrategy,
+)
 from baybe.optimizers.continuous import ContinuousOptimizer
 
 __all__ = [
+    "Alternating",
+    "CompositionStrategy",
     "ContinuousOptimizer",
     "OptimizerProtocol",
 ]
