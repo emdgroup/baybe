5efdf9c7ab6e6b3635f85ca1765eab1213181dcd
Martin Fitzner <martin.fitzner@merckgroup.com>
2026-07-22 11:30:20 +0200

    Expand threshold_conditions hypothesis strategy to vary all attributes

--- tests/hypothesis_strategies/conditions.py
+++ tests/hypothesis_strategies/conditions.py
@@ -5,7 +5,11 @@ from typing import Any
 import hypothesis.strategies as st
 
 from baybe.constraints import SubSelectionCondition, ThresholdCondition
-from tests.hypothesis_strategies.basic import finite_floats
+from baybe.constraints.conditions import (
+    _threshold_operators,
+    _valid_tolerance_operators,
+)
+from tests.hypothesis_strategies.basic import finite_floats, positive_finite_floats
 
 
 def sub_selection_conditions(superset: list[Any] | None = None):
@@ -19,6 +23,15 @@ def sub_selection_conditions(superset: list[Any] | None = None):
     )
 
 
-def threshold_conditions():
+@st.composite
+def threshold_conditions(draw: st.DrawFn):
     """Generate :class:`baybe.constraints.conditions.ThresholdCondition`."""
-    return st.builds(ThresholdCondition, threshold=finite_floats())
+    threshold = draw(finite_floats())
+    operator = draw(st.sampled_from(list(_threshold_operators.keys())))
+    if operator in _valid_tolerance_operators:
+        tolerance = draw(positive_finite_floats())
+    else:
+        tolerance = None
+    return ThresholdCondition(
+        threshold=threshold, operator=operator, tolerance=tolerance
+    )
