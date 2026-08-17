664646916d5fed8b5544ffa484a501a74f7cc961
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2026-08-17 15:57:04 +0200

    Remove `check_links` from doc building script

--- docs/scripts/build_documentation.py
+++ docs/scripts/build_documentation.py
@@ -7,7 +7,6 @@ import shutil
 from subprocess import check_call, run
 
 from build_examples import build_examples
-from check_links import check_links
 from utils import adjust_pictures
 
 parser = argparse.ArgumentParser()
@@ -18,15 +17,9 @@ parser.add_argument(
     action="store_true",
 )
 parser.add_argument(
-    "-l",
-    "--no_linkcheck",
-    help="Do not check the links.",
-    action="store_true",
-)
-parser.add_argument(
     "-r",
     "--full-rebuild",
-    help="Perform a full rebuild, independent of `-e` and `-l` flags.",
+    help="Perform a full rebuild, independent of `-e` flag.",
     action="store_true",
 )
 parser.add_argument(
@@ -45,7 +38,6 @@ parser.add_argument(
 # Parse input arguments
 args = parser.parse_args()
 RUN_EXAMPLES = args.run_examples
-LINKCHECK = not args.no_linkcheck
 FULL_REBUILD = args.full_rebuild
 INCLUDE_WARNINGS = args.include_warnings
 FORCE = args.force
@@ -79,16 +71,15 @@ def _run_apidoc() -> None:
 
 def build_documentation(
     run_examples: bool = False,
-    verify_links: bool = False,
     full_rebuild: bool = False,
     force: bool = False,
 ) -> None:
     """Build the documentation.
 
     A full build of the documentation consists of converting the examples into jupyter
-    notebooks, executing them, transforming them into markdown files, as well as
-    checking all links and performing the actual ``sphinx-build``. Such a full build can
-    be triggered using the ``full_rebuild`` flag.
+    notebooks, executing them, transforming them into markdown files, and performing
+    the actual ``sphinx-build``. Such a full build can be triggered using the
+    ``full_rebuild`` flag.
     If this flag is not set, this function tries to re-use as much of potentially
     existing structures like already built examples as possible. This behavior can be
     changed by using the other flags.
@@ -97,18 +88,14 @@ def build_documentation(
         run_examples: Fully recalculate the examples. If this is ``False`` and no
             folder containing an already built set of examples is found, dummy files
             replicating the structure of the examples are created.
-        verify_links: Check both internal and external links.
         full_rebuild: Perform a full rebuild of the documentation, including a
-            recalculation of the examples and checking the links. Note that this option
-            ignores the choices for ``run_examples`` and ``check_links`` if set to
-            ``True.
+            recalculation of the examples.
         force: Force-build the steps, ignoring any errors or warnings.
     """
     examples_directory = pathlib.Path("docs/examples")
     examples_exist = examples_directory.is_dir()
 
     rerun_examples = run_examples or full_rebuild
-    perform_linkcheck = verify_links or full_rebuild
 
     if rerun_examples:
         build_examples(
@@ -124,9 +111,6 @@ def build_documentation(
             dummy=True,
             remove_dir=examples_exist,
         )
-
-    if perform_linkcheck:
-        check_links()
 
     # Generate the API reference stubs via sphinx-apidoc
     _run_apidoc()
@@ -158,11 +142,8 @@ if __name__ == "__main__":
     if not INCLUDE_WARNINGS:
         os.environ["PYTHONWARNINGS"] = "ignore"
 
-    print(f"{LINKCHECK=}")
-
     build_documentation(
         run_examples=RUN_EXAMPLES,
-        verify_links=LINKCHECK,
         full_rebuild=FULL_REBUILD,
         force=FORCE,
     )
